d395f91c2c53cd782b0ef5691c4c75f697f9dcd8
malinink <apmath@malinink.ru>
2016-10-28 00:02:40 +0300

    Init with spring web application on Maven

---
+++ src/main/java/easytests/EasyTestsApplication.java
@@ -0,0 +1,12 @@
+package easytests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyTestsApplication.class, args);
+	}
+}
