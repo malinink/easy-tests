b18d30730cad7832afa1aa61efbeb9b6bbfd14c2
SingularityA <singularity.m101@gmail.com>
2016-11-03 16:32:07 +0300

    Edit UsersController: add students to list, correct code according to naming convention in java

--- src/main/java/easytests/controllers/UsersController.java
+++ src/main/java/easytests/controllers/UsersController.java
@@ -12,23 +12,33 @@ import java.util.ArrayList;
 
 /**
  * Created by vkpankov on 31.10.2016.
+ * Edited by SingularityA on 03.11.2016
 */
 @Controller
 @RequestMapping("/")
 public class UsersController {
+
     @RequestMapping("/users")
-    public ModelAndView GetUsersList(Model model)
-    {
+    public ModelAndView getUsersList(Model model) {
 
-        ArrayList<User> Users = new ArrayList<>(5);
-        Users.add(new User("Dmitriy","Ivanov","Ivanovich"));
-        Users.add(new User("Michael","Lightman","Smirnovich"));
-        Users.add(new User("Mark","Petrov","Petrovich"));
-        Users.add(new User("Dmitriy","Ivanov","Ivanovich"));
-        Users.add(new User("Dmitriy","Ivanov","Ivanovich"));
-        return new ModelAndView("UsersView", "UsersList", Users);
+        ArrayList<User> users = new ArrayList<>(15);
+        users.add(new User("Артем","Багаев","Робизонович"));
+        users.add(new User("Андрей","Влад","Иванович"));
+        users.add(new User("Зоригто","Доржиев","Жаргалович"));
+        users.add(new User("Алексей","Ермолаев","Николаевич"));
+        users.add(new User("Илья","Живолуп","Дмитриевич"));
+        users.add(new User("Василий","Каминский","Владимирович"));
+        users.add(new User("Владислав","Марков","Викторович"));
+        users.add(new User("Дмитрий","Марулин","Алексеевич"));
+        users.add(new User("Викентий","Панков","Дмитриевич"));
+        users.add(new User("Антон","Панченко","Дмитриевич"));
+        users.add(new User("Никита","Попов","Алексеевич"));
+        users.add(new User("Карина","Сахарова","Сергеевна"));
+        users.add(new User("Лия","Халиуллина","Рауфофна"));
+        users.add(new User("Джамиль","Шайморданов","Амирович"));
+        users.add(new User("Валентин","Шульга","Александрович"));
 
+        return new ModelAndView("UsersView", "usersList", users);
 
     }
-
 }
