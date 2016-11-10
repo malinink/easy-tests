7dfb141d3d4072119ac742f37b4ea7d0460ffa68
vkpankov <vkpankov@gmail.com>
2016-11-10 20:10:12 +0300

    Add UserService class

---
+++ src/main/java/easytests/service/UserService.java
@@ -0,0 +1,28 @@
+package service;
+
+import easytests.ConnectionFactory;
+import easytests.dao.UserDAO;
+import easytests.entities.User;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+/**
+ * Created by vkpankov on 10.11.2016.
+ */
+public class UserService {
+    public List<User> getAllUsers() throws Exception {
+        SqlSession session = ConnectionFactory.getSession().openSession();
+        UserDAO dao = session.getMapper(UserDAO.class);
+        List<User> users = dao.getAllUsers();
+        session.close();
+        return users;
+    }
+    public void addUser(User user) throws Exception{
+        SqlSession session = ConnectionFactory.getSession().openSession();
+        UserDAO dao =session.getMapper(UserDAO.class);
+        dao.addUser(user);
+        session.commit();
+        session.close();
+    }
+}
