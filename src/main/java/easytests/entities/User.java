94d4bac5093364e8610c107238f56bdccc2abd3d
Firkhraag <siglandrei@mail.ru>
2016-11-11 00:51:53 +0300

    Add User JPA Entity

--- src/main/java/easytests/entities/User.java
+++ src/main/java/easytests/entities/User.java
@@ -1,16 +1,48 @@
 package easytests.entities;
 
 import easytests.entities.user.CommonFieldsInterface;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+
+@Entity
+@Table(name="users")
 public class User implements CommonFieldsInterface {
+
+    @Id
+    @GenericGenerator(name="kaugen" , strategy="increment")
+    @GeneratedValue(generator="kaugen")
+    @Column(name="id", nullable = false)
+    private long id;
+
+    @Column(name="firstname", nullable = false)
     private String firstName;
+
+    @Column(name="lastname", nullable = false)
     private String lastName;
+
+    @Column(name="surname", nullable = false)
     private String surname;
 
     public User(String firstName, String lastName, String surname) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.surname = surname;
+    }
+
+    protected User(){}
+
+
+    public User(long id) {
+        this.id = id;
+        this.firstName = "NULL";
+        this.lastName = "NULL";
+        this.surname = "NULL";
     }
 
     @Override
@@ -28,4 +60,21 @@ public class User implements CommonFieldsInterface {
         return this.surname;
     }
 
+    @Override
+    public long getId() { return id; }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
