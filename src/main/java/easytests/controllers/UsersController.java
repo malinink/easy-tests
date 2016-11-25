2b028d872f11f37aeb5b1f49594199a5fc7c8db3
malinink <apmath@malinink.ru>
2016-11-25 22:05:40 +0300

    Use UsersMapper in UsrsController

--- src/main/java/easytests/controllers/UsersController.java
+++ src/main/java/easytests/controllers/UsersController.java
@@ -1,41 +1,23 @@
 package easytests.controllers;
 
 import easytests.entities.User;
+import easytests.mappers.UsersMapper;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.util.ArrayList;
 
-/**
- * Created by vkpankov on 31.10.2016.
- * Edited by SingularityA on 03.11.2016
- * Edited by malinink on 05.11.2016
- */
 @Controller
 @RequestMapping("/")
 public class UsersController {
 
+    @Autowired
+    private UsersMapper usersMapper;
+
     @RequestMapping("/users")
     public String list(Model model) {
-
-        ArrayList<User> users = new ArrayList<>(15);
-
-        users.add(new User("Артем","Багаев","Робизонович"));
-        users.add(new User("Андрей","Влад","Иванович"));
-        users.add(new User("Зоригто","Доржиев","Жаргалович"));
-        users.add(new User("Алексей","Ермолаев","Николаевич"));
-        users.add(new User("Илья","Живолуп","Дмитриевич"));
-        users.add(new User("Василий","Каминский","Владимирович"));
-        users.add(new User("Владислав","Марков","Викторович"));
-        users.add(new User("Дмитрий","Марулин","Алексеевич"));
-        users.add(new User("Викентий","Панков","Дмитриевич"));
-        users.add(new User("Антон","Панченко","Дмитриевич"));
-        users.add(new User("Никита","Попов","Алексеевич"));
-        users.add(new User("Карина","Сахарова","Сергеевна"));
-        users.add(new User("Лия","Халиуллина","Рауфофна"));
-        users.add(new User("Джамиль","Шайморданов","Амирович"));
-        users.add(new User("Валентин","Шульга","Александрович"));
-
+        List<User> users = this.usersMapper.readAll();
         model.addAttribute("users", users);
         return "users/list";
     }
