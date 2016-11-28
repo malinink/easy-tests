51e91384eb40770f30ccaa26f51e6fdace98bd19
SingularityA <singularity.m101@gmail.com>
2016-11-28 17:10:26 +0300

    Add newline in the end

--- src/main/java/easytests/EasyTestsApplication.java
+++ src/main/java/easytests/EasyTestsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasyTestsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasyTestsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EasyTestsApplication.class, args);
+    }
 }
