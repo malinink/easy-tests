1ca568a9b7be86e81be434abf514d7798929dcf2
malinink <apmath@malinink.ru>
2016-11-29 20:52:28 +0300

    Add docs folder into gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,34 @@ var gulp = require('gulp');
 
 // Include plugins
 var plugins = require("gulp-load-plugins")({
-	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
-	replaceString: /\bgulp[\-.]/
+    pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+    replaceString: /\bgulp[\-.]/
 });
 
 var config = {
+    'docs': 'docs/',
     'dest': 'src/main/resources/public/',
-	'source': 'bower_components'
+    'source': 'bower_components'
 };
 
 gulp.task('fonts', function() {
-	gulp.src(['bower_components/materialize/dist/fonts/*'])
-		.pipe(gulp.dest(config.dest + 'fonts'));
+    gulp.src(['bower_components/materialize/dist/fonts/*'])
+        .pipe(gulp.dest(config.docs + 'fonts'))
+        .pipe(gulp.dest(config.dest + 'fonts'));
 });
 
 gulp.task('js', function() {
-	gulp.src(plugins.mainBowerFiles())
-		.pipe(plugins.filter('*.js'))
-		.pipe(gulp.dest(config.dest + 'js'));
+    gulp.src(plugins.mainBowerFiles())
+        .pipe(plugins.filter('*.js'))
+        .pipe(gulp.dest(config.docs + 'js'))
+        .pipe(gulp.dest(config.dest + 'js'));
 });
 
 gulp.task('css', function() {
-	gulp.src(plugins.mainBowerFiles())
-		.pipe(plugins.filter('*.css'))
-		.pipe(gulp.dest(config.dest + 'css'));
+    gulp.src(plugins.mainBowerFiles())
+        .pipe(plugins.filter('*.css'))
+        .pipe(gulp.dest(config.docs + 'css'))
+        .pipe(gulp.dest(config.dest + 'css'));
 });
 
 gulp.task('img', function() {
