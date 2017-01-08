c30aadad7652a4040ea0ea20d1b8088f990d0d40
malinink <apmath@malinink.ru>
2017-01-08 12:46:24 +0300

    [cs] fixes

--- src/main/java/easytests/EasyTestsApplication.java
+++ src/main/java/easytests/EasyTestsApplication.java
@@ -1,10 +1,16 @@
 package easytests;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author malinink
+ */
 @SpringBootApplication
-public class EasyTestsApplication {
+public final class EasyTestsApplication {
+    private EasyTestsApplication() {
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EasyTestsApplication.class, args);
