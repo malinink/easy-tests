ae430addecc0fb30b482a95b252f35eefab1d6b9
Loriens <lerd.95@mail.ru>
2017-02-19 13:35:03 +0300

    Create TopicsMapper

---
+++ src/main/java/easytests/mappers/TopicsMapper.java
@@ -0,0 +1,14 @@
+
+package easytests.mappers;
+
+import easytests.entities.Topic;
+
+import java.util.List;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @author loriens
+ */
+
+public class TopicsMapper {
+}
