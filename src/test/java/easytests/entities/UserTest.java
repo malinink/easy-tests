5e5e727705aeddf34bdfa02c7a5ffd09133c28cc
malinink <apmath@malinink.ru>
2017-02-19 23:13:02 +0300

    [cs] on user test

--- src/test/java/easytests/entities/UserTest.java
+++ src/test/java/easytests/entities/UserTest.java
@@ -9,21 +9,21 @@ import static org.junit.Assert.assertEquals;
 public class UserTest {
     @Test
     public void testFirstName() throws Exception {
-        User testUser = new User();
+        final User testUser = new User();
         testUser.setFirstName("Ivan");
         assertEquals("Ivan", testUser.getFirstName());
     }
 
     @Test
     public void testLastName() throws Exception {
-        User testUser = new User();
+        final User testUser = new User();
         testUser.setLastName("Ivanov");
         assertEquals("Ivanov", testUser.getLastName());
     }
 
     @Test
     public void testSurname() throws Exception {
-        User testUser = new User();
+        final User testUser = new User();
         testUser.setSurname("Ivanovich");
         assertEquals("Ivanovich", testUser.getSurname());
     }
