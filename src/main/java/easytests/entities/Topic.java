b00d264875c89b457f677e1890dbdf4e49192cec
Loriens <lerd.95@mail.ru>
2017-02-19 13:09:37 +0300

    Create Topic.java

--- src/main/java/easytests/entities/Topic.java
+++ src/main/java/easytests/entities/Topic.java
@@ -1,7 +1,32 @@
 package easytests.entities;
 
+
+import easytests.entities.topic.CommonFieldsInterface;
+
+import java.util.List;
+
 /**
- * Created by loriens on 19.02.17.
+ * @author loriens
  */
-public class Topic {
+
+public class Topic implements CommonFieldsInterface {
+    String name;
+
+    List<Question> questions;
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    List<Question> getQuestions() {
+        return questions;
+    }
+
+    void addQuestion(Question question) {
+        questions.add(question);
+    }
 }
