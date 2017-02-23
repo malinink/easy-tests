aa412d6734d0beb273c607716aad78813c218da5
SingularityA <singularity.m101@gmail.com>
2017-02-23 23:54:33 +0300

    Update IssueStandardInterface

--- src/main/java/easytests/entities/IssueStandardInterface.java
+++ src/main/java/easytests/entities/IssueStandardInterface.java
@@ -1,7 +1,37 @@
 package easytests.entities;
 
+import java.util.List;
+
 /**
  * @author malinink
  */
 public interface IssueStandardInterface extends EntityInterface {
+
+    IssueStandardInterface setId(Integer id);
+
+    Integer getTimeLimit();
+
+    IssueStandardInterface setTimeLimit(Integer timeLimit);
+
+    Integer getQuestionsNumber();
+
+    IssueStandardInterface setQuestionsNumber(Integer questionsNumber);
+
+    List<Integer> getHighPriorityTopics();
+
+    IssueStandardInterface setHighPriorityTopics(List<Integer> highPriorityTopics);
+
+    List<Integer> getLowPriorityTopics();
+
+    IssueStandardInterface setLowPriorityTopics(List<Integer> lowPriorityTopics);
+
+    List<QuestionTypeLimitsInterface> getQuestionTypeLimits();
+
+    IssueStandardInterface setQuestionTypeLimits(List<QuestionTypeLimitsInterface> questionTypesLimits);
+
+    Integer getSubjectId();
+
+    IssueStandardInterface setSubjectId(Integer subjectId);
+
+    String toString();
 }
