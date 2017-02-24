a87a0a82238fefd96f260bc8a01d537608ab994b
FortyWays <zedront307@gmail.com>
2017-02-24 20:51:32 +0300

    Added test data for issues table

--- src/main/resources/sql/mappersTestData.sql
+++ src/main/resources/sql/mappersTestData.sql
@@ -21,6 +21,13 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE issues (
+  id          SERIAL        NOT NULL,
+  name        VARCHAR(100)  NOT NULL,
+  author_id   SERIAL        NOT NULL,
+  PRIMARY KEY (id)
+)
+
 
 ----------------------
 -- INSERT DATA
@@ -30,4 +37,9 @@ INSERT INTO users (first_name, last_name, surname) VALUES
   ('FirstName1', 'LastName1', 'Surname1'),
   ('FirstName2', 'LastName2', 'Surname2'),
   ('FirstName3', 'LastName3', 'Surname3');
+
+INSERT INTO issues (name, author_id) VALUES
+  ('Name1', 11),
+  ('Name2', 12),
+  ('Name3', 13);
 
