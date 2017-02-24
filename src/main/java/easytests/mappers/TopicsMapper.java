c9904f55e96c0a612932c91b800448fff8342d43
Loriens <lerd.95@mail.ru>
2017-02-24 15:41:27 +0300

    Update TopicsMapper

--- src/main/java/easytests/mappers/TopicsMapper.java
+++ src/main/java/easytests/mappers/TopicsMapper.java
@@ -1,7 +1,7 @@
 
 package easytests.mappers;
 
-import easytests.entities.Topic;
+import easytests.entities.*;
 
 import java.util.List;
 import org.apache.ibatis.annotations.*;
@@ -11,4 +11,29 @@ import org.apache.ibatis.annotations.*;
  */
 
 public interface TopicsMapper {
+
+    @Results(
+            id = "topicResults",
+            value = {
+                    @Result(property = "id", column = "id"),
+                    @Result(property = "name", column = "name"),
+                    @Result(property = "questions_id", column = "questions_id")
+            })
+    @Select("SELECT * FROM topics")
+    List<Topic> findAll();
+
+    @Select("SELECT * FROM topics where id=#{id}")
+    @ResultMap("Topic")
+    Topic find(Integer id);
+
+    @Insert("INSERT INTO topics (id, name, questions_id) VALUES(#{id}, #{name}, #{questions_id})")
+    @Options(useGeneratedKeys = true, keyColumn = "id")
+    void insert(TopicInterface topic);
+
+    @Update("UPDATE topics SET first_name=#{firstName}, last_name=#{last_name}, surname=#{surname} WHERE id=#{id}")
+    void update(TopicInterface topic);
+
+    @Delete("DELETE FROM topics WHERE id=#{id}")
+    void delete(TopicInterface topic);
+
 }
