ec438b9cffe992e59aca11744813b02c3d64b9a1
Loriens <lerd.95@mail.ru>
2017-02-24 14:52:05 +0300

    Update class Topic

--- src/main/java/easytests/entities/Topic.java
+++ src/main/java/easytests/entities/Topic.java
@@ -24,7 +24,7 @@ public class Topic implements CommonFieldsInterface {
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
