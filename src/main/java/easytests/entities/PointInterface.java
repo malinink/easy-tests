e067b1b2c3c2482871cd40459c1a6966c05080b6
tapochek97 <tapochek97@ya.ru>
2017-02-25 00:29:41 +0300

    Updated PointInterface Class

--- src/main/java/easytests/entities/PointInterface.java
+++ src/main/java/easytests/entities/PointInterface.java
@@ -1,7 +1,26 @@
 package easytests.entities;
 
+import java.util.List;
+
 /**
  * @author malinink
  */
 public interface PointInterface extends EntityInterface {
+    PointInterface setId(Integer id);
+
+    String getText();
+
+    PointInterface setText(String text);
+
+    String getType();
+
+    PointInterface setType(String type);
+
+    Quiz getQuiz();
+
+    PointInterface setQuiz(Quiz quiz);
+
+    List<Solution> getSolutions();
+
+    PointInterface setSolutions(List<Solution> solutions);
 }
