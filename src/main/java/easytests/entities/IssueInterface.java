1278583d7cc392b1dc9c0f69b5a930dac52ef67a
FortyWays <zedront307@gmail.com>
2017-02-24 20:48:38 +0300

    Issue Intefrace

--- src/main/java/easytests/entities/IssueInterface.java
+++ src/main/java/easytests/entities/IssueInterface.java
@@ -1,7 +1,23 @@
 package easytests.entities;
 
+
+
 /**
  * @author malinink
  */
 public interface IssueInterface extends IssueStandardInterface {
+
+    IssueInterface setId(Integer id);
+
+    String getName();
+
+    IssueInterface setName(String name);
+
+    Integer getAuthorId();
+
+    IssueInterface setAuthorId(Integer authorId);
+
+    //List<Quiz> getQuizzes();
+
+    //IssueInterface setQuizzes(List<Quiz> quizzes);
 }
