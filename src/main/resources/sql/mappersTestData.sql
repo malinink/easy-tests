89bb46142732e019d5d1ea381d260f89b1457de0
Firkhraag <siglandrei@mail.ru>
2017-02-25 13:34:40 +0300

    Add test table for Question

--- src/main/resources/sql/mappersTestData.sql
+++ src/main/resources/sql/mappersTestData.sql
@@ -7,6 +7,7 @@
 -- DROP TABLES
 ----------------------
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS questions;
 
 
 ----------------------
@@ -21,6 +22,14 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
+CREATE TABLE questions (
+  id          SERIAL      NOT NULL,
+  text        VARCHAR(80) NOT NULL,
+  type        INTEGER     NOT NULL,
+  topic_id    INTEGER     NOT NULL,
+  PRIMARY KEY (id)
+);
+
 
 ----------------------
 -- INSERT DATA
@@ -30,4 +39,9 @@ INSERT INTO users (first_name, last_name, surname) VALUES
   ('FirstName1', 'LastName1', 'Surname1'),
   ('FirstName2', 'LastName2', 'Surname2'),
   ('FirstName3', 'LastName3', 'Surname3');
+  
+INSERT INTO questions (text, type, topic_id) VALUES
+  ('test1', 1, 1),
+  ('test2', 2, 1),
+  ('test3', 3, 1);
 
