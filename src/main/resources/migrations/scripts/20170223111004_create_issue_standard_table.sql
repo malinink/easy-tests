102a69459b3e8b368901a9b21ab19b92c22df986
SingularityA <singularity.m101@gmail.com>
2017-02-26 01:49:23 +0300

    Remove foreign keys from migrations

--- src/main/resources/migrations/scripts/20170223111004_create_issue_standard_table.sql
+++ src/main/resources/migrations/scripts/20170223111004_create_issue_standard_table.sql
@@ -2,14 +2,12 @@
 -- Migration SQL that makes the change goes here.
 
 CREATE TABLE issue_standard (
-  id                SERIAL      NOT NULL,
+  id                SERIAL   NOT NULL,
   time_limit        INTEGER,
-  questions_number  INTEGER     CHECK (questions_number > 0),
-  subject_id        INTEGER     NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (subject_id)
-        REFERENCES subjects (id)
-        ON DELETE CASCADE
+  questions_number  INTEGER,
+  subject_id        INTEGER  NOT NULL,
+    CONSTRAINT positiveness CHECK (questions_number > 0 AND time_limit > 0),
+    PRIMARY KEY (id)
 );
 
 -- //@UNDO
--- src/main/resources/migrations/scripts/20170223111013_create_dependent_tables.sql
+++ src/main/resources/migrations/scripts/20170223111013_create_dependent_tables.sql
@@ -1,40 +1,28 @@
 -- // create_dependent_tables
 -- Migration SQL that makes the change goes here.
 
-CREATE TYPE PRIORITY AS ENUM ('LOW', 'HIGH');
+CREATE TYPE TOPIC_PRIORITY AS ENUM ('LOW', 'HIGH');
 
 CREATE TABLE topic_priorities (
-  id                   SERIAL      NOT NULL,
-  topic_id             INTEGER     NOT NULL,
-  topic_priority       PRIORITY    NOT NULL,
-  issue_standard_id    INTEGER     NOT NULL,
+  id                 SERIAL          NOT NULL,
+  topic_id           INTEGER         NOT NULL,
+  priority           TOPIC_PRIORITY  NOT NULL,
+  issue_standard_id  INTEGER         NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (topic_id, issue_standard_id),
-    FOREIGN KEY (topic_id)
-        REFERENCES topics (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (issue_standard_id)
-        REFERENCES issue_standard (id)
-        ON DELETE CASCADE
+    UNIQUE (topic_id, issue_standard_id)
 );
 
 CREATE TABLE question_type_options (
-  id                   SERIAL      NOT NULL,
-  question_type_id     INTEGER     NOT NULL,
-  min_number           INTEGER,
-  max_number           INTEGER,
-  time_limit           INTEGER,
-  issue_standard_id    INTEGER     NOT NULL,
+  id                 SERIAL   NOT NULL,
+  question_type_id   INTEGER  NOT NULL,
+  min_number         INTEGER,
+  max_number         INTEGER,
+  time_limit         INTEGER,
+  issue_standard_id  INTEGER  NOT NULL,
     CONSTRAINT positiveness CHECK (min_number >= 0 AND max_number > 0 AND time_limit > 0),
     CONSTRAINT check_limits CHECK (max_number >= min_number),
     PRIMARY KEY (id),
-    UNIQUE (question_type_id, issue_standard_id),
-    FOREIGN KEY (question_type_id)
-        REFERENCES question_types (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (issue_standard_id)
-        REFERENCES issue_standard (id)
-        ON DELETE CASCADE
+    UNIQUE (question_type_id, issue_standard_id)
 );
 
 -- //@UNDO
@@ -42,4 +30,4 @@ CREATE TABLE question_type_options (
 
 DROP TABLE question_type_options;
 DROP TABLE topic_priorities;
-DROP TYPE PRIORITY;
+DROP TYPE TOPIC_PRIORITY;
