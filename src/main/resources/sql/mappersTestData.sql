560349f6e71a8980427c4cc53b8624b646e2ba8c
SingularityA <singularity.m101@gmail.com>
2017-02-26 01:50:24 +0300

    Update test data in mappersTestData.sql

--- src/main/resources/sql/mappersTestData.sql
+++ src/main/resources/sql/mappersTestData.sql
@@ -8,11 +8,8 @@
 ----------------------
 DROP TABLE IF EXISTS question_type_options;
 DROP TABLE IF EXISTS topic_priorities;
-DROP  TYPE IF EXISTS PRIORITY;
+DROP  TYPE IF EXISTS TOPIC_PRIORITY;
 DROP TABLE IF EXISTS issue_standard;
-DROP TABLE IF EXISTS question_types;
-DROP TABLE IF EXISTS topics;
-DROP TABLE IF EXISTS subjects;
 DROP TABLE IF EXISTS users;
 
 ----------------------
@@ -27,74 +24,37 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE subjects (
-  id        SERIAL    NOT NULL,
-  user_id   INTEGER   NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id)
-        REFERENCES users (id)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE topics (
-  id            SERIAL    NOT NULL,
-  subject_id    INTEGER   NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (subject_id)
-        REFERENCES subjects (id)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE question_types (
-  id      SERIAL      NOT NULL,
+CREATE TABLE issue_standard (
+  id                SERIAL   NOT NULL,
+  time_limit        INTEGER,
+  questions_number  INTEGER,
+  subject_id        INTEGER  NOT NULL,
+    CONSTRAINT positiveness CHECK (questions_number > 0 AND time_limit > 0),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE issue_standard (
-  id                SERIAL      NOT NULL,
-  time_limit        INTEGER,
-  questions_number  INTEGER     CHECK (questions_number > 0),
-  subject_id        INTEGER     NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (subject_id)
-        REFERENCES subjects (id)
-        ON DELETE CASCADE
-);
-
-CREATE TYPE PRIORITY AS ENUM ('LOW', 'HIGH');
+CREATE TYPE TOPIC_PRIORITY AS ENUM ('LOW', 'HIGH');
 
 CREATE TABLE topic_priorities (
-  id                   SERIAL      NOT NULL,
-  topic_id             INTEGER     NOT NULL,
-  topic_priority       PRIORITY    NOT NULL,
-  issue_standard_id    INTEGER     NOT NULL,
+  id                 SERIAL          NOT NULL,
+  topic_id           INTEGER         NOT NULL,
+  priority           TOPIC_PRIORITY  NOT NULL,
+  issue_standard_id  INTEGER         NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (topic_id, issue_standard_id),
-    FOREIGN KEY (topic_id)
-        REFERENCES topics (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (issue_standard_id)
-        REFERENCES issue_standard (id)
-        ON DELETE CASCADE
+    UNIQUE (topic_id, issue_standard_id)
 );
 
 CREATE TABLE question_type_options (
-  id                   SERIAL      NOT NULL,
-  question_type_id     INTEGER     NOT NULL,
-  min_number           INTEGER,
-  max_number           INTEGER,
-  time_limit           INTEGER,
-  issue_standard_id    INTEGER     NOT NULL,
+  id                 SERIAL   NOT NULL,
+  question_type_id   INTEGER  NOT NULL,
+  min_number         INTEGER,
+  max_number         INTEGER,
+  time_limit         INTEGER,
+  issue_standard_id  INTEGER  NOT NULL,
     CONSTRAINT positiveness CHECK (min_number >= 0 AND max_number > 0 AND time_limit > 0),
     CONSTRAINT check_limits CHECK (max_number >= min_number),
     PRIMARY KEY (id),
-    UNIQUE (question_type_id, issue_standard_id),
-    FOREIGN KEY (question_type_id)
-        REFERENCES question_types (id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (issue_standard_id)
-        REFERENCES issue_standard (id)
-        ON DELETE CASCADE
+    UNIQUE (question_type_id, issue_standard_id)
 );
 
 ----------------------
@@ -106,20 +66,11 @@ INSERT INTO users (first_name, last_name, surname) VALUES
   ('FirstName2', 'LastName2', 'Surname2'),
   ('FirstName3', 'LastName3', 'Surname3');
 
-INSERT INTO subjects (user_id) VALUES
-  (1), (1), (2), (3), (3);
-
-INSERT INTO topics (subject_id) VALUES
-  (1), (1), (3), (5), (5);
-
-INSERT INTO question_types (id) VALUES
-  (1), (2), (3);
-
 INSERT INTO issue_standard (time_limit, questions_number, subject_id) VALUES
   (300, 30, 1),
   (NULL, 15, 3);
 
-INSERT INTO topic_priorities (topic_id, topic_priority, issue_standard_id) VALUES
+INSERT INTO topic_priorities (topic_id, priority, issue_standard_id) VALUES
   (1, 'HIGH', 1),
   (2, 'LOW', 1),
   (3, 'HIGH', 2);
