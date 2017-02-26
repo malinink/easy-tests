bc6ab707721946aab8ae7cc94a43e8ce0c91a4f8
SingularityA <singularity.m101@gmail.com>
2017-02-26 03:57:20 +0300

    Update migration

--- src/main/resources/migrations/scripts/20170223111004_create_issue_standard_table.sql
+++ src/main/resources/migrations/scripts/20170223111004_create_issue_standard_table.sql
@@ -7,7 +7,8 @@ CREATE TABLE issue_standard (
   questions_number  INTEGER,
   subject_id        INTEGER  NOT NULL,
     CONSTRAINT positiveness CHECK (questions_number > 0 AND time_limit > 0),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (subject_id)
 );
 
 -- //@UNDO
