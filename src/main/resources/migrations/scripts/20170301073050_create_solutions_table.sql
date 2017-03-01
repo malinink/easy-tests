c40f385963f9ea0dbcbb7411369ddb826bbbef65
SingularityA <singularity.m101@gmail.com>
2017-03-01 10:54:54 +0300

    Add migration for solutions table

---
+++ src/main/resources/migrations/scripts/20170301073050_create_solutions_table.sql
@@ -0,0 +1,15 @@
+-- // create_solutions_table
+-- Migration SQL that makes the change goes here.
+
+CREATE TABLE solutions (
+  id        SERIAL  NOT NULL,
+  answer_id INTEGER,
+  point_id  INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+DROP TABLE solutions;
