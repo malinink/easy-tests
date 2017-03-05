2cf6b2cbb51f82962b6533eb2710d095a06966bc
malinink <apmath@malinink.ru>
2017-03-05 15:43:53 +0300

    Fix test, first attempt on UsersService

--- src/test/java/easytests/services/UsersServiceTest.java
+++ src/test/java/easytests/services/UsersServiceTest.java
@@ -3,10 +3,12 @@ package easytests.services;
 import easytests.entities.UserEntity;
 import easytests.mappers.UsersMapper;
 import easytests.models.UserModel;
-import org.apache.catalina.User;
+import easytests.models.UserModelInterface;
 import org.junit.*;
 import org.junit.runner.*;
 import static org.mockito.BDDMockito.*;
+
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.boot.test.mock.mockito.*;
@@ -26,21 +28,36 @@ public class UsersServiceTest {
     private UsersService usersService;
 
     @Test
-    public void saveCreatesEntityTest() {
-        final UserModel userModel = new UserModel();
+    public void testFindAbsentModel() {
+        final Integer id = 10;
+        final UserModelInterface userModel = this.usersService.find(id);
+        Assert.assertEquals(null, userModel);
+    }
+
+    @Test
+    public void testSaveCreatesEntity() {
+        final UserModelInterface userModel = new UserModel();
+        userModel.setFirstName("FirstName");
+        userModel.setLastName("LastName");
+        userModel.setSurname("Surname");
         final UserEntity userEntity = new UserEntity();
         userEntity.map(userModel);
         this.usersService.save(userModel);
+
         verify(this.usersMapper, times(1)).insert(userEntity);
     }
 
     @Test
-    public void saveUpdatesEntityTest() {
-        final UserModel userModel = new UserModel();
+    public void testSaveUpdatesEntity() {
+        final UserModelInterface userModel = new UserModel();
         userModel.setId(1);
+        userModel.setFirstName("FirstName");
+        userModel.setLastName("LastName");
+        userModel.setSurname("Surname");
         final UserEntity userEntity = new UserEntity();
         userEntity.map(userModel);
         this.usersService.save(userModel);
+
         verify(this.usersMapper, times(1)).update(userEntity);
     }
 }
