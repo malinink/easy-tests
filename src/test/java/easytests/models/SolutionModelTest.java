0d92d815d51247426bf52110c35d095e18088832
SingularityA <singularity.m101@gmail.com>
2017-03-06 21:18:40 +0300

    Update SolutionModelTest: remove extra tests

--- src/test/java/easytests/models/SolutionModelTest.java
+++ src/test/java/easytests/models/SolutionModelTest.java
@@ -22,29 +22,6 @@ public class SolutionModelTest {
     }
 
     @Test
-    public void testId() throws Exception {
-        final SolutionModelInterface solution = new SolutionModel();
-        solution.setId(1);
-        Assert.assertEquals((Integer) 1, solution.getId());
-    }
-
-    @Test
-    public void testAnswer() throws Exception {
-        final SolutionModelInterface solution = new SolutionModel();
-        AnswerModelInterface answer = Mockito.mock(AnswerModelInterface.class);
-        solution.setAnswer(answer);
-        Assert.assertEquals(answer, solution.getAnswer());
-    }
-
-    @Test
-    public void testPoint() throws Exception {
-        final SolutionModelInterface solution = new SolutionModel();
-        PointModelInterface point = Mockito.mock(PointModelInterface.class);
-        solution.setPoint(point);
-        Assert.assertEquals(point, solution.getPoint());
-    }
-
-    @Test
     public void testMap() throws Exception {
         final Integer id = 1;
         final Integer answerId = 2;
