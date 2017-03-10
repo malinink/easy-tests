7b22c9d69c522ed1f19f73b40e283b31c2534090
DoZor-80 <zorikto-d@yandex.ru>
2017-03-10 23:13:46 +0300

    Created TesteeModel

---
+++ src/main/java/easytests/models/TesteeModel.java
@@ -0,0 +1,28 @@
+package easytests.models;
+
+import easytests.entities.TesteeEntity;
+import lombok.*;
+
+/**
+ * @author DoZor-80
+ */
+@Data
+public class TesteeModel implements TesteeModelInterface{
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String surname;
+
+    private Integer groupNumber;
+
+    public void map(TesteeEntity testeeEntity) {
+        this.setId(testeeEntity.getId());
+        this.setFirstName(testeeEntity.getFirstName());
+        this.setLastName(testeeEntity.getLastName());
+        this.setSurname(testeeEntity.getSurname());
+        this.setGroupNumber(testeeEntity.getGroupNumber());
+    }
+}
