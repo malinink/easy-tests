7b8a6ee26b3b48010386e814e190cb197a65c95f
FortyWays <zedront307@gmail.com>
2017-03-10 22:22:23 +0300

    Test for IssuesModel

---
+++ src/test/java/easytests/models/IssueModelTest.java
@@ -0,0 +1,48 @@
+package easytests.models;
+
+import easytests.entities.IssueEntity;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.meanbean.test.BeanTester;
+import org.meanbean.test.EqualsMethodTester;
+import org.meanbean.test.HashCodeMethodTester;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author fortyways
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IssueModelTest {
+
+    @Test
+    public void testCommon() throws Exception {
+        new BeanTester().testBean(UserModel.class);
+        new EqualsMethodTester().testEqualsMethod(UserModel.class);
+        new HashCodeMethodTester().testHashCodeMethod(UserModel.class);
+    }
+
+    @Test
+    public void testMap() throws Exception {
+        final Integer issueId = 1;
+        final String name = "Name";
+        final Integer authorId = 11;
+        final IssueEntity issueEntity = Mockito.mock(IssueEntity.class);
+
+        Mockito.when(issueEntity.getId()).thenReturn(issueId);
+        Mockito.when(issueEntity.getName()).thenReturn(name);
+        Mockito.when(issueEntity.getAuthorId()).thenReturn(authorId);
+
+        final IssueModel issueModel = new IssueModel();
+        issueModel.map(issueEntity);
+
+        Assert.assertEquals(issueId, issueModel.getId());
+        Assert.assertEquals(name, issueModel.getName());
+        Assert.assertEquals(authorId, issueModel.getAuthorId());
+
+    }
+
+}
