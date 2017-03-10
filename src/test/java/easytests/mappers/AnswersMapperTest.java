7b94a9a6017c87cb48e4d0409b0eed0a60d4b82a
rezenbekk <rezenbekk@gmail.com>
2017-03-10 23:19:02 +0300

    Added AnswerMapperTest

--- src/test/java/easytests/mappers/AnswersMapperTest.java
+++ src/test/java/easytests/mappers/AnswersMapperTest.java
@@ -1,17 +1,22 @@
 package easytests.mappers;
 
 import easytests.config.DatabaseConfig;
-import easytests.entities.Answer;
-import easytests.entities.AnswerInterface;
+import easytests.entities.AnswerEntity;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.*;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 /**
@@ -27,21 +32,79 @@ public class AnswersMapperTest {
     private AnswersMapper answersMapper;
 
     @Test
-    public void testFind() throws Exception {
-        final AnswerInterface answer = this.answersMapper.find(1);
+    public void testFindAll() throws Exception {
+        final List<AnswerEntity> answersEntities = this.answersMapper.findAll();
+        Assert.assertEquals((long) 3, (long) answersEntities.size());
+    }
 
-        Assert.assertEquals((long) 1, (long) answer.getId());
+    @Test
+    public void testFind() throws Exception {
+        final AnswerEntity answer = this.answersMapper.find(1);
+        Assert.assertEquals((Integer) 1, answer.getId());
         Assert.assertEquals("Answer1", answer.getTxt());
-        Assert.assertEquals((long) 1, (long) answer.getQuestionId());
-        Assert.assertEquals(true, answer.getIsRight());
+        Assert.assertEquals((Integer) 1, answer.getQuestionId());
+        Assert.assertEquals(true, answer.isRight());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        final Integer id = this.answersMapper.findAll().size() + 1;
+        final String txt = "Text";
+        final Integer questionId = 1;
+        final Boolean right = true;
+
+        AnswerEntity answerEntity = Mockito.mock(AnswerEntity.class);
+        Mockito.when(answerEntity.getTxt()).thenReturn(txt);
+        Mockito.when(answerEntity.getQuestionId()).thenReturn(questionId);
+        Mockito.when(answerEntity.isRight()).thenReturn(right);
+
+        this.answersMapper.insert(answerEntity);
+
+        verify(answerEntity, times(1)).setId(id);
+
+        answerEntity = this.answersMapper.find(id);
+        Assert.assertEquals(id, answerEntity.getId());
+        Assert.assertEquals(txt, answerEntity.getTxt());
+        Assert.assertEquals(questionId, answerEntity.getQuestionId());
+        Assert.assertEquals(right, answerEntity.isRight());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        final Integer id = 1;
+        final String txt = "Answer1";
+        final Integer questionId = 1;
+        final Boolean right = true;
+
+        AnswerEntity answerEntity = this.answersMapper.find(id);
+        Assert.assertNotNull(answerEntity);
+        Assert.assertEquals(id, answerEntity.getId());
+        Assert.assertEquals(txt, answerEntity.getTxt());
+        Assert.assertEquals(questionId, answerEntity.getQuestionId());
+        Assert.assertEquals(right, answerEntity.isRight());
+
+        answerEntity = Mockito.mock(AnswerEntity.class);
+        Mockito.when(answerEntity.getId()).thenReturn(id);
+        Mockito.when(answerEntity.getTxt()).thenReturn(txt);
+        Mockito.when(answerEntity.getQuestionId()).thenReturn(questionId);
+        Mockito.when(answerEntity.isRight()).thenReturn(right);
+
+        this.answersMapper.update(answerEntity);
+
+        answerEntity = this.answersMapper.find(id);
+        Assert.assertEquals(id, answerEntity.getId());
+        Assert.assertEquals(txt, answerEntity.getTxt());
+        Assert.assertEquals(questionId, answerEntity.getQuestionId());
+        Assert.assertEquals(right, answerEntity.isRight());
     }
 
     @Test
     public void testDelete() throws Exception {
-        Answer answer = this.answersMapper.find(1);
-        Assert.assertNotNull(answer);
-        this.answersMapper.delete(answer);
-        answer = this.answersMapper.find(1);
-        Assert.assertNull(answer);
+        AnswerEntity answerEntity = this.answersMapper.find(1);
+        Assert.assertNotNull(answerEntity);
+
+        this.answersMapper.delete(answerEntity);
+        answerEntity = this.answersMapper.find(1);
+        Assert.assertNull(answerEntity);
     }
 }
