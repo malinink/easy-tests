9d59b5965205b6a9307c66b6a9de764277f29ea9
rezenbekk <rezenbekk@gmail.com>
2017-03-10 23:21:06 +0300

    Added AnswersServiceTest

--- src/test/java/easytests/services/AnswersServiceTest.java
+++ src/test/java/easytests/services/AnswersServiceTest.java
@@ -1,11 +1,20 @@
 package easytests.services;
 
-import easytests.entities.Answer;
+import easytests.entities.AnswerEntity;
 import easytests.mappers.AnswersMapper;
+import easytests.models.AnswerModel;
+import easytests.models.AnswerModelInterface;
+import easytests.services.exceptions.DeleteUnidentifiedModelException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.*;
 
 import static org.mockito.BDDMockito.*;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 import org.springframework.boot.test.mock.mockito.*;
@@ -18,24 +27,124 @@ import org.springframework.test.context.junit4.*;
 @SpringBootTest
 public class AnswersServiceTest {
 
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
     @MockBean
     private AnswersMapper answersMapper;
 
     @Autowired
     private AnswersService answersService;
 
-    @Test
-    public void saveCreatesEntityTest() {
-        final Answer answer = new Answer();
-        this.answersService.save(answer);
-        verify(this.answersMapper, times(1)).insert(answer);
+    private AnswerModelInterface createAnswerModel(Integer id, String txt, Integer questionId, boolean right) {
+        final AnswerModelInterface answerModel = new AnswerModel();
+        answerModel.setId(id);
+        answerModel.setTxt(txt);
+        answerModel.setQuestionId(questionId);
+        answerModel.setRight(right);
+        return answerModel;
+    }
+
+    private AnswerEntity createAnswerEntityMock(Integer id, String txt, Integer questionId, boolean right) {
+        final AnswerEntity answerEntity = Mockito.mock(AnswerEntity.class);
+        Mockito.when(answerEntity.getId()).thenReturn(id);
+        Mockito.when(answerEntity.getTxt()).thenReturn(txt);
+        Mockito.when(answerEntity.getQuestionId()).thenReturn(questionId);
+        Mockito.when(answerEntity.isRight()).thenReturn(right);
+        return answerEntity;
+    }
+
+    private AnswerModelInterface mapAnswerModel(AnswerEntity answerEntity) {
+        final AnswerModelInterface answerModel = new AnswerModel();
+        answerModel.map(answerEntity);
+        return answerModel;
+    }
+
+    private AnswerEntity mapAnswerEntity(AnswerModelInterface answerModel) {
+        final AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.map(answerModel);
+        return answerEntity;
     }
 
     @Test
-    public void saveUpdatesEntityTest() {
-        final Answer answer = new Answer();
-        answer.setId(1);
-        this.answersService.save(answer);
-        verify(this.answersMapper, times(1)).update(answer);
+    public void testFindAllPresentList() throws Exception {
+        final List<AnswerEntity> answersEntities = new ArrayList<>(2);
+        final AnswerEntity answerEntityFirst = this.createAnswerEntityMock(1, "Answer1", 1, true);
+        final AnswerEntity answerEntitySecond = this.createAnswerEntityMock(2, "Answer2", 2, false);
+        answersEntities.add(answerEntityFirst);
+        answersEntities.add(answerEntitySecond);
+        given(this.answersMapper.findAll()).willReturn(answersEntities);
+
+        final List<AnswerModelInterface> answersModels = this.answersService.findAll();
+
+        Assert.assertEquals(2, answersModels.size());
+        Assert.assertEquals(answersModels.get(0), this.mapAnswerModel(answerEntityFirst));
+        Assert.assertEquals(answersModels.get(1), this.mapAnswerModel(answerEntitySecond));
     }
+
+    @Test
+    public void testFindAllAbsentList() throws Exception {
+        given(this.answersMapper.findAll()).willReturn(new ArrayList<>(0));
+
+        final List<AnswerModelInterface> answersModels = this.answersService.findAll();
+
+        Assert.assertEquals(0, answersModels.size());
+    }
+
+    @Test
+    public void testFindPresentModel() throws Exception {
+        final Integer id = 1;
+        final AnswerEntity answerEntity = this.createAnswerEntityMock(id, "NewAnswer", 1, true);
+        given(this.answersMapper.find(id)).willReturn(answerEntity);
+
+        final AnswerModelInterface answerModel = this.answersService.find(id);
+
+        Assert.assertEquals(this.mapAnswerModel(answerEntity), answerModel);
+    }
+
+    @Test
+    public void testFindAbsentModel() throws Exception {
+        final Integer id = 10;
+        given(this.answersMapper.find(id)).willReturn(null);
+
+        final AnswerModelInterface answerModel = this.answersService.find(id);
+
+        Assert.assertEquals(null, answerModel);
+    }
+
+    @Test
+    public void testSaveCreatesEntity() throws Exception {
+        final AnswerModelInterface answerModel = this.createAnswerModel(null, "Answer", 1, true);
+
+        this.answersService.save(answerModel);
+
+        verify(this.answersMapper, times(1)).insert(this.mapAnswerEntity(answerModel));
+    }
+
+    @Test
+    public void testSaveUpdatesEntity() throws Exception {
+        final AnswerModelInterface answerModel = this.createAnswerModel(1, "Answer", 1, true);
+
+        this.answersService.save(answerModel);
+
+        verify(this.answersMapper, times(1)).update(this.mapAnswerEntity(answerModel));
+    }
+
+    @Test
+    public void testDeleteIdentifiedModel() throws Exception {
+        final AnswerModelInterface answerModel = this.createAnswerModel(1, "Answer", 1, true);
+
+        this.answersService.delete(answerModel);
+
+        verify(this.answersMapper, times(1)).delete(this.mapAnswerEntity(answerModel));
+    }
+
+    @Test
+    public void testDeleteUnidentifiedModel() throws Exception {
+        final AnswerModelInterface answerModel = this.createAnswerModel(null, "Answer", 1, true);
+
+        exception.expect(DeleteUnidentifiedModelException.class);
+        this.answersService.delete(answerModel);
+    }
+
 }
