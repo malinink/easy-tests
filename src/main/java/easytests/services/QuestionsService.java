56c02667305837a4acce1ac7b514a7fb3ade64d5
Firkhraag <siglandrei@mail.ru>
2017-03-11 00:34:57 +0300

    Update QuestionsService

--- src/main/java/easytests/services/QuestionsService.java
+++ src/main/java/easytests/services/QuestionsService.java
@@ -1,8 +1,10 @@
 package easytests.services;
 
-import easytests.entities.Question;
-import easytests.entities.QuestionInterface;
+import easytests.entities.QuestionEntity;
 import easytests.mappers.QuestionsMapper;
+import easytests.models.QuestionModel;
+import easytests.models.QuestionModelInterface;
+import easytests.services.exceptions.DeleteUnidentifiedModelException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,32 +17,53 @@ import org.springframework.stereotype.Service;
 public class QuestionsService {
 
     @Autowired
-    private QuestionsMapper usersMapper;
+    private QuestionsMapper questionsMapper;
 
-    public List<QuestionInterface> findAll() {
-        return this.map(this.usersMapper.findAll());
+    public List<QuestionModelInterface> findAll() {
+        return this.map(this.questionsMapper.findAll());
     }
 
-    public QuestionInterface find(Integer id) {
-        return this.usersMapper.find(id);
-    }
-
-    public void save(QuestionInterface question) {
-        if (question.getId() == null) {
-            this.usersMapper.insert(question);
-        } else {
-            this.usersMapper.update(question);
+    public QuestionModelInterface find(Integer id) {
+        final QuestionEntity questionEntity = this.questionsMapper.find(id);
+        if (questionEntity == null) {
+            return null;
         }
+        return this.map(questionEntity);
     }
 
-    public void delete(QuestionInterface question) {
-        this.usersMapper.delete(question);
+    public void save(QuestionModelInterface questionModel) {
+        final QuestionEntity questionEntity = this.map(questionModel);
+        if (questionEntity.getId() == null) {
+            this.questionsMapper.insert(questionEntity);
+            return;
+        }
+        this.questionsMapper.update(questionEntity);
     }
 
-    private List<QuestionInterface> map(List<Question> questionsList) {
-        final List<QuestionInterface> resultQuestionList = new ArrayList(questionsList.size());
-        for (Question question: questionsList) {
-            resultQuestionList.add(question);
+    public void delete(QuestionModelInterface questionModel) {
+        final QuestionEntity questionEntity = this.map(questionModel);
+        if (questionEntity.getId() == null) {
+            throw new DeleteUnidentifiedModelException();
+        }
+        this.questionsMapper.delete(questionEntity);
+    }
+
+    private QuestionModelInterface map(QuestionEntity questionEntity) {
+        final QuestionModelInterface questionModel = new QuestionModel();
+        questionModel.map(questionEntity);
+        return questionModel;
+    }
+
+    private QuestionEntity map(QuestionModelInterface questionModel) {
+        final QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.map(questionModel);
+        return questionEntity;
+    }
+
+    private List<QuestionModelInterface> map(List<QuestionEntity> questionsList) {
+        final List<QuestionModelInterface> resultQuestionList = new ArrayList(questionsList.size());
+        for (QuestionEntity question: questionsList) {
+            resultQuestionList.add(this.map(question));
         }
         return resultQuestionList;
     }
