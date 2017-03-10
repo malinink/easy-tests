23fd1e0df82fea824fee99bd023c5010f7179f12
rezenbekk <rezenbekk@gmail.com>
2017-03-10 23:16:41 +0300

    Modified AnswerModelInterface

--- src/main/java/easytests/models/AnswerModelInterface.java
+++ src/main/java/easytests/models/AnswerModelInterface.java
@@ -1,8 +1,24 @@
 package easytests.models;
 
+import easytests.entities.AnswerEntity;
+
 /**
- * @author SomeOne
+ * @author rezenbekk
  */
 public interface AnswerModelInterface extends ModelInterface {
+    void setId(Integer id);
 
+    String getTxt();
+
+    void setTxt(String txt);
+
+    Integer getQuestionId();
+
+    void setQuestionId(Integer id);
+
+    Boolean isRight();
+
+    void setRight(Boolean right);
+
+    void map(AnswerEntity answerEntity);
 }
