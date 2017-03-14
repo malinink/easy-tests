1b710bec66af2f65ac972df45f19f3a3e211784f
SingularityA <singularity.m101@gmail.com>
2017-03-14 18:05:34 +0300

    Update IssueStandard mapper (cause: entity has changed)

--- src/main/java/easytests/mappers/IssueStandardsMapper.java
+++ src/main/java/easytests/mappers/IssueStandardsMapper.java
@@ -1,8 +1,6 @@
 package easytests.mappers;
 
-import easytests.entities.IssueStandard;
-import easytests.entities.IssueStandardInterface;
-import easytests.entities.SubjectInterface;
+import easytests.entities.IssueStandardEntity;
 import java.util.List;
 import org.apache.ibatis.annotations.*;
 
@@ -17,27 +15,29 @@ public interface IssueStandardsMapper {
         value = {
             @Result(property = "id", column = "id"),
             @Result(property = "timeLimit", column = "time_limit"),
-            @Result(property = "questionsNumber", column = "questions_number")
+            @Result(property = "questionsNumber", column = "questions_number"),
+            @Result(property = "subjectId", column = "subject_id")
         })
     @Select("SELECT * FROM issue_standard")
-    List<IssueStandard> findAll();
+    List<IssueStandardEntity> findAll();
 
     @Select("SELECT * FROM issue_standard WHERE id=#{id}")
     @ResultMap("IssueStandard")
-    IssueStandard find(Integer id);
+    IssueStandardEntity find(Integer id);
 
-    @Select("SELECT * FROM issue_standard WHERE subject_id=#{id}")
+    @Select("SELECT * FROM issue_standard WHERE subject_id=#{subjectId}")
     @ResultMap("IssueStandard")
-    IssueStandard findBySubject(SubjectInterface subject);
+    IssueStandardEntity findBySubjectId(Integer subjectId);
 
     @Insert("INSERT INTO issue_standard (time_limit, questions_number, subject_id)"
-            + " VALUES (#{timeLimit}, #{questionsNumber}, #{subject.id})")
+            + " VALUES (#{timeLimit}, #{questionsNumber}, #{subjectId})")
     @Options(useGeneratedKeys = true, keyColumn = "id")
-    void insert(IssueStandardInterface issueStandard);
+    void insert(IssueStandardEntity issueStandard);
 
-    @Update("UPDATE issue_standard SET time_limit=#{timeLimit}, questions_number=#{questionsNumber} WHERE id=#{id}")
-    void update(IssueStandardInterface issueStandard);
+    @Update("UPDATE issue_standard SET time_limit=#{timeLimit}, questions_number=#{questionsNumber},"
+            + " subject_id=#{subjectId} WHERE id=#{id}")
+    void update(IssueStandardEntity issueStandard);
 
     @Delete("DELETE FROM issue_standard WHERE id=#{id}")
-    void delete(IssueStandardInterface issueStandard);
+    void delete(IssueStandardEntity issueStandard);
 }
