ca81cebd491c59b5e920ea9d85c4a91ad60d2520
SingularityA <singularity.m101@gmail.com>
2017-03-14 23:01:42 +0300

    Create IssueStandardTopicPriority model interface

--- src/main/java/easytests/models/IssueStandardTopicPriorityModelInterface.java
+++ src/main/java/easytests/models/IssueStandardTopicPriorityModelInterface.java
@@ -1,7 +1,25 @@
 package easytests.models;
 
+import easytests.entities.IssueStandardTopicPriorityEntity;
+
 /**
  * @author SingularityA
  */
 public interface IssueStandardTopicPriorityModelInterface extends ModelInterface {
+
+    void setId(Integer id);
+
+    Integer getTopicId();
+
+    void setTopicId(Integer topicId);
+
+    Boolean getIsPreferable();
+
+    void setIsPreferable(Boolean isPreferable);
+
+    IssueStandardModelInterface getIssueStandard();
+
+    void setIssueStandard(IssueStandardModelInterface issueStandard);
+
+    void map(IssueStandardTopicPriorityEntity topicPriorityEntity);
 }
