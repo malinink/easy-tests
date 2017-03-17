3c09cf8ad6716b03a131ee604314860f843d7e41
rezenbekk <rezenbekk@gmail.com>
2017-03-17 12:40:48 +0300

    isRight -> getRight (этого я пытался избежать с помощью кастомных геттеров)

--- src/main/java/easytests/models/AnswerModel.java
+++ src/main/java/easytests/models/AnswerModel.java
@@ -12,19 +12,13 @@ public class AnswerModel implements AnswerModelInterface {
 
     private String txt;
 
-    private Integer questionId;
+    private QuestionModelInterface question;
 
-    @Getter(AccessLevel.NONE)
     private Boolean right;
-
-    public Boolean isRight() {
-        return this.right;
-    }
 
     public void map(AnswerEntity answerEntity) {
         this.setId(answerEntity.getId());
         this.setTxt(answerEntity.getTxt());
-        this.setQuestionId(answerEntity.getQuestionId());
-        this.setRight(answerEntity.isRight());
+        this.setRight(answerEntity.getRight());
     }
 }
