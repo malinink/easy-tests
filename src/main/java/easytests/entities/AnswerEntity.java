2444277c04f0c1e8c95f32cd3a17906fb21256ed
rezenbekk <rezenbekk@gmail.com>
2017-03-17 12:41:49 +0300

    getQuestionId->getQuestion; isRight->getRight

--- src/main/java/easytests/entities/AnswerEntity.java
+++ src/main/java/easytests/entities/AnswerEntity.java
@@ -14,17 +14,12 @@ public class AnswerEntity {
 
     private Integer questionId;
 
-    @Getter(AccessLevel.NONE)
     private Boolean right;
-
-    public Boolean isRight() {
-        return right;
-    }
 
     public void map(AnswerModelInterface answerModel) {
         this.setId(answerModel.getId());
         this.setTxt(answerModel.getTxt());
-        this.setQuestionId(answerModel.getQuestionId());
-        this.setRight(answerModel.isRight());
+        this.setQuestionId(answerModel.getQuestion().getId());
+        this.setRight(answerModel.getRight());
     }
 }
