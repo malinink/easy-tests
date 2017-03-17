145aa9c17ec845c8001f51517aca9ccec4e18316
rezenbekk <rezenbekk@gmail.com>
2017-03-17 12:50:44 +0300

    Removed @Ignore

--- src/test/java/easytests/models/empty/AnswerModelEmptyTest.java
+++ src/test/java/easytests/models/empty/AnswerModelEmptyTest.java
@@ -1,7 +1,6 @@
 package easytests.models.empty;
 
 import easytests.models.exceptions.CreateEmptyModelWithNullIdException;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author rezenbekk
  */
 @RunWith(SpringRunner.class)
-@Ignore
 @SpringBootTest
 public class AnswerModelEmptyTest extends AbstractModelEmptyTest {
     public AnswerModelEmptyTest() {
