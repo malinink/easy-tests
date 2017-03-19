510026b940ce03db26728cb050e98ca7cb24d0d7
malinink <apmath@malinink.ru>
2017-03-19 22:30:30 +0300

    Add UsersOptionsTest template

---
+++ src/test/java/easytests/options/UsersOptionsTest.java
@@ -0,0 +1,17 @@
+package easytests.options;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author malinink
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersOptionsTest {
+    @Test
+    public void test() throws Exception {
+    }
+}
