4169c74d4b6d3a46b00f14084dcc148064bae26d
malinink <apmath@malinink.ru>
2017-03-23 23:27:28 +0300

    Update UsersMapper to provide access to new fields

--- src/main/java/easytests/mappers/UsersMapper.java
+++ src/main/java/easytests/mappers/UsersMapper.java
@@ -16,20 +16,24 @@ public interface UsersMapper {
             @Result(property = "id", column = "id"),
             @Result(property = "firstName", column = "first_name"),
             @Result(property = "lastName", column = "last_name"),
-            @Result(property = "surname", column = "surname")
+            @Result(property = "surname", column = "surname"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "isAdmin", column = "is_admin"),
+            @Result(property = "state", column = "state"),
         })
-    @Select("SELECT id, first_name, last_name, surname FROM users")
+    @Select("SELECT * FROM users")
     List<UserEntity> findAll();
 
-    @Select("SELECT id, first_name, last_name, surname FROM users where id=#{id}")
+    @Select("SELECT * FROM users where id=#{id}")
     @ResultMap("User")
     UserEntity find(Integer id);
 
-    @Insert("INSERT INTO users (first_name, last_name, surname) VALUES(#{firstName}, #{lastName}, #{surname})")
+    @Insert("INSERT INTO users (first_name, last_name, surname, email, password, is_admin, state) VALUES(#{firstName}, #{lastName}, #{surname}, #{email}, #{password}, #{isAdmin}, #{state})")
     @Options(useGeneratedKeys = true, keyColumn = "id")
     void insert(UserEntity user);
 
-    @Update("UPDATE users SET first_name=#{firstName}, last_name=#{lastName}, surname=#{surname} WHERE id=#{id}")
+    @Update("UPDATE users SET first_name=#{firstName}, last_name=#{lastName}, surname=#{surname}, email=#{email}, password=#{password}, is_admin=#{isAdmin}, state=#{state} WHERE id=#{id}")
     void update(UserEntity user);
 
     @Delete("DELETE FROM users WHERE id=#{id}")
