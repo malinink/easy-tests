f6cbb744ebbbd74c6a1e2d83ef00fd4b6972a451
vkpankov <vkpankov@gmail.com>
2017-03-24 00:11:59 +0300

    Create SubjectsOptionsInterface

--- src/main/java/easytests/options/SubjectsOptionsInterface.java
+++ src/main/java/easytests/options/SubjectsOptionsInterface.java
@@ -1,13 +1,37 @@
 package easytests.options;
 
 import easytests.models.SubjectModelInterface;
+import easytests.services.IssueStandardsServiceInterface;
+import easytests.services.SubjectsServiceInterface;
+import easytests.services.TopicsServiceInterface;
+import easytests.services.UsersServiceInterface;
+
 import java.util.List;
 
 /**
  * @author malinink
  */
 public interface SubjectsOptionsInterface {
+
+    void setSubjectsService(SubjectsServiceInterface subjectsService);
+
+    void setUsersService(UsersServiceInterface usersService);
+
+    void setIssueStandardsService(IssueStandardsServiceInterface issueStandardsService);
+
+    void setTopicsService(TopicsServiceInterface topicsService);
+
+    SubjectsOptionsInterface withUser(UsersOptionsInterface usersOptions);
+
+    SubjectsOptionsInterface withIssueStandard(IssueStandardsOptionsInterface issueStandardOptions);
+
+    SubjectsOptionsInterface withTopics(TopicsOptionsInterface topicsOptions);
+
     SubjectModelInterface withRelations(SubjectModelInterface subjectModel);
 
     List<SubjectModelInterface> withRelations(List<SubjectModelInterface> subjectsModels);
+
+    void saveWithRelations(SubjectModelInterface subjectModel);
+
+    void deleteWithRelations(SubjectModelInterface subjectModel);
 }
