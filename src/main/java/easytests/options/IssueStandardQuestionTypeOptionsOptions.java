ac76d5c5febc4d8351609a4e07886c272e440512
SingularityA <singularity.m101@gmail.com>
2017-03-24 15:10:26 +0300

    Add QuestionTypeOptions options

---
+++ src/main/java/easytests/options/IssueStandardQuestionTypeOptionsOptions.java
@@ -0,0 +1,65 @@
+package easytests.options;
+
+import easytests.models.IssueStandardQuestionTypeOptionModelInterface;
+import easytests.services.IssueStandardQuestionTypeOptionsServiceInterface;
+import easytests.services.IssueStandardsServiceInterface;
+import java.util.List;
+import lombok.Setter;
+
+/**
+ * @author SingularityA
+ */
+public class IssueStandardQuestionTypeOptionsOptions implements IssueStandardQuestionTypeOptionsOptionsInterface {
+
+    @Setter
+    private IssueStandardQuestionTypeOptionsServiceInterface questionTypeOptionsService;
+
+    @Setter
+    private IssueStandardsServiceInterface issueStandardsService;
+
+    private IssueStandardsOptionsInterface issueStandardsOptions;
+
+    @Override
+    public IssueStandardQuestionTypeOptionsOptionsInterface
+        withIssueStandard(IssueStandardsOptionsInterface issueStandardsOptions) {
+        this.issueStandardsOptions = issueStandardsOptions;
+        return this;
+    }
+
+    @Override
+    public IssueStandardQuestionTypeOptionModelInterface
+        withRelations(IssueStandardQuestionTypeOptionModelInterface questionTypeOptionModel) {
+
+        if (this.issueStandardsOptions != null) {
+            questionTypeOptionModel.setIssueStandard(
+                    this.issueStandardsService.find(questionTypeOptionModel.getIssueStandard().getId()));
+        }
+        return questionTypeOptionModel;
+    }
+
+    @Override
+    public List<IssueStandardQuestionTypeOptionModelInterface>
+        withRelations(List<IssueStandardQuestionTypeOptionModelInterface> questionTypeOptionModels) {
+
+        for (IssueStandardQuestionTypeOptionModelInterface questionTypeOptionModel: questionTypeOptionModels) {
+            this.withRelations(questionTypeOptionModel);
+        }
+        return questionTypeOptionModels;
+    }
+
+    @Override
+    public void saveWithRelations(IssueStandardQuestionTypeOptionModelInterface questionTypeOptionModel) {
+        if (this.issueStandardsOptions != null) {
+            this.issueStandardsService.save(questionTypeOptionModel.getIssueStandard());
+        }
+        this.questionTypeOptionsService.save(questionTypeOptionModel);
+    }
+
+    @Override
+    public void deleteWithRelations(IssueStandardQuestionTypeOptionModelInterface questionTypeOptionModel) {
+        this.questionTypeOptionsService.delete(questionTypeOptionModel);
+        if (this.issueStandardsOptions != null) {
+            this.issueStandardsService.delete(questionTypeOptionModel.getIssueStandard());
+        }
+    }
+}
