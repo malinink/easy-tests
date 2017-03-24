18bdf9860601bf93c506702d393b1a8680da41a5
vkpankov <vkpankov@gmail.com>
2017-03-24 12:27:20 +0300

    Update TopicsServiceInterface, add save/delete, modify findBySubject

--- src/main/java/easytests/services/TopicsService.java
+++ src/main/java/easytests/services/TopicsService.java
@@ -1,14 +1,12 @@
 package easytests.services;
 
 import easytests.entities.TopicEntity;
-import easytests.mappers.TopicsMapper;
 import easytests.models.SubjectModelInterface;
 import easytests.models.TopicModel;
 import easytests.models.TopicModelInterface;
 import easytests.options.TopicsOptionsInterface;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -19,20 +17,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicsService implements TopicsServiceInterface {
 
-    @Autowired
-    private TopicsMapper topicsMapper;
-
     public List<TopicModelInterface> findBySubject(SubjectModelInterface subjectModel,
                                                    TopicsOptionsInterface topicsOptions) {
-
-        return topicsOptions.withRelations(this.map(this.topicsMapper.findBySubjectId(subjectModel.getId())));
-
+        //TODO vkpankov, need topicsMapper for it
+        //return topicsOptions.withRelations(this.map(this.topicsMapper.findBySubjectId(subjectModel.getId())));
+        return null;
     }
 
     public List<TopicModelInterface> findBySubject(SubjectModelInterface subjectModel) {
 
-        return this.map(this.topicsMapper.findBySubjectId(subjectModel.getId()));
+        //TODO vkpankov, need topicsMapper for it
+        //return this.map(this.topicsMapper.findBySubjectId(subjectModel.getId()));
+        return null;
+    }
 
+    public void save(List<TopicModelInterface> topicModel, TopicsOptionsInterface topicOptions) {
+        //TODO: Loriends
+    }
+
+    public void delete(List<TopicModelInterface> topicModel, TopicsOptionsInterface topicOptions) {
+        //TODO: Loriends
     }
 
     private TopicModelInterface map(TopicEntity topicEntity) {
