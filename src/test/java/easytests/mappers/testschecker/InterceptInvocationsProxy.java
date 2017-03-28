20b85805a8c34896eec6551e76827a4548b46559
vkpankov <vkpankov@gmail.com>
2017-03-28 12:29:26 +0300

    Create InterceptInvocationsProxy

---
+++ src/test/java/easytests/mappers/testschecker/InterceptInvocationsProxy.java
@@ -0,0 +1,36 @@
+package easytests.mappers.testschecker;
+
+import lombok.Getter;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xxx
+ */
+public final class InterceptInvocationsProxy implements InvocationHandler {
+
+    @Getter
+    private final List<Method> methodList;
+
+    private final Object mapperBean;
+
+    public InterceptInvocationsProxy(Object originalMapper) {
+
+        methodList = new ArrayList<>();
+        mapperBean = originalMapper;
+
+    }
+
+    public Object invoke(Object proxy, Method m, Object[] args) {
+        methodList.add(m);
+        try {
+            return m.invoke(mapperBean, args);
+        } catch (Exception anyException) {
+            return null;
+        }
+    }
+}
+
