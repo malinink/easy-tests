fbc286f5ee0de73f55b9cfb2819c165f14fb2b60
vkpankov <vkpankov@gmail.com>
2017-04-07 16:53:43 +0300

    SubjectEntity: add description field

--- src/main/java/easytests/entities/SubjectEntity.java
+++ src/main/java/easytests/entities/SubjectEntity.java
@@ -15,10 +15,13 @@ public class SubjectEntity {
 
     private Integer userId;
 
+    private String description;
+
     public void map(SubjectModelInterface subjectModel) {
 
         this.setId(subjectModel.getId());
         this.setName(subjectModel.getName());
+        this.setDescription(subjectModel.getDescription());
         this.setUserId(subjectModel.getUser().getId());
     }
 }
