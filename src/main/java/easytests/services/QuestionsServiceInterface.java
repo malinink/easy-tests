9088a9bb782918087f0a07a5200d03d07e448414
Firkhraag <firkhraag@gmail.com>
2017-04-07 23:05:17 +0300

    provide QuestionsServiceInterface (not mine)

---
+++ src/main/java/easytests/services/QuestionsServiceInterface.java
@@ -0,0 +1,32 @@
+package easytests.services;
+
+import easytests.models.QuestionModelInterface;
+import easytests.models.TopicModelInterface;
+import easytests.options.QuestionsOptionsInterface;
+import java.util.List;
+
+/**
+ * @author firkhraag
+ */
+public interface QuestionsServiceInterface extends ServiceInterface {
+
+    List<QuestionModelInterface> findAll();
+
+    List<QuestionModelInterface> findAll(QuestionsOptionsInterface questionsOptions);
+
+    List<QuestionModelInterface> findByTopic(TopicModelInterface topicModel);
+
+    List<QuestionModelInterface> findByTopic(TopicModelInterface topicModel, QuestionsOptionsInterface questionsOptions);
+
+    QuestionModelInterface find(Integer id);
+
+    QuestionModelInterface find(Integer id, QuestionsOptionsInterface questionsOptions);
+
+    void save(QuestionModelInterface questionModel);
+
+    void save(QuestionModelInterface questionModel, QuestionsOptionsInterface questionsOptions);
+
+    void delete(QuestionModelInterface questionModel);
+
+    void delete(QuestionModelInterface questionModel, QuestionsOptionsInterface questionsOptions);
+}
