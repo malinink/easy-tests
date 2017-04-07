f3e6bbf09203cdad7a072e05b87bff2c22454c23
FortyWays <zedront307@gmail.com>
2017-04-07 22:29:54 +0300

    Changed QuizzesService(Empty for now)

--- src/main/java/easytests/services/QuizzesService.java
+++ src/main/java/easytests/services/QuizzesService.java
@@ -1,7 +1,10 @@
 package easytests.services;
 
-import easytests.entities.*;
 import easytests.mappers.QuizzesMapper;
+
+import easytests.models.IssueModelInterface;
+import easytests.models.QuizModelInterface;
+import easytests.options.QuizzesOptionsInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,34 +14,23 @@ import org.springframework.stereotype.Service;
  * @author vkpankov
  */
 @Service
-public class QuizzesService {
-
+public class QuizzesService implements QuizzesServiceInterface {
     @Autowired
     private QuizzesMapper quizzesMapper;
 
-    public List<QuizInterface> findAll() {
-        return quizzesMapper.findAll();
+    @Override
+    public List<QuizModelInterface> findByIssue(IssueModelInterface issueModel,
+                                                QuizzesOptionsInterface quizzesOptions) {
+        return null;
     }
 
-    public QuizInterface find(Integer id) {
-        return quizzesMapper.find(id);
+    @Override
+    public void save(List<QuizModelInterface> quizModels, QuizzesOptionsInterface quizzesOptions) {
+        //
     }
 
-    public List<QuizInterface> findByIssue(IssueInterface issue) {
-        return quizzesMapper.findByIssue(issue);
-    }
-
-    public void save(QuizInterface quiz) {
-
-        if (quiz.getId() == null) {
-            this.quizzesMapper.insert(quiz);
-        } else {
-            this.quizzesMapper.update(quiz);
-        }
-
-    }
-
-    public void delete(QuizInterface quiz) {
-        this.quizzesMapper.delete(quiz);
+    @Override
+    public void delete(List<QuizModelInterface> quizModels, QuizzesOptionsInterface quizzesOptions) {
+        //
     }
 }
