31a5edc3b9e75c8bbeb74cd652e0e19ef924fcf7
malinink <apmath@malinink.ru>
2017-04-07 23:40:56 +0300

    Append minimal methods into question model interface

--- src/main/java/easytests/options/QuestionsOptionsInterface.java
+++ src/main/java/easytests/options/QuestionsOptionsInterface.java
@@ -1,8 +1,14 @@
 package easytests.options;
 
+import easytests.models.QuestionModelInterface;
+import java.util.List;
+
 
 /**
  * @author malinink
  */
 public interface QuestionsOptionsInterface extends OptionsInterface {
+    QuestionModelInterface withRelations(QuestionModelInterface questionModel);
+
+    List<QuestionModelInterface> withRelations(List<QuestionModelInterface> questionModel);
 }
