82e69097936b37350f9f141b7d9b626bd4239eab
malinink <apmath@malinink.ru>
2017-04-07 22:07:02 +0300

    Add topic model interface

--- src/main/java/easytests/models/TopicModelInterface.java
+++ src/main/java/easytests/models/TopicModelInterface.java
@@ -1,8 +1,22 @@
 package easytests.models;
 
+import java.util.List;
+
 /**
- * @author vkpankov
+ * @author malinink
  */
 public interface TopicModelInterface extends ModelInterface {
+    void setId(Integer id);
 
+    String getName();
+
+    void setName(String name);
+
+    SubjectModelInterface getSubject();
+
+    void setSubject(SubjectModelInterface subject);
+
+    List<QuestionModelInterface> getQuestions();
+
+    void setQuestions(List<QuestionModelInterface> questions);
 }
