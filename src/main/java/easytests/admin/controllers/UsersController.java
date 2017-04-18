bfc0288179f9c75f35538d5f1ec0140875d6118c
malinink <apmath@malinink.ru>
2017-04-18 22:21:29 +0300

    Add admin package

---
+++ src/main/java/easytests/admin/controllers/UsersController.java
@@ -0,0 +1,16 @@
+package easytests.admin.controllers;
+
+import easytests.personal.controllers.AbstractPersonalController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author malinink
+ */
+@Controller
+@RequestMapping("/admin/users/")
+public class UsersController extends AbstractPersonalController {
+
+
+
+}
