9d1a209c223532ac24cffe1c6455c7b38aba56c1
tapochek97 <tapochek97@ya.ru>
2017-04-24 02:03:06 +0300

    Add PointModelInterface

--- src/main/java/easytests/models/PointModelInterface.java
+++ src/main/java/easytests/models/PointModelInterface.java
@@ -1,8 +1,26 @@
 package easytests.models;
 
+import easytests.entities.PointEntity;
+
 /**
- * @author SomeOne
+ * @author nikitalpopov
  */
 public interface PointModelInterface extends ModelInterface {
+
+    void setId(Integer id);
+
+    String getType();
+
+    void setType(String type);
+
+    String getText();
+
+    void setText(String text);
+
+    QuizModelInterface getQuiz();
+
+    void setQuiz(QuizModelInterface quiz);
+
+    void map(PointEntity pointEntity);
 
 }
