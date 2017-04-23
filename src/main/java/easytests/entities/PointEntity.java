8112f9f626f3b66d2c30823a400fea0577114944
tapochek97 <tapochek97@ya.ru>
2017-04-24 01:43:18 +0300

    Add PointEntity instead of Point

---
+++ src/main/java/easytests/entities/PointEntity.java
@@ -0,0 +1,31 @@
+package easytests.entities;
+
+import easytests.models.PointModelInterface;
+import lombok.Data;
+
+/**
+ * @author nikitalpopov
+ */
+@Data
+public class PointEntity {
+
+    private Integer id;
+
+    private String type;
+
+    private String text;
+
+    private Integer quizId;
+
+    private void map(PointModelInterface pointModel) {
+
+        this.setId(pointModel.getId());
+
+        this.setType(pointModel.getType());
+
+        this.setText(pointModel.getText());
+
+        this.setQuizId(pointModel.getQuiz().getId());
+
+    }
+}
