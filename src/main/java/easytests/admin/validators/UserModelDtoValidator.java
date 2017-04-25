5d2b2e5d7fa543c7f5308f3e2587f7084c78f973
malinink <apmath@malinink.ru>
2017-04-25 22:20:25 +0300

    Add user validation further class realization

--- src/main/java/easytests/admin/validators/UserModelDtoValidator.java
+++ src/main/java/easytests/admin/validators/UserModelDtoValidator.java
@@ -23,13 +23,48 @@ public class UserModelDtoValidator extends AbstractValidator {
 
     public void validate(Object obj, Errors errors) {
         final UserModelDto userModelDto = (UserModelDto) obj;
+        this.validateIdEquals(errors, userModelDto);
         this.validateEmailIsUnique(errors, userModelDto);
+        this.validatePassword(errors, userModelDto);
+        this.validatePasswordRepeat(errors, userModelDto);
+    }
+
+    private void validateIdEquals(Errors errors, UserModelDto userModelDto) {
+        final Integer routeId = userModelDto.getRouteId();
+        final Integer id = userModelDto.getId();
+        if (((routeId == null) && (id != null)) || ((routeId != null) && !routeId.equals(id))) {
+            reject(errors, "id", "Id must be the same");
+        }
     }
 
     private void validateEmailIsUnique(Errors errors, UserModelDto userModelDto) {
         final UserModelInterface userModel = this.usersService.findByEmail(userModelDto.getEmail());
         if (userModel != null) {
             reject(errors, "email", "This Email already present");
+        }
+    }
+
+    private void validatePassword(Errors errors, UserModelDto userModelDto) {
+        final String password = userModelDto.getPassword();
+        final String passwordField = "password";
+        if ("".equals(password) || (password == null)) {
+            if (userModelDto.getRouteId() == null) {
+                reject(errors, passwordField, "Password is required on creating user procedure");
+            }
+            return;
+        }
+        if (password.length() < 8) {
+            reject(errors, passwordField, "Password must be at least 8 symbols");
+        }
+        // TODO add more specific checks on password
+    }
+
+    private void validatePasswordRepeat(Errors errors, UserModelDto userModelDto) {
+        final String password = userModelDto.getPassword();
+        final String passwordRepeat = userModelDto.getPasswordRepeat();
+        if (((password == null) && (passwordRepeat != null))
+                || ((password != null) && !password.equals(passwordRepeat))) {
+            reject(errors, "passwordRepeat", "Password repeat differs from password");
         }
     }
 }
