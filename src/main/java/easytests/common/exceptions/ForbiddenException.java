bb3a2080d7a273feb10fa469ec8a380ec0f413c9
malinink <apmath@malinink.ru>
2017-04-27 22:05:46 +0300

    Move forbiden exception into common package

--- src/main/java/easytests/common/exceptions/ForbiddenException.java
+++ src/main/java/easytests/common/exceptions/ForbiddenException.java
@@ -1,4 +1,4 @@
-package easytests.personal.exceptions;
+package easytests.common.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
