e3a5ac11d8c0c5b7e119da10299c037f412c3dcd
SingularityA <singularity.m101@gmail.com>
2017-04-28 20:57:01 +0300

    Add exceptions in common module

---
+++ src/main/java/easytests/common/exceptions/ForbiddenException.java
@@ -0,0 +1,13 @@
+package easytests.common.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author vkpankov
+ */
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "403 Access denied")
+public class ForbiddenException extends RuntimeException {
+
+}
+
