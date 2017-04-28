2cc696bce688dc0d60c1fe657da3c73e2588af96
DoZor-80 <zorikto-d@yandex.ru>
2017-04-28 20:24:03 +0300

    Updated TesteeEntityTest

--- src/test/java/easytests/entities/TesteeEntityTest.java
+++ src/test/java/easytests/entities/TesteeEntityTest.java
@@ -1,6 +1,7 @@
 package easytests.entities;
 
 import easytests.models.TesteeModelInterface;
+import easytests.models.empty.QuizModelEmpty;
 import org.junit.Assert;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
@@ -26,6 +27,7 @@ public class TesteeEntityTest {
         final String lastName = "LastName";
         final String surname = "Surname";
         final Integer groupNumber = 307;
+        final Integer quizId = 3;
 
         final TesteeModelInterface testeeModel = Mockito.mock(TesteeModelInterface.class);
         Mockito.when(testeeModel.getId()).thenReturn(testeeId);
@@ -33,6 +35,7 @@ public class TesteeEntityTest {
         Mockito.when(testeeModel.getLastName()).thenReturn(lastName);
         Mockito.when(testeeModel.getSurname()).thenReturn(surname);
         Mockito.when(testeeModel.getGroupNumber()).thenReturn(groupNumber);
+        Mockito.when(testeeModel.getQuiz()).thenReturn(new QuizModelEmpty(quizId));
 
         final TesteeEntity testeeEntity = new TesteeEntity();
         testeeEntity.map(testeeModel);
@@ -42,5 +45,6 @@ public class TesteeEntityTest {
         Assert.assertEquals(lastName, testeeEntity.getLastName());
         Assert.assertEquals(surname, testeeEntity.getSurname());
         Assert.assertEquals(groupNumber, testeeEntity.getGroupNumber());
+        Assert.assertEquals(quizId, testeeEntity.getQuizId());
     }
 }
