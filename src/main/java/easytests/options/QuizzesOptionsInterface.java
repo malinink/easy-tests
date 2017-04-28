9fc71ad1ecad50107045e42df805387430fb9e0d
DoZor-80 <zorikto-d@yandex.ru>
2017-04-28 20:10:57 +0300

    Updated QuizzesOptionsInterface

--- src/main/java/easytests/options/QuizzesOptionsInterface.java
+++ src/main/java/easytests/options/QuizzesOptionsInterface.java
@@ -1,14 +1,23 @@
 package easytests.options;
 
 import easytests.models.QuizModelInterface;
-
+import easytests.services.IssuesServiceInterface;
+import easytests.services.QuizzesServiceInterface;
 import java.util.List;
 
 /**
  * @author fortyways
  */
 public interface QuizzesOptionsInterface {
+    void setQuizzesService(QuizzesServiceInterface quizzesService);
+
+    void setIssuesService(IssuesServiceInterface issuesService);
+
     QuizModelInterface withRelations(QuizModelInterface subjectModel);
 
     List<QuizModelInterface> withRelations(List<QuizModelInterface> subjectsModels);
+
+    void saveWithRelations(QuizModelInterface quizModel);
+
+    void deleteWithRelations(QuizModelInterface quizModel);
 }
