489505695a8d280126fc26ee586125d4e107eb0b
DoZor-80 <zorikto-d@yandex.ru>
2017-04-28 20:00:35 +0300

    Created QuizModelEmpty

---
+++ src/main/java/easytests/models/empty/QuizModelEmpty.java
@@ -0,0 +1,61 @@
+package easytests.models.empty;
+
+import easytests.entities.QuizEntity;
+import easytests.models.IssueModelInterface;
+import easytests.models.PointModelInterface;
+import easytests.models.QuizModelInterface;
+import easytests.models.exceptions.CallMethodOnEmptyModelException;
+import java.util.List;
+
+/**
+ * @author DoZor-80
+ */
+public class QuizModelEmpty extends AbstractModelEmpty implements QuizModelInterface {
+    public QuizModelEmpty() {
+        super();
+    }
+
+    public QuizModelEmpty(Integer id) {
+        super(id);
+    }
+
+    @Override
+    public void setId(Integer id) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public String getInviteCode() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public void setInviteCode(String inviteCode) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public IssueModelInterface getIssue() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public void setIssue(IssueModelInterface issue) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public List<PointModelInterface> getPoints() {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public void setPoints(List<PointModelInterface> points) {
+        throw new CallMethodOnEmptyModelException();
+    }
+
+    @Override
+    public void map(QuizEntity quizEntity) {
+        throw new CallMethodOnEmptyModelException();
+    }
+}
