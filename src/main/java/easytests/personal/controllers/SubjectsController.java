0b69512593cfc4c66b6dd5056f43344df74c9d61
vkpankov <vkpankov@gmail.com>
2017-04-28 12:18:13 +0300

    Fix @Controller annotation

--- src/main/java/easytests/personal/controllers/SubjectsController.java
+++ src/main/java/easytests/personal/controllers/SubjectsController.java
@@ -19,6 +19,7 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.*;
  * @author vkpankov
  */
 @SuppressWarnings("ClassDataAbstractionCoupling")
+@Controller
 @RequestMapping("/personal/subjects/")
 public class SubjectsController extends AbstractPersonalController {
 
