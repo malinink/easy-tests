c58e759542125aee130730802c3802a8dcf68abd
DoZor-80 <zorikto-d@yandex.ru>
2017-04-28 19:59:09 +0300

    Updated TesteesMapper

--- src/main/java/easytests/mappers/TesteesMapper.java
+++ src/main/java/easytests/mappers/TesteesMapper.java
@@ -16,7 +16,8 @@ public interface TesteesMapper {
                     @Result(property = "firstName", column = "first_name"),
                     @Result(property = "lastName", column = "last_name"),
                     @Result(property = "surname", column = "surname"),
-                    @Result(property = "groupNumber", column = "group_number")
+                    @Result(property = "groupNumber", column = "group_number"),
+                    @Result(property = "quizId", column = "quiz_id")
             })
     @Select("SELECT * FROM testees")
     List<TesteeEntity> findAll();
@@ -25,13 +26,17 @@ public interface TesteesMapper {
     @ResultMap("Testee")
     TesteeEntity find(Integer id);
 
+    @Select("SELECT * FROM testees WHERE quiz_id=#{quizId}")
+    @ResultMap("Testee")
+    TesteeEntity findByQuizId(Integer quizId);
+
     @Insert("INSERT INTO testees (first_name, last_name, surname, group_number) VALUES(#{firstName}, "
-            + "#{lastName}, #{surname}, #{groupNumber})")
+            + "#{lastName}, #{surname}, #{groupNumber}, #{quizId})")
     @Options(useGeneratedKeys = true, keyColumn = "id")
     void insert(TesteeEntity testee);
 
     @Update("UPDATE testees SET first_name=#{firstName}, last_name=#{lastName}, surname=#{surname}, "
-            + "group_number=#{groupNumber} WHERE id=#{id}")
+            + "group_number=#{groupNumber}, quiz_id=#{quizId} WHERE id=#{id}")
     void update(TesteeEntity testee);
 
     @Delete("DELETE FROM testees WHERE id=#{id}")
