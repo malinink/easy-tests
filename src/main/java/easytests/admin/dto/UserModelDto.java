ddc5fbeb2d467dd85e9f3e12256520aa64efade8
malinink <apmath@malinink.ru>
2017-04-29 21:46:54 +0300

    I still need id for email validation, so id is present, BUT not mapping

--- src/main/java/easytests/admin/dto/UserModelDto.java
+++ src/main/java/easytests/admin/dto/UserModelDto.java
@@ -2,10 +2,7 @@ package easytests.admin.dto;
 
 import easytests.models.UserModelInterface;
 import easytests.models.empty.ModelsListEmpty;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -16,6 +13,9 @@ import org.hibernate.validator.constraints.NotEmpty;
  */
 @Data
 public class UserModelDto {
+    @Null
+    private Integer id;
+
     @NotNull
     @NotEmpty
     @Size(min = 2, max = 30)
