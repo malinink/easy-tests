cae97a4e411a791b4a05419edb7472ca92f7d852
SingularityA <singularity.m101@gmail.com>
2017-05-02 23:04:15 +0300

    Change TopicPriority dto (cause: checkboxes)

--- src/main/java/easytests/personal/dto/IssueStandardTopicPriorityDto.java
+++ src/main/java/easytests/personal/dto/IssueStandardTopicPriorityDto.java
@@ -4,12 +4,15 @@ import easytests.models.IssueStandardTopicPriorityModelInterface;
 import easytests.models.empty.IssueStandardModelEmpty;
 import easytests.models.empty.TopicModelEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 /**
  * @author Singularity
  */
 @Data
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
 public class IssueStandardTopicPriorityDto {
 
     private Integer id;
@@ -17,19 +20,25 @@ public class IssueStandardTopicPriorityDto {
     @NotNull(message = "Topic may not be null")
     private Integer topicId;
 
-    @NotNull(message = "Priority may not be null")
-    private Boolean isPreferable;
+    @Size(max = 2)
+    private String isPreferable;
 
     public void map(IssueStandardTopicPriorityModelInterface topicPriorityModel) {
         this.setId(topicPriorityModel.getId());
         this.setTopicId(topicPriorityModel.getTopic().getId());
-        this.setIsPreferable(topicPriorityModel.getIsPreferable());
+        if (topicPriorityModel.getIsPreferable()) {
+            this.isPreferable = "on";
+        }
     }
 
     public void mapInto(IssueStandardTopicPriorityModelInterface topicPriorityModel, Integer issueStandardId) {
         topicPriorityModel.setId(this.getId());
         topicPriorityModel.setTopic(new TopicModelEmpty(this.getTopicId()));
-        topicPriorityModel.setIsPreferable(this.getIsPreferable());
+        if ("on".equals(this.isPreferable)) {
+            topicPriorityModel.setIsPreferable(true);
+        } else {
+            topicPriorityModel.setIsPreferable(false);
+        }
         topicPriorityModel.setIssueStandard(new IssueStandardModelEmpty(issueStandardId));
     }
 }
