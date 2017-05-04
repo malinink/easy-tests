cb5087cdf3ba192cadd20acbe27aa2a134a46935
tapochek97 <tapochek97@ya.ru>
2017-05-04 23:13:57 +0300

    Update PointsMapperTest

--- src/test/java/easytests/mappers/PointsMapperTest.java
+++ src/test/java/easytests/mappers/PointsMapperTest.java
@@ -6,6 +6,7 @@ import easytests.mappers.PointsMapper;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -25,19 +26,25 @@ import java.util.List;
 @ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {DatabaseConfig.class})
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/mappersTestData.sql")
 public class PointsMapperTest {
+
     @Autowired
     private PointsMapper pointsMapper;
 
     @Test
     public void testFindAll() throws Exception {
-        final List<Point> testPoints = this.pointsMapper.findAll();
-        Assert.assertNotNull(testPoints);
-        Assert.assertEquals(3, testPoints.size());
+
+        final List<PointEntity> testPointEntities = this.pointsMapper.findAll();
+
+        Assert.assertNotNull(testPointEntities);
+        Assert.assertEquals((long) 3, (long) testPointEntities.size());
+
     }
 
     @Test
     public void testFind() throws Exception {
-        final PointInterface testPoint = this.pointsMapper.find(1);
+
+        final PointEntity testPoint = this.pointsMapper.find(1);
+
         Assert.assertNotNull(testPoint);
         Assert.assertEquals((Integer) 1, testPoint.getId());
         Assert.assertEquals("type1", testPoint.getType());
@@ -47,34 +54,73 @@ public class PointsMapperTest {
 
     @Test
     public void testInsert() throws Exception {
-        final Point testPoint = new Point();
-        testPoint.setType("type4");
-        testPoint.setText("test4");
-        testPoint.setQuizId(4);
+
+        final Integer testId = this.pointsMapper.findAll().size() + 1;
+        final String testType = "Test type";
+        final String testText = "Test text";
+        final Integer testQuizId = 1;
+
+        final PointEntity testPoint = Mockito.mock(PointEntity.class);
+
+        Mockito.when(testPoint.getId()).thenReturn(testId);
+        Mockito.when(testPoint.getType()).thenReturn(testType);
+        Mockito.when(testPoint.getText()).thenReturn(testText);
+        Mockito.when(testPoint.getQuizId()).thenReturn(testQuizId);
+
         pointsMapper.insert(testPoint);
 
-        final Point readPoint = pointsMapper.find(testPoint.getId());
+        final PointEntity readPoint = pointsMapper.find(testPoint.getId());
+
         Assert.assertNotNull(readPoint);
+        Assert.assertEquals(testId, readPoint.getId());
+        Assert.assertEquals(testType, readPoint.getType());
+        Assert.assertEquals(testText, readPoint.getText());
+        Assert.assertEquals(testQuizId, readPoint.getQuizId());
+
     }
 
     @Test
     public void testUpdate() throws Exception {
-        final PointInterface testPoint = this.pointsMapper.find(2);
-        testPoint.setQuizId(4);
+
+        final Integer testId = 2;
+        final String testType = "Test type";
+        final String testText = "Test text";
+        final Integer testQuizId = 1;
+
+        PointEntity testPoint = this.pointsMapper.find(testId);
+
+        Assert.assertNotEquals(testType, testPoint.getType());
+        Assert.assertNotEquals(testText, testPoint.getType());
+        Assert.assertNotEquals(testQuizId, testPoint.getQuizId());
+
+        testPoint = Mockito.mock(PointEntity.class);
+
+        Mockito.when(testPoint.getId()).thenReturn(testId);
+        Mockito.when(testPoint.getType()).thenReturn(testType);
+        Mockito.when(testPoint.getText()).thenReturn(testText);
+        Mockito.when(testPoint.getQuizId()).thenReturn(testQuizId);
+
         this.pointsMapper.update(testPoint);
 
-        final Point checkUpdatePoint = pointsMapper.find(testPoint.getId());
-        Assert.assertEquals((Integer) 4, checkUpdatePoint.getQuizId());
+        final PointEntity checkUpdatePoint = pointsMapper.find(testId);
+
+        Assert.assertEquals(testType, checkUpdatePoint.getType());
+        Assert.assertEquals(testText, checkUpdatePoint.getText());
+        Assert.assertEquals(testQuizId, checkUpdatePoint.getQuizId());
+
     }
 
     @Test
     public void testDelete() throws Exception {
-        PointInterface testPoint = this.pointsMapper.find(1);
+
+        PointEntity testPoint = this.pointsMapper.find(1);
         Assert.assertNotNull(testPoint);
 
         this.pointsMapper.delete(testPoint);
+
         testPoint = this.pointsMapper.find(1);
         Assert.assertNull(testPoint);
+
     }
 
 }
