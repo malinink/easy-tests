c65508dabffa559ee17bf52d5c1d4e98f206164f
Loriens <lerd.95@mail.ru>
2017-05-11 23:01:28 +0300

    Fill ServiceInterface

--- src/main/java/easytests/services/ServiceInterface.java
+++ src/main/java/easytests/services/ServiceInterface.java
@@ -1,8 +1,31 @@
 package easytests.services;
 
 
+import easytests.entities.SolutionEntity;
+import easytests.models.PointModelInterface;
+import easytests.models.SolutionModelInterface;
+
+import java.util.List;
+
 /**
  * @author malinink
  */
 public interface ServiceInterface {
+    List<SolutionModelInterface> findAll();
+
+    SolutionModelInterface find(Integer id);
+
+    List<SolutionModelInterface> findByPoint(PointModelInterface point);
+
+    void save(SolutionModelInterface solutionModel);
+
+    void save(List<SolutionModelInterface> solutionModels);
+
+    void delete(SolutionModelInterface solutionModel);
+
+    SolutionModelInterface map(SolutionEntity solutionEntity);
+
+    SolutionEntity map(SolutionModelInterface solutionModel);
+
+    List<SolutionModelInterface> map(List<SolutionEntity> solutionEntities);
 }
