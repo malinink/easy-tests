a311b824fd92e374e72d558637d133f3131988ef
Loriens <lerd.95@mail.ru>
2017-05-12 20:02:49 +0300

    Small fix

--- src/main/java/easytests/services/PointsServiceInterface.java
+++ src/main/java/easytests/services/PointsServiceInterface.java
@@ -9,6 +9,6 @@ import java.util.List;
  */
 public interface PointsServiceInterface extends ServiceInterface {
     List<PointModelInterface> findAll();
-    
+
     PointModelInterface find(Integer id);
 }
