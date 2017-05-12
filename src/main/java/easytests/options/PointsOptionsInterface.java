b8088ebce4022000c931b4cb6bf4c130cac5ebff
Loriens <lerd.95@mail.ru>
2017-05-12 20:53:48 +0300

    Update PointsOptionsInterface

--- src/main/java/easytests/options/PointsOptionsInterface.java
+++ src/main/java/easytests/options/PointsOptionsInterface.java
@@ -4,12 +4,15 @@ import easytests.models.PointModelInterface;
 import easytests.services.AnswersServiceInterface;
 import easytests.services.PointsServiceInterface;
 import easytests.services.QuizzesServiceInterface;
+import easytests.services.SolutionsServiceInterface;
 
 /**
  * @author loriens
  */
 public interface PointsOptionsInterface {
     void setPointsService(PointsServiceInterface pointsService);
+
+    void setSolutionsService(SolutionsServiceInterface solutionsService);
 
     void setQuizzesService(QuizzesServiceInterface quizzesService);
 
