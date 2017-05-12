73a4b4189005872845cee74b3662a978a727a7f4
rezenbekk <rezenbekk@gmail.com>
2017-05-12 22:58:08 +0300

    Add serialNumber to Mapper

--- src/main/java/easytests/mappers/AnswersMapper.java
+++ src/main/java/easytests/mappers/AnswersMapper.java
@@ -16,24 +16,28 @@ public interface AnswersMapper {
                     @Result(property = "id", column = "id"),
                     @Result(property = "txt", column = "txt"),
                     @Result(property = "questionId", column = "question_id"),
+                    @Result(property = "serialNumber", column = "serial_number"),
                     @Result(property = "right", column = "is_right")
             })
-    @Select("SELECT id, txt, question_id, is_right FROM answers")
+    @Select("SELECT id, txt, question_id, serial_number, is_right FROM answers")
     List<AnswerEntity> findAll();
 
-    @Select("SELECT id, txt, question_id, is_right FROM answers where id=#{id}")
+    @Select("SELECT id, txt, question_id, serial_number, is_right FROM answers where id=#{id}")
     @ResultMap("AnswerEntity")
     AnswerEntity find(Integer id);
 
-    @Select("SELECT id, txt, question_id, is_right FROM answers WHERE question_id=#{questionId}")
+    @Select("SELECT id, txt, question_id, serial_number, is_right FROM answers WHERE"
+            + " question_id=#{questionId}")
     @ResultMap("AnswerEntity")
     List<AnswerEntity> findByQuestionId(Integer id);
 
-    @Insert("INSERT INTO answers (txt, question_id, is_right) VALUES(#{txt}, #{questionId}, #{right})")
+    @Insert("INSERT INTO answers (txt, question_id, serial_number is_right) VALUES(#{txt},"
+            + " #{questionId}, #{serialNumber}, #{right})")
     @Options(useGeneratedKeys = true, keyColumn = "id")
     void insert(AnswerEntity answer);
 
-    @Update("UPDATE answers SET txt=#{txt}, question_id=#{questionId}, is_right=#{right} WHERE id=#{id}")
+    @Update("UPDATE answers SET txt=#{txt}, question_id=#{questionId}, serial_number=#{serialNumber},"
+            + " is_right=#{right} WHERE id=#{id}")
     void update(AnswerEntity answer);
 
     @Delete("DELETE FROM answers WHERE id=#{id}")
