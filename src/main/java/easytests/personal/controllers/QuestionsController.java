eb09b389d4d67089668eff901ca4e9ccdfcc8c49
Firkhraag <firkhraag@gmail.com>
2017-05-12 11:35:43 +0300

    Fix Questions Controller

--- src/main/java/easytests/personal/controllers/QuestionsController.java
+++ src/main/java/easytests/personal/controllers/QuestionsController.java
@@ -15,12 +15,12 @@ import easytests.services.QuestionTypesService;
 import easytests.services.QuestionsService;
 import easytests.services.TopicsService;
 import java.util.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import org.springframework.web.bind.annotation.*;
 
 /**
