a382855634bb0784f95ce935663041d221fd97c2
Loriens <lerd.95@mail.ru>
2017-05-12 20:03:47 +0300

    Finished SolutionsOptions

--- src/main/java/easytests/options/SolutionsOptions.java
+++ src/main/java/easytests/options/SolutionsOptions.java
@@ -1,5 +1,8 @@
 package easytests.options;
 
+import easytests.models.PointModel;
+import easytests.models.SolutionModelInterface;
+import easytests.services.PointsServiceInterface;
 import easytests.services.SolutionsServiceInterface;
 import lombok.EqualsAndHashCode;
 import lombok.Setter;
@@ -12,13 +15,42 @@ public class SolutionsOptions implements SolutionsOptionsInterface {
     @Setter
     private SolutionsServiceInterface solutionsService;
 
-/*    @Setter
+    @Setter
     private PointsServiceInterface pointsService;
 
     private PointsOptionsInterface pointsOptions;
 
-    public SoltionsOptionsInterface withPoint(PointsOptionsInterface pointsOptions) {
+    public SolutionsOptionsInterface withPoints(PointsOptionsInterface pointsOptions) {
         this.pointsOptions = pointsOptions;
         return this;
-    }*/
+    }
+
+    public SolutionsOptionsInterface withRelations(SolutionModelInterface solutionModel) {
+        if (this.pointsOptions != null) {
+            solutionModel.setPoint(this.pointsService.find(
+                    solutionModel.getPoint().getId(), this.pointsOptions));
+        }
+
+        return solutionModel;
+    }
+
+
+    public void saveWithRelations(SolutionModelInterface solutionModel) {
+        if (this.pointsOptions != null) {
+            this.pointsOptions.withSolutions(this);
+            this.pointsOptions.saveWithRelations(solutionModel.getPoint());
+            return;
+        }
+    }
+
+
+    public void deleteWithRelations(SolutionModelInterface solutionModel) {
+        if (this.pointsOptions != null) {
+            this.pointsOptions.withSolutions(this);
+            this.pointsOptions.deleteWithRelations(solutionModel.getPoint());
+            return;
+        }
+
+        this.solutionsService.delete(solutionModel);
+    }
 }
