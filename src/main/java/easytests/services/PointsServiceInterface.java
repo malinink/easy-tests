8970775bd3b9d40cf340571911ca850ed5ece5e0
Loriens <lerd.95@mail.ru>
2017-05-12 20:41:39 +0300

    Add delete() to PointsServiceInterface

--- src/main/java/easytests/services/PointsServiceInterface.java
+++ src/main/java/easytests/services/PointsServiceInterface.java
@@ -11,4 +11,6 @@ public interface PointsServiceInterface extends ServiceInterface {
     List<PointModelInterface> findAll();
 
     PointModelInterface find(Integer id);
+
+    void delete(PointModelInterface pointModel);
 }
