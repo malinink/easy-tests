fd8c59184805b1370bd7362873348b68727bcae5
Loriens <lerd.95@mail.ru>
2017-05-12 19:35:10 +0300

    Add SolutionsOptions to SolutionsService

--- src/main/java/easytests/services/SolutionsService.java
+++ src/main/java/easytests/services/SolutionsService.java
@@ -22,10 +22,17 @@ public class SolutionsService {
     @Autowired
     private SolutionsMapper solutionsMapper;
 
+    @Override
     public List<SolutionModelInterface> findAll() {
         return this.map(this.solutionsMapper.findAll());
     }
 
+    @Override
+    public List<SolutionModelInterface> findAll(SolutionsOptionsInterface solutionsOptions) {
+        return this.withServices(solutionsOptions).withRelations(this.findAll());
+    }
+
+    @Override
     public SolutionModelInterface find(Integer id) {
         final SolutionEntity solutionEntity = this.solutionsMapper.find(id);
         if (solutionEntity == null) {
@@ -34,10 +41,16 @@ public class SolutionsService {
         return this.map(solutionEntity);
     }
 
+    @Override
+    public SolutionModelInterface find(Integer id, SolutionsOptionsInterface solutionsOptions) {
+        return this.withServices(solutionsOptions).withRelations(this.find(id));
+    }
+
     public List<SolutionModelInterface> findByPoint(PointModelInterface point) {
         return this.map(this.solutionsMapper.findByPointId(point.getId()));
     }
 
+    @Override
     public void save(SolutionModelInterface solutionModel) {
         final SolutionEntity solutionEntity = this.map(solutionModel);
         if (solutionEntity.getId() != null) {
@@ -48,18 +61,37 @@ public class SolutionsService {
         solutionModel.setId(solutionEntity.getId());
     }
 
+    @Override
+    public SolutionModelInterface save(SolutionModelInterface solutionModel, SolutionsOptionsInterface solutionsOptions) {
+        return this.withServices(solutionsOptions).saveWithRelations(solutionModel);
+    }
+
+    @Override
     public void save(List<SolutionModelInterface> solutionModels) {
         for (SolutionModelInterface solutionModel: solutionModels) {
             this.save(solutionModel);
         }
     }
 
+    @Override
+    public void save(List<SolutionModelInterface> solutionModels, SolutionsOptionsInterface solutionsOptions) {
+        for (SolutionModelInterface solutionModel: solutionModels) {
+            this.save(solutionModel, solutionsOptions);
+        }
+    }
+
+    @Override
     public void delete(SolutionModelInterface solutionModel) {
         final SolutionEntity solutionEntity = this.map(solutionModel);
         if (solutionEntity.getId() == null) {
             throw new DeleteUnidentifiedModelException();
         }
         this.solutionsMapper.delete(solutionEntity);
+    }
+
+    @Override
+    public void delete(SolutionModelInterface solutionModel, SolutionsOptionsInterface solutionsOptions) {
+        this.withServices(solutionsOptions).deleteWithRelations(solutionsOptions);
     }
 
     private SolutionModelInterface map(SolutionEntity solutionEntity) {
@@ -82,7 +114,7 @@ public class SolutionsService {
         return solutionModels;
     }
 
-    private SolutionsOptionsInterface withService(SolutionsOptionsInterface solutionOptions) {
+    private SolutionsOptionsInterface withServices(SolutionsOptionsInterface solutionOptions) {
         //solutionOptions.setSolutionsService(this);
         return solutionOptions;
     }
