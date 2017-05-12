ff24b611b69dbc319f68dd83fff40a8770458bb1
Firkhraag <firkhraag@gmail.com>
2017-05-12 10:27:31 +0300

    Add whitespace

--- src/main/java/easytests/options/builder/TopicsOptionsBuilder.java
+++ src/main/java/easytests/options/builder/TopicsOptionsBuilder.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
  * @author firkhraag
  */
 @Service
-public class TopicsOptionsBuilder{
+public class TopicsOptionsBuilder {
 
     public TopicsOptionsInterface forDelete() {
         return new TopicsOptions();
