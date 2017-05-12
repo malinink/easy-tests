b7e989b5ac16cdc6d91da810407176f532e9ca21
Loriens <lerd.95@mail.ru>
2017-05-12 19:53:41 +0300

    Add PointsServiceInterface

---
+++ src/main/java/easytests/services/PointsServiceInterface.java
@@ -0,0 +1,14 @@
+package easytests.services;
+
+import easytests.models.PointModelInterface;
+
+import java.util.List;
+
+/**
+ * @author loriens
+ */
+public interface PointsServiceInterface extends ServiceInterface {
+    List<PointModelInterface> findAll();
+    
+    PointModelInterface find(Integer id);
+}
