5bcfd9e1b2e0f8d320b26a13d6222637cda6b2f2
tapochek97 <tapochek97@ya.ru>
2017-05-15 22:08:00 +0300

    Add PointsOptionsInterface

---
+++ src/main/java/easytests/options/PointsOptionsInterface.java
@@ -0,0 +1,35 @@
+package easytests.options;
+
+import easytests.models.PointModelInterface;
+import easytests.services.QuestionsServiceInterface;
+import easytests.services.QuizzesServiceInterface;
+import easytests.services.SolutionsServiceInterface;
+
+import java.util.List;
+
+/**
+ * @author nikitalpopov
+ */
+public interface PointsOptionsInterface extends OptionsInterface {
+
+    void setQuizzesService(QuizzesServiceInterface quizzesService);
+
+    void setQuestionsService(QuestionsServiceInterface questionsService);
+
+    void setSolutionsService(SolutionsServiceInterface solutionsService);
+
+    PointsOptionsInterface withQuiz(QuizzesOptionsInterface quizzesOptions);
+
+    PointsOptionsInterface withQuestion(QuestionsOptionsInterface questionsOptions);
+
+    PointsOptionsInterface withSolutions(SolutionsOptionsInterface solutionsOptions);
+
+    PointModelInterface withRelations(PointModelInterface pointModel);
+
+    List<PointModelInterface> withRelations(List<PointModelInterface> pointModels);
+
+    void saveWithRelations(PointModelInterface pointModel);
+
+    void deleteWithRelations(PointModelInterface pointModel);
+
+}
