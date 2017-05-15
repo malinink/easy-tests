c6b20d352f63eedaa7681f5f411f0d04790887e1
FortyWays <zedront307@gmail.com>
2017-05-15 20:08:07 +0300

    Changed IssueOptions. Added link with subject

--- src/main/java/easytests/options/IssuesOptions.java
+++ src/main/java/easytests/options/IssuesOptions.java
@@ -3,6 +3,7 @@ package easytests.options;
 import easytests.models.IssueModelInterface;
 import easytests.services.IssuesServiceInterface;
 import easytests.services.QuizzesServiceInterface;
+import easytests.services.SubjectsServiceInterface;
 import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Setter;
@@ -20,6 +21,11 @@ public class IssuesOptions implements IssuesOptionsInterface {
     @Setter
     private QuizzesServiceInterface quizzesService;
 
+    @Setter
+    private SubjectsServiceInterface subjectsService;
+
+    private SubjectsOptionsInterface subjectsOptions;
+
     private QuizzesOptionsInterface quizzesOptions;
 
     @Override
@@ -35,6 +41,9 @@ public class IssuesOptions implements IssuesOptionsInterface {
         }
         if (this.quizzesOptions != null) {
             issueModel.setQuizzes(this.quizzesService.findByIssue(issueModel, this.quizzesOptions));
+        }
+        if (this.subjectsOptions != null) {
+            issueModel.setSubject(this.subjectsService.find(issueModel.getSubject().getId(), this.subjectsOptions));
         }
         return issueModel;
     }
@@ -53,6 +62,9 @@ public class IssuesOptions implements IssuesOptionsInterface {
         if (this.quizzesOptions != null) {
             this.quizzesService.save(issueModel.getQuizzes(), this.quizzesOptions);
         }
+        if (this.subjectsOptions != null) {
+            this.subjectsService.save(issueModel.getSubject(), this.subjectsOptions);
+        }
     }
 
     @Override
@@ -60,6 +72,17 @@ public class IssuesOptions implements IssuesOptionsInterface {
         if (this.quizzesOptions != null) {
             this.quizzesService.delete(issueModel.getQuizzes(), this.quizzesOptions);
         }
+        if (this.subjectsOptions != null) {
+            this.subjectsService.delete(issueModel.getSubject(), this.subjectsOptions);
+        }
+
         this.issuesService.delete(issueModel);
     }
+
+    @Override
+    public IssuesOptionsInterface withSubject(SubjectsOptionsInterface subjectsOptions) {
+        this.subjectsOptions = subjectsOptions;
+        return this;
+    }
+
 }
