c37b18b9a8be947d03c05611ddfb0140316b5315
Loriens <lerd.95@mail.ru>
2017-05-15 17:02:14 +0300

    Delete getId in PointModelInterface

--- src/main/java/easytests/models/PointModelInterface.java
+++ src/main/java/easytests/models/PointModelInterface.java
@@ -11,8 +11,6 @@ public interface PointModelInterface extends ModelInterface {
 
     void setId(Integer id);
 
-    Integer getId();
-
     QuestionModelInterface getQuestion();
 
     void setQuestion(QuestionModelInterface question);
