d4abce67ef0f11795bb30a5e0108fd7286a53309
Firkhraag <firkhraag@gmail.com>
2017-05-16 20:08:36 +0300

    Add QuestionModelDtoValidator

---
+++ src/main/java/easytests/personal/validators/QuestionModelDtoValidator.java
@@ -0,0 +1,37 @@
+package easytests.personal.validators;
+
+import easytests.personal.dto.QuestionModelDto;
+import easytests.common.validators.AbstractDtoValidator;
+import easytests.services.QuestionTypesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+
+
+/**
+ * @author firkhraag
+ */
+@Service
+public class QuestionModelDtoValidator extends AbstractDtoValidator {
+
+    @Autowired
+    private QuestionTypesService questionTypesService;
+
+    public boolean supports(Class candidate) {
+        return QuestionModelDto.class.isAssignableFrom(candidate);
+    }
+
+    public void validate(Object obj, Errors errors) {
+        final QuestionModelDto questionModelDto = (QuestionModelDto) obj;
+        this.validateQuestionTypes(errors, questionModelDto);
+    }
+
+    private void validateQuestionTypes(Errors errors, QuestionModelDto questionModelDto) {
+        final Integer questionTypeId = questionModelDto.getQuestionTypeId();
+        final Integer size = questionTypesService.findAll().size();
+        if (questionTypeId == null)
+            reject(errors, "questionTypeId", "Select question type");
+        else if (!((questionTypeId >= 1) && (questionTypeId <= size)))
+            reject(errors, "questionTypeId", "Wrong question type");
+    }
+}
