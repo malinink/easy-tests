95e438e312433b18b2c0cc8960143024ecf5b273
rezenbekk <rezenbekk@gmail.com>
2017-05-16 11:40:49 +0300

    Codestyle fix at migration

--- src/main/resources/migrations/scripts/20170512202842_add_serial_number_to_Answer.sql
+++ src/main/resources/migrations/scripts/20170512202842_add_serial_number_to_Answer.sql
@@ -1,21 +1,4 @@
---
---    Copyright 2010-2016 the original author or authors.
---
---    Licensed under the Apache License, Version 2.0 (the "License");
---    you may not use this file except in compliance with the License.
---    You may obtain a copy of the License at
---
---       http://www.apache.org/licenses/LICENSE-2.0
---
---    Unless required by applicable law or agreed to in writing, software
---    distributed under the License is distributed on an "AS IS" BASIS,
---    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
---    See the License for the specific language governing permissions and
---    limitations under the License.
---
-
 -- // add_serial_number_to_Answer
--- Migration SQL that makes the change goes here.
 
 ALTER TABLE answers
   ADD COLUMN serial_number INTEGER NOT NULL DEFAULT 0,
@@ -25,5 +8,4 @@ ALTER TABLE answers
 -- SQL to undo the change goes here.
 
 ALTER TABLE answers
-  DROP CONSTRAINT CHK_serial_number,
   DROP COLUMN serial_number;
