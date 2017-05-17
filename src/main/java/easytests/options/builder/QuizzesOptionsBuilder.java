32e7b835aa2e262b3cb8808b448178cc8a734b96
tapochek97 <tapochek97@ya.ru>
2017-05-18 01:19:34 +0300

    Add empty QuizzesOptionsBuilder

---
+++ src/main/java/easytests/options/builder/QuizzesOptionsBuilder.java
@@ -0,0 +1,24 @@
+package easytests.options.builder;
+
+import easytests.options.QuizzesOptionsInterface;
+
+/**
+ * @author nikitalpopov
+ */
+public class QuizzesOptionsBuilder implements QuizzesOptionsBuilderInterface {
+
+    @Override
+    public QuizzesOptionsInterface forDelete() {
+
+        return null;
+
+    }
+
+    @Override
+    public QuizzesOptionsInterface forAuth() {
+
+        return null;
+
+    }
+
+}
