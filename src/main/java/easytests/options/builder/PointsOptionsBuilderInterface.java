e42130bcc7010f5a4ef08746355c9de2b56778c1
tapochek97 <tapochek97@ya.ru>
2017-05-17 18:27:18 +0300

    Add PointsOptionsBuilderInterface

---
+++ src/main/java/easytests/options/builder/PointsOptionsBuilderInterface.java
@@ -0,0 +1,12 @@
+package easytests.options.builder;
+
+/**
+ * @author nikitalpopov
+ */
+public interface PointsOptionsBuilderInterface extends OptionsBuilderInterface {
+
+    PointsOptionsInterface forDelete();
+
+    PointsOptionsInterface forAuth();
+
+}
