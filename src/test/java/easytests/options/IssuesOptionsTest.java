7a9ba5942d822d1399833f8d036b9baf198229ed
FortyWays <zedront307@gmail.com>
2017-05-17 23:16:25 +0300

    Updated IssuesOptions test

--- src/test/java/easytests/options/IssuesOptionsTest.java
+++ src/test/java/easytests/options/IssuesOptionsTest.java
@@ -2,8 +2,11 @@ package easytests.options;
 
 import easytests.models.IssueModelInterface;
 import easytests.models.QuizModelInterface;
+import easytests.models.SubjectModelInterface;
+import easytests.models.empty.SubjectModelEmpty;
 import easytests.services.IssuesServiceInterface;
 import easytests.services.QuizzesServiceInterface;
+import easytests.services.SubjectsServiceInterface;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.mockito.BDDMockito.given;
 import java.util.ArrayList;
 import java.util.List;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 /**
  * @author fortyways
@@ -23,19 +27,37 @@ import static org.mockito.Mockito.verify;
 public class IssuesOptionsTest {
     @Test
     public void testWithRelationsOnSingleModel() throws Exception {
+
         final IssueModelInterface issueModel = Mockito.mock(IssueModelInterface.class);
+
+        issueModel.setId(1);
+        given(issueModel.getSubject()).willReturn(new SubjectModelEmpty(1));
+
         final IssuesOptionsInterface issuesOptions = new IssuesOptions();
+
+        final SubjectsServiceInterface subjectsService = Mockito.mock(SubjectsServiceInterface.class);
+        final SubjectsOptionsInterface subjectsOptions = Mockito.mock(SubjectsOptionsInterface.class);
         final QuizzesServiceInterface quizzesService = Mockito.mock(QuizzesServiceInterface.class);
         final QuizzesOptionsInterface quizzesOptions = Mockito.mock(QuizzesOptionsInterface.class);
+
+        issuesOptions.setSubjectsService(subjectsService);
         issuesOptions.setQuizzesService(quizzesService);
+
+        issuesOptions.withSubject(subjectsOptions);
         issuesOptions.withQuizzes(quizzesOptions);
+
+        final SubjectModelInterface subjectModel = Mockito.mock(SubjectModelInterface.class);
         final List<QuizModelInterface> quizzesModels = new ArrayList<>();
         quizzesModels.add(Mockito.mock(QuizModelInterface.class));
+
+        given(subjectsService.find(issueModel.getSubject().getId(), subjectsOptions)).willReturn(subjectModel);
         given(quizzesService.findByIssue(issueModel, quizzesOptions)).willReturn(quizzesModels);
 
         final IssueModelInterface issueModelWithRelations = issuesOptions.withRelations(issueModel);
 
         Assert.assertEquals(issueModel, issueModelWithRelations);
+
+        verify(subjectsService).find(1, subjectsOptions);
         verify(quizzesService).findByIssue(issueModel, quizzesOptions);
         verify(issueModel).setQuizzes(quizzesModels);
         verify(issueModel).setQuizzes(Mockito.anyList());
@@ -45,9 +67,15 @@ public class IssuesOptionsTest {
     public void testWithRelationsOnNull() throws Exception {
         final IssueModelInterface issueModel = null;
         final IssuesOptionsInterface issuesOptions = new IssuesOptions();
+
+        final SubjectsServiceInterface subjectsService = Mockito.mock(SubjectsServiceInterface.class);
+        final SubjectsOptionsInterface subjectsOptions = Mockito.mock(SubjectsOptionsInterface.class);
         final QuizzesServiceInterface quizzesService = Mockito.mock(QuizzesServiceInterface.class);
         final QuizzesOptionsInterface quizzesOptions = Mockito.mock(QuizzesOptionsInterface.class);
+
+        issuesOptions.setSubjectsService(subjectsService);
         issuesOptions.setQuizzesService(quizzesService);
+        issuesOptions.withSubject(subjectsOptions);
         issuesOptions.withQuizzes(quizzesOptions);
 
         final IssueModelInterface issueModelWithRelations = issuesOptions.withRelations(issueModel);
@@ -59,43 +87,69 @@ public class IssuesOptionsTest {
     public void testWithRelationsOnModelsList() throws Exception {
         final IssueModelInterface issueModelFirst = Mockito.mock(IssueModelInterface.class);
         issueModelFirst.setId(1);
+        given(issueModelFirst.getSubject()).willReturn(new SubjectModelEmpty(1));
         final IssueModelInterface issueModelSecond = Mockito.mock(IssueModelInterface.class);
         issueModelSecond.setId(2);
+        given(issueModelSecond.getSubject()).willReturn(new SubjectModelEmpty(2));
         final List<IssueModelInterface> issuesModels = new ArrayList<>(2);
         issuesModels.add(issueModelFirst);
         issuesModels.add(issueModelSecond);
 
         final IssuesOptionsInterface issuesOptions = new IssuesOptions();
+
         final QuizzesServiceInterface quizzesService = Mockito.mock(QuizzesServiceInterface.class);
         final QuizzesOptionsInterface quizzesOptions = Mockito.mock(QuizzesOptionsInterface.class);
+
+        final SubjectsServiceInterface subjectsService = Mockito.mock(SubjectsServiceInterface.class);
+        final SubjectsOptionsInterface subjectsOptions = Mockito.mock(SubjectsOptionsInterface.class);
+
+        issuesOptions.setSubjectsService(subjectsService);
         issuesOptions.setQuizzesService(quizzesService);
+        issuesOptions.withSubject(subjectsOptions);
         issuesOptions.withQuizzes(quizzesOptions);
+
         final List<QuizModelInterface> quizzesModelsFirst = new ArrayList<>();
         quizzesModelsFirst.add(Mockito.mock(QuizModelInterface.class));
+
         final List<QuizModelInterface> quizzesModelsSecond = new ArrayList<>();
         quizzesModelsSecond.add(Mockito.mock(QuizModelInterface.class));
         quizzesModelsSecond.add(Mockito.mock(QuizModelInterface.class));
+
         given(quizzesService.findByIssue(issueModelFirst, quizzesOptions)).willReturn(quizzesModelsFirst);
         given(quizzesService.findByIssue(issueModelSecond, quizzesOptions)).willReturn(quizzesModelsSecond);
+
+        final SubjectModelInterface subjectModelFirst = Mockito.mock(SubjectModelInterface.class);
+        final SubjectModelInterface subjectModelSecond = Mockito.mock(SubjectModelInterface.class);
+
+        given(subjectsService.find(1, subjectsOptions)).willReturn(subjectModelFirst);
+        given(subjectsService.find(2, subjectsOptions)).willReturn(subjectModelSecond);
 
         final List<IssueModelInterface> issuesModelsWithRelations = issuesOptions.withRelations(issuesModels);
 
         Assert.assertEquals(issuesModelsWithRelations, issuesModels);
         verify(quizzesService).findByIssue(issueModelFirst, quizzesOptions);
+        verify(subjectsService).find(1,subjectsOptions);
         verify(issuesModels.get(0)).setQuizzes(quizzesModelsFirst);
         verify(issuesModels.get(0)).setQuizzes(Mockito.anyList());
+        verify(issuesModels.get(0)).setSubject(subjectModelFirst);
+
         verify(quizzesService).findByIssue(issueModelSecond, quizzesOptions);
+        verify(subjectsService).find(2,subjectsOptions);
         verify(issuesModels.get(1)).setQuizzes(quizzesModelsSecond);
         verify(issuesModels.get(1)).setQuizzes(Mockito.anyList());
+        verify(issuesModels.get(1)).setSubject(subjectModelSecond);
     }
 
     @Test
     public void testSaveWithRelations() throws Exception {
         final IssueModelInterface issueModel = Mockito.mock(IssueModelInterface.class);
+
         final IssuesOptionsInterface issuesOptions = new IssuesOptions();
         final IssuesServiceInterface issuesService = Mockito.mock(IssuesServiceInterface.class);
+
         final QuizzesServiceInterface quizzesService = Mockito.mock(QuizzesServiceInterface.class);
         final QuizzesOptionsInterface quizzesOptions = Mockito.mock(QuizzesOptionsInterface.class);
+
         issuesOptions.setIssuesService(issuesService);
         issuesOptions.setQuizzesService(quizzesService);
         issuesOptions.withQuizzes(quizzesOptions);
@@ -129,5 +183,31 @@ public class IssuesOptionsTest {
 
         inOrder.verify(quizzesService).delete(quizzesModels, quizzesOptions);
         inOrder.verify(issuesService).delete(issueModel);
+    }
+
+    @Test
+    public void testSaveDeleteWithSubject(){
+        final IssuesOptionsInterface issuesOptions = new IssuesOptions();
+
+        final SubjectsOptionsInterface subjectsOptions = Mockito.mock(SubjectsOptionsInterface.class);
+
+        final SubjectsServiceInterface subjectsService = Mockito.mock(SubjectsServiceInterface.class);
+
+        issuesOptions.setSubjectsService(subjectsService);
+        issuesOptions.withSubject(subjectsOptions);
+
+        final IssueModelInterface issueModel = Mockito.mock(IssueModelInterface.class);
+
+        final IssuesOptionsInterface issuesOptionsSpy = Mockito.spy(issuesOptions);
+
+        issuesOptionsSpy.saveWithRelations(issueModel);
+
+        verify(subjectsOptions, times(1)).saveWithRelations(issueModel.getSubject());
+
+        issuesOptionsSpy.deleteWithRelations(issueModel);
+
+        verify(subjectsOptions, times(1)).deleteWithRelations(issueModel.getSubject());
+
+
     }
 }
