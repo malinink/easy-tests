301e2c002e2756f9687b383beac7d89213f5267c
tapochek97 <tapochek97@ya.ru>
2017-05-18 01:20:04 +0300

    Add empty SolutionsOptionsBuilder

---
+++ src/main/java/easytests/options/builder/SolutionsOptionsBuilder.java
@@ -0,0 +1,24 @@
+package easytests.options.builder;
+
+import easytests.options.SolutionsOptionsInterface;
+
+/**
+ * @author nikitalpopov
+ */
+public class SolutionsOptionsBuilder implements SolutionsOptionsBuilderInterface {
+
+    @Override
+    public SolutionsOptionsInterface forDelete() {
+
+        return null;
+
+    }
+
+    @Override
+    public SolutionsOptionsInterface forAuth() {
+
+        return null;
+
+    }
+
+}
