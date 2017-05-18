79fde8417c65d2825486c7da9fa579a47423d9f0
Loriens <lerd.95@mail.ru>
2017-05-18 22:47:38 +0300

    Fix SolutionsOptions

--- src/main/java/easytests/options/SolutionsOptions.java
+++ src/main/java/easytests/options/SolutionsOptions.java
@@ -1,6 +1,7 @@
 package easytests.options;
 
 import easytests.models.SolutionModelInterface;
+import easytests.services.AnswersServiceInterface;
 import easytests.services.PointsServiceInterface;
 import easytests.services.SolutionsServiceInterface;
 import groovy.transform.EqualsAndHashCode;
@@ -18,10 +19,20 @@ public class SolutionsOptions implements SolutionsOptionsInterface {
     @Setter
     private PointsServiceInterface pointsService;
 
+    @Setter
+    private AnswersServiceInterface answersService;
+
     private PointsOptionsInterface pointsOptions;
+
+    private AnswersOptionsInterface answersOptions;
 
     public SolutionsOptionsInterface withPoint(PointsOptionsInterface pointsOptions) {
         this.pointsOptions = pointsOptions;
+        return this;
+    }
+
+    public SolutionsOptionsInterface withAnswer(AnswersOptionsInterface answersOptions) {
+        this.answersOptions = answersOptions;
         return this;
     }
 
@@ -33,6 +44,10 @@ public class SolutionsOptions implements SolutionsOptionsInterface {
 
         if (this.pointsOptions != null) {
             solutionModel.setPoint(this.pointsService.find(solutionModel.getPoint().getId(), this.pointsOptions));
+        }
+
+        if (this.answersOptions != null) {
+            solutionModel.setAnswer(this.answersService.find(solutionModel.getAnswer().getId(), this.answersOptions));
         }
 
         return solutionModel;
@@ -54,12 +69,21 @@ public class SolutionsOptions implements SolutionsOptionsInterface {
         if (this.pointsOptions != null) {
             this.pointsService.save(solutionModel.getPoint(), this.pointsOptions);
         }
+
+        if (this.answersOptions != null) {
+            this.answersService.save(solutionModel.getAnswer(), this.answersOptions);
+        }
     }
 
     public void deleteWithRelations(SolutionModelInterface solutionModel) {
         if (this.pointsOptions != null) {
             this.pointsService.delete(solutionModel.getPoint(), this.pointsOptions);
-            this.pointsOptions.deleteWithRelations(solutionModel.getPoint(), pointsOptions);
+            this.pointsOptions.deleteWithRelations(solutionModel.getPoint());
+        }
+
+        if (this.answersOptions != null) {
+            this.answersService.delete(solutionModel.getAnswer(), this.answersOptions);
+            this.answersOptions.deleteWithRelations(solutionModel.getAnswer());
         }
 
         this.solutionsService.delete(solutionModel);
