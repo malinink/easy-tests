17637978ec8f9a888ed343e521ddad7933c2bf7a
Firkhraag <firkhraag@gmail.com>
2017-05-18 20:23:54 +0300

    Update QuestionModelDtoValidator

--- src/main/java/easytests/personal/validators/QuestionModelDtoValidator.java
+++ src/main/java/easytests/personal/validators/QuestionModelDtoValidator.java
@@ -1,11 +1,14 @@
 package easytests.personal.validators;
 
+import easytests.models.QuestionTypeModelInterface;
 import easytests.common.validators.AbstractDtoValidator;
 import easytests.personal.dto.QuestionModelDto;
 import easytests.services.QuestionTypesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -29,10 +32,14 @@ public class QuestionModelDtoValidator extends AbstractDtoValidator {
     private void validateQuestionTypes(Errors errors, QuestionModelDto questionModelDto) {
         final Integer questionTypeId = questionModelDto.getQuestionTypeId();
         final String questionTypeIdField = "questionTypeId";
-        final Integer size = questionTypesService.findAll().size();
+        final List<QuestionTypeModelInterface> questionTypeModels = questionTypesService.findAll();
+        final ArrayList<Integer> questionTypeModelsIds = new ArrayList();
+        for (QuestionTypeModelInterface questionTypeModel : questionTypeModels) {
+            questionTypeModelsIds.add(questionTypeModel.getId());
+        }
         if (questionTypeId == null) {
             reject(errors, questionTypeIdField, "Select question type");
-        } else if (!((questionTypeId >= 1) && (questionTypeId <= size))) {
+        } else if (!questionTypeModelsIds.contains(questionTypeId)) {
             reject(errors, questionTypeIdField, "Wrong question type");
         }
     }
