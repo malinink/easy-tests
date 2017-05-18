fcfd783af4b6128bc0b7e6c717523221246517c1
FortyWays <zedront307@gmail.com>
2017-05-18 11:56:05 +0300

    Added startedAt, finishedAt, codeExpired fields

--- src/main/java/easytests/entities/QuizEntity.java
+++ src/main/java/easytests/entities/QuizEntity.java
@@ -1,6 +1,7 @@
 package easytests.entities;
 
 import easytests.models.QuizModelInterface;
+import java.time.LocalDateTime;
 import lombok.Data;
 
 /**
@@ -15,12 +16,19 @@ public class QuizEntity {
 
     private String inviteCode;
 
+    private boolean codeExpired;
+
+    private LocalDateTime startedAt;
+
+    private LocalDateTime finishedAt;
+
     public void map(QuizModelInterface quizModel) {
 
         this.setId(quizModel.getId());
-
+        this.setCodeExpired(quizModel.isCodeExpired());
+        this.setStartedAt(quizModel.getStartedAt());
+        this.setFinishedAt(quizModel.getFinishedAt());
         this.setInviteCode(quizModel.getInviteCode());
-
         this.setIssueId(quizModel.getIssue().getId());
 
     }
