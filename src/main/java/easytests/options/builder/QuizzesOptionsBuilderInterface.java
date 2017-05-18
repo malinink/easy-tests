6788f2ab78493a22b06ef3b591351cd1a4c6552a
FortyWays <zedront307@gmail.com>
2017-05-19 02:27:15 +0300

    Created QuizzesOptionsBuilderInterface

---
+++ src/main/java/easytests/options/builder/QuizzesOptionsBuilderInterface.java
@@ -0,0 +1,14 @@
+package easytests.options.builder;
+
+import easytests.options.QuizzesOptionsInterface;
+
+/**
+ * @author fortyways
+ */
+public interface QuizzesOptionsBuilderInterface extends OptionsBuilderInterface {
+
+    QuizzesOptionsInterface forAuth();
+
+    QuizzesOptionsInterface forDelete();
+
+}
