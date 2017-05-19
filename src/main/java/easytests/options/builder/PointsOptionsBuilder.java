4ec36dcfb45dd951d7db7a6f6d5c7161f1510200
tapochek97 <tapochek97@ya.ru>
2017-05-19 19:42:47 +0300

    Fix PointsOptionsBuilder

--- src/main/java/easytests/options/builder/PointsOptionsBuilder.java
+++ src/main/java/easytests/options/builder/PointsOptionsBuilder.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PointsOptionsBuilder implements PointsOptionsBuilderInterface {
 
-
     @Autowired
     private QuizzesOptionsBuilder quizzesOptionsBuilder;
 
