3db319ad3d2ac3cec92a9d510bf135db2bce5c75
FortyWays <zedront307@gmail.com>
2017-05-19 11:44:36 +0300

    Created PointsOptions(temp)

---
+++ src/main/java/easytests/options/PointsOptions.java
@@ -0,0 +1,11 @@
+package easytests.options;
+
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author fortyways
+ */
+@EqualsAndHashCode
+public class PointsOptions implements PointsOptionsInterface {
+
+}
