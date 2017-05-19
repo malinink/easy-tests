eb721202dda67afc59e21f58b7f4368981fb3a3a
tapochek97 <tapochek97@ya.ru>
2017-05-19 04:43:49 +0300

    Update SecurityConfig Add BCryptPasswordEncoder

--- src/main/java/easytests/config/SecurityConfig.java
+++ src/main/java/easytests/config/SecurityConfig.java
@@ -3,10 +3,13 @@ package easytests.config;
 import easytests.auth.handlers.AuthenticationFailureHandler;
 import easytests.auth.services.AuthUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 /**
@@ -17,6 +20,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthUsersService authUsersService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
