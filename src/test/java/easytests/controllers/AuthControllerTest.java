bed46096a71746ddfbbe597e3f4a97a7c7115323
FortyWays <zedront307@gmail.com>
2017-05-19 14:21:12 +0300

    Updated AuthControllerTest

--- src/test/java/easytests/controllers/AuthControllerTest.java
+++ src/test/java/easytests/controllers/AuthControllerTest.java
@@ -5,16 +5,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-
 import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 /**
 /**
 @author fortyways
@@ -33,11 +33,29 @@ public class AuthControllerTest {
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
     @Test
-    public void testSignIn() throws Exception{
+    public void testSignInFailed() throws Exception{
+
         mockMvc.perform(get("/auth/sign-in"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("auth/sign-in"))
-                .andExpect(model().attributeExists("login"));
+                .andExpect(model().attributeDoesNotExist("login"));
+
+
+    }
+
+    @Test
+    public void testSignInSuccess() throws Exception{
+
+        MockHttpSession session = new MockHttpSession();
+
+        session.setAttribute("LAST_LOGIN","sup");
+
+        mockMvc.perform(get("/auth/sign-in").session(session))
+
+                .andExpect(status().isOk())
+                .andExpect(view().name("auth/sign-in"))
+                .andExpect(model().attributeExists("login"))
+                .andExpect(model().attribute("login", "sup"));
     }
 
 }
