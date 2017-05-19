56c2537e13e286801f561cad8c93127def6d2377
DoZor-80 <zorikto-d@yandex.ru>
2017-05-19 13:45:45 +0300

    Updated AuthUsersServiceTest  (mock UsersServiceInterface)

--- src/test/java/easytests/auth/services/AuthUsersServiceTest.java
+++ src/test/java/easytests/auth/services/AuthUsersServiceTest.java
@@ -2,9 +2,8 @@ package easytests.auth.services;
 
 import easytests.models.UserModel;
 import easytests.models.UserModelInterface;
-import easytests.services.UsersService;
+import easytests.services.UsersServiceInterface;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -24,17 +23,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestPropertySource(locations = {"classpath:database.test.properties"})
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:sql/mappersTestData.sql")
 public class AuthUsersServiceTest {
-    private String email;
-    private UsersService usersService;
     private UserModelInterface userModel;
 
     @Autowired
     private AuthUsersService authUsersService;
 
-    @Before
-    public void setUp() {
-        usersService = Mockito.mock(UsersService.class);
-        email = "email3@gmail.com";
+    @Test
+    public void testLoadUserByUsername() throws Exception {
+        final UsersServiceInterface usersService = Mockito.mock(UsersServiceInterface.class);
+        final String email = "email3@gmail.com";
         userModel = new UserModel();
         userModel.setId(1);
         userModel.setFirstName("FirstName3");
@@ -44,10 +41,6 @@ public class AuthUsersServiceTest {
         userModel.setPassword("hash3");
         userModel.setIsAdmin(true);
         userModel.setState(3);
-    }
-
-    @Test
-    public void testLoadUserByUsername() throws Exception {
         when(usersService.findByEmail(email)).thenReturn(userModel);
         final UserDetails user = this.authUsersService.loadUserByUsername(email);
         Assert.assertEquals(userModel.getEmail(), user.getUsername());
