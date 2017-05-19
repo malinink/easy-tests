4b1aece5d42304455d2218cd65a54a23d58e6251
FortyWays <zedront307@gmail.com>
2017-05-19 11:43:45 +0300

    Created PointsOptionsBuilder(temp)

---
+++ src/main/java/easytests/options/builder/PointsOptionsBuilder.java
@@ -0,0 +1,20 @@
+package easytests.options.builder;
+
+import easytests.options.PointsOptionsInterface;
+
+/**
+ * @author fortyways
+ */
+public class PointsOptionsBuilder implements PointsOptionsBuilderInterface {
+
+    @Override
+    public PointsOptionsInterface forAuth() {
+        return null;
+    }
+
+    @Override
+    public PointsOptionsInterface forDelete() {
+        return null;
+    }
+
+}
