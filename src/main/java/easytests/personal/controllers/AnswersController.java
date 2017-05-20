c7391aacc1b6487dd28a7d9eabcef9e4a4cd253f
rezenbekk <rezenbekk@gmail.com>
2017-05-20 05:31:48 +0300

    fix AnswersController

--- src/main/java/easytests/personal/controllers/AnswersController.java
+++ src/main/java/easytests/personal/controllers/AnswersController.java
@@ -1,23 +1,26 @@
 package easytests.personal.controllers;
 
 import easytests.common.controllers.AbstractCrudController;
-import easytests.common.exceptions.NotFoundException;
 import easytests.models.*;
+import easytests.options.AnswersOptions;
 import easytests.options.AnswersOptionsInterface;
 import easytests.options.QuestionsOptionsInterface;
 import easytests.options.builder.AnswersOptionsBuilder;
 import easytests.options.builder.QuestionsOptionsBuilder;
 import easytests.personal.dto.AnswerDto;
+import easytests.personal.dto.AnswerListDto;
 import easytests.personal.validators.AnswerDtoValidator;
 import easytests.services.AnswersService;
 import easytests.services.QuestionTypesService;
 import easytests.services.QuestionsService;
 import java.util.*;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -55,14 +58,13 @@ public class AnswersController extends AbstractCrudController {
             @PathVariable("topicId") Integer topicId) {
         final QuestionModelInterface questionModel = getQuestionModel(questionId, false);
         injectQuestionTypeModels(model);
-        final List<AnswerModelInterface> answersList = getAnswerModelList(questionModel);
-        final List<AnswerDto> answerDtoList = this.getAnswerDtoList(answersList);
-        Collections.sort(answerDtoList, Comparator.comparingInt(AnswerDto::getSerialNumber));
+        final AnswerListDto answerDtoList = new AnswerListDto();
+        answerDtoList.map(questionModel);
         setUpdateBehaviour(model);
         model.addAttribute("questionId", questionId);
         model.addAttribute("question", questionModel);
-        model.addAttribute("topicId", topicId);
         model.addAttribute("answerDtoList", answerDtoList);
+        model.addAttribute("topicId", topicId);
         return "answers/form";
     }
 
@@ -70,73 +72,72 @@ public class AnswersController extends AbstractCrudController {
     public String save(
             Model model,
             @PathVariable("questionId") Integer questionId,
-            /*@Valid*/ @ModelAttribute("answerDtoList") ArrayList<AnswerDto> answerDtoList,
+            @Valid AnswerListDto answerDtoList,
             BindingResult bindingResult,
             @PathVariable("topicId") Integer topicId) {
-        System.out.println("DTO LIST SIZE: " + answerDtoList.size());
         final QuestionModelInterface questionModel = this.getQuestionModel(questionId, false);
         final List<AnswerModelInterface> oldAnswerList = questionModel.getAnswers();
-        //TODO: схоронить все полученные ответы, если ответа не получили, он удаляется
-        final List<AnswerModelInterface> answerList = this.getAnswerModelList(answerDtoList);
+        final List<AnswerDto> newAnswerList = new ArrayList<>();
+        if (answerDtoList.getAnswersList() != null) {
+            newAnswerList.addAll(answerDtoList.getAnswersList());
+        }
         for (AnswerDto answerDto
-                : answerDtoList) {
-            System.out.println(answerDto.getTxt());
+                : newAnswerList) {
             this.answerDtoValidator.validate(answerDto, bindingResult);
         }
-        this.answerDtoValidator.validateWithQuestionType(answerDtoList,
+        this.answerDtoValidator.validateWithQuestionType(answerDtoList.getAnswersList(),
                 questionModel.getQuestionType().getId(), bindingResult);
         if (bindingResult.hasErrors()) {
+            injectQuestionTypeModels(model);
             setUpdateBehaviour(model);
-            model.addAttribute("answerDtoList", answerDtoList);
+            final AnswerListDto oldDtoList = new AnswerListDto();
+            oldDtoList.map(questionModel);
+            model.addAttribute("answerDtoList", oldDtoList);
             model.addAttribute("topicId", topicId);
+            model.addAttribute("question", questionModel);
+
             model.addAttribute("questionId", questionId);
             model.addAttribute("errors", bindingResult);
+
+            System.out.println("\nHAS ERRORS");
+            System.out.println(bindingResult + "\n\n");
 
             return "answers/form";
         }
 
-        for (Integer i = 0; i < answerDtoList.size(); i++) {
-            final AnswerDto answerDto = answerDtoList.get(i);
-            final AnswerModelInterface answer = answerList.get(i);
+        for (Integer i = 0; i < newAnswerList.size(); i++) {
+            final AnswerDto answerDto = newAnswerList.get(i);
+            final AnswerModelInterface answer = new AnswerModel();
+            System.out.println(answerDto);
             answerDto.mapInto(answer);
             this.answersService.save(answer);
         }
+
+        this.deleteDeadAnswers(newAnswerList, oldAnswerList);
+
         return "redirect:/personal/topics/" + topicId + "/questions/" + questionId + "/update_answers/";
     }
-    /*
-    private TopicModelInterface getCurrentTopicModel(Integer topicId) {
-        final TopicsOptionsInterface topicsOptions = this.topicsOptionsBuilder.forAuth();
-        final TopicModelInterface topicModel = topicsService.find(topicId, topicsOptions);
-        checkModel(topicModel);
-        return topicModel;
-    }
 
-    private void checkModel(QuestionModelInterface questionModel, Integer topicId) {
-        if (questionModel == null) {
-            throw new NotFoundException();
-        }
-        if (!questionModel.getTopic().getId().equals(this.getCurrentTopicModel(topicId).getId())) {
-            throw new ForbiddenException();
-        }
-    }
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    private void deleteDeadAnswers(List<AnswerDto> newAnswers,
+                                   List<AnswerModelInterface> oldAnswers) {
 
-    private void checkModel(TopicModelInterface topicModel) {
-        if (topicModel == null) {
-            throw new NotFoundException();
-        }
-        if (!topicModel.getSubject().getUser().getId().equals(this.getCurrentUserModel().getId())) {
-            throw new ForbiddenException();
-        }
-    }
-    */
-    private void checkModel(AnswerModelInterface answerModel, Integer questionId) {
-        if (answerModel == null) {
-            throw new NotFoundException();
+        final List<Integer> newAnswersIds = new ArrayList<>(newAnswers.size());
+
+        newAnswersIds.addAll(newAnswers.stream().map(AnswerDto::getId)
+                .collect(Collectors.toList()));
+        for (AnswerModelInterface oldAnswer : oldAnswers) {
+            if (!newAnswersIds.contains(oldAnswer.getId())) {
+                System.out.println("DELETION: " + oldAnswer);
+                this.answersService.delete(oldAnswer);
+            }
         }
     }
 
     private QuestionModelInterface getQuestionModel(Integer id, Boolean forDelete) {
-        final QuestionsOptionsInterface questionsOptions = this.questionsOptionsBuilder.forAuth();
+        final QuestionsOptionsInterface questionsOptions = this.questionsOptionsBuilder
+                .forAuth()
+                .withAnswers(new AnswersOptions());
         final QuestionModelInterface questionModel = this.questionsService.find(id, questionsOptions);
         //checkModel(questionModel, topicId);
         if (forDelete) {
@@ -150,47 +151,10 @@ public class AnswersController extends AbstractCrudController {
         model.addAttribute("questionTypes", questionTypes);
     }
 
-    private AnswerModelInterface getAnswerModel(Integer id, Integer questionId) {
-        final AnswersOptionsInterface answersOptions = this.answersOptionsBuilder.forAuth();
-        final AnswerModelInterface answerModel = this.answersService.find(id, answersOptions);
-
-        return answerModel;
-    }
-
-    private List<AnswerModelInterface> getAnswerModelList(Integer questionId) {
-        final QuestionModelInterface questionModel = questionsService.find(questionId);
-        final AnswersOptionsInterface answersOptions = this.answersOptionsBuilder.forAuth();
-        final List<AnswerModelInterface> answersList = answersService.findByQuestion(questionModel, answersOptions);
-
-        return answersList;
-    }
-
     private List<AnswerModelInterface> getAnswerModelList(QuestionModelInterface questionModel) {
         final AnswersOptionsInterface answersOptions = this.answersOptionsBuilder.forAuth();
         final List<AnswerModelInterface> answersList = answersService.findByQuestion(questionModel, answersOptions);
 
         return answersList;
-    }
-
-    private List<AnswerModelInterface> getAnswerModelList(List<AnswerDto> answerDtoList) {
-        final List<AnswerModelInterface> answerList = new ArrayList<AnswerModelInterface>();
-        for (AnswerDto answerDto
-                : answerDtoList) {
-            final AnswerModel answer = new AnswerModel();
-            answerDto.mapInto(answer);
-            answerList.add(answer);
-        }
-        return answerList;
-    }
-    
-    private List<AnswerDto> getAnswerDtoList(List<AnswerModelInterface> answersList) {
-        final List<AnswerDto> answerDtoList = new ArrayList<AnswerDto>();
-        for (AnswerModelInterface answer
-                : answersList) {
-            final AnswerDto answerDto = new AnswerDto();
-            answerDto.map(answer);
-            answerDtoList.add(answerDto);
-        }
-        return answerDtoList;
     }
 }
