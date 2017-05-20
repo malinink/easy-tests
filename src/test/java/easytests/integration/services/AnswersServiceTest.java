38dc83142fc649d4304c28480fa37c1ed080aa5a
rezenbekk <rezenbekk@gmail.com>
2017-05-20 07:08:15 +0300

    Rewrite AnswersServiceTest

--- src/test/java/easytests/integration/services/AnswersServiceTest.java
+++ src/test/java/easytests/integration/services/AnswersServiceTest.java
@@ -2,8 +2,10 @@ package easytests.integration.services;
 
 import easytests.models.*;
 import easytests.models.empty.QuestionModelEmpty;
+import easytests.options.AnswersOptions;
+import easytests.options.QuestionsOptions;
 import easytests.services.AnswersService;
-import easytests.services.QuestionsService;
+import easytests.support.Models;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,22 +27,6 @@ public class AnswersServiceTest {
     @Autowired
     private AnswersService answersService;
 
-    @Autowired
-    private QuestionsService questionsService;
-
-    private AnswerModelInterface createAnswerModel(Integer id, String txt, Integer questionId,
-                                                    Integer serialNumber, Boolean right) {
-
-        final AnswerModelInterface answerModel = new AnswerModel();
-        answerModel.setId(id);
-        answerModel.setTxt(txt);
-        answerModel.setSerialNumber(serialNumber);
-        answerModel.setRight(right);
-        answerModel.setQuestion(questionsService.find(questionId));
-
-        return answerModel;
-    }
-
     @Test
     public void testSaveModel() throws Exception {
 
@@ -59,23 +45,49 @@ public class AnswersServiceTest {
     }
 
     @Test
-    public void testFindPresentModel() throws Exception {
-        final Integer id = 1;
-        final AnswerModelInterface answerModel = this.createAnswerModel(id, "Answer1", 1, 1, true);
-
-        final AnswerModelInterface builtAnswerModel = this.answersService.find(id);
-
-        Assert.assertEquals(answerModel.getId(), builtAnswerModel.getId());
-        Assert.assertEquals(answerModel.getTxt(), builtAnswerModel.getTxt());
-        Assert.assertEquals(answerModel.getSerialNumber(), builtAnswerModel.getSerialNumber());
-        Assert.assertEquals(answerModel.getRight(), builtAnswerModel.getRight());
-    }
-
-    @Test
     public void testFindAbsentModel() throws Exception {
         final Integer id = 10;
         final AnswerModelInterface answerModel = this.answersService.find(id);
 
         Assert.assertEquals(null, answerModel);
+    }
+
+    @Test
+    public void testFindWithOptions() throws Exception {
+        final Integer id = 1;
+        final Integer questionId = 1;
+
+        final AnswerModelInterface answerModel = Models.createAnswerModel(id, "Answer1", 1, 1, true);
+        final QuestionModelInterface questionModel = Models.createQuestionModel(questionId, "test1", 1, 1);
+        answerModel.setQuestion(questionModel);
+
+        final AnswerModelInterface buildAnswerModel
+                = this.answersService.find(id, new AnswersOptions().withQuestion(new QuestionsOptions()));
+
+        Assert.assertEquals(answerModel, buildAnswerModel);
+        Assert.assertEquals(questionModel, answerModel.getQuestion());
+    }
+
+    @Test
+    public void testSaveInsertsModel() throws Exception {
+        final Integer id = this.answersService.findAll().size() + 1;
+        final AnswerModelInterface answerModel = Models.createAnswerModel(null, "New answer", 5, 1, true);
+
+        this.answersService.save(answerModel);
+        final AnswerModelInterface builtAnswerModel = this.answersService.find(id);
+
+        Assert.assertEquals(answerModel, builtAnswerModel);
+    }
+
+    @Test
+    public void testSaveUpdatesModel() throws Exception {
+        final Integer id = 1;
+        final AnswerModelInterface answerModel = Models.createAnswerModel(id, "Newest Answer",
+                1, 1, true);
+        Assert.assertNotEquals(answerModel, this.answersService.find(id));
+
+        this.answersService.save(answerModel);
+
+        Assert.assertEquals(answerModel, this.answersService.find(id));
     }
 }
