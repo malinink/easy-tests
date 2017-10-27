09f8d8fbc72e284e93d2d39d0ace1916ae353c9b
ielay <ielay@yandex.ru>
2017-10-27 18:31:03 +0300

    added tests to findAll method

--- src/test/java/easytests/core/services/QuestionTypesServiceTest.java
+++ src/test/java/easytests/core/services/QuestionTypesServiceTest.java
@@ -4,7 +4,6 @@ import easytests.core.entities.QuestionTypeEntity;
 import easytests.core.mappers.QuestionTypesMapper;
 import easytests.core.models.QuestionTypeModel;
 import easytests.core.models.QuestionTypeModelInterface;
-import easytests.core.models.UserModelInterface;
 import easytests.support.Entities;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 
@@ -36,7 +38,7 @@ public class QuestionTypesServiceTest {
     }
 
     @Test
-    public void testFindById(){
+    public void testFindById() throws Exception {
         final Integer id = 1;
         final QuestionTypeEntity questionTypeEntityMock = Entities.createQuestionTypeEntity(
                 id,
@@ -58,6 +60,47 @@ public class QuestionTypesServiceTest {
         final QuestionTypeModelInterface questionTypeModel = this.questionTypesService.find(id);
 
         Assert.assertEquals(null, questionTypeModel);
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        final List<QuestionTypeEntity> questionTypeEntities = this.getQuestionTypeEntities();
+
+        given(this.questionTypesMapper.findAll()).willReturn(questionTypeEntities);
+
+        final List<QuestionTypeModelInterface> questionTypeModels = this.questionTypesService.findAll();
+
+        Assert.assertEquals(this.getQuestionTypeModels(), questionTypeModels);
+    }
+
+    @Test
+    public void testFindAllReturnsNull() throws Exception {
+        given(this.questionTypesMapper.findAll()).willReturn(new ArrayList<>(0));
+
+        final List<QuestionTypeModelInterface> questionTypeModels = this.questionTypesService.findAll();
+
+        Assert.assertEquals(0, questionTypeModels.size());
+    }
+
+    private List<QuestionTypeEntity> getQuestionTypeEntities(){
+        final List<QuestionTypeEntity> questionTypeEntities = new ArrayList<>(2);
+        questionTypeEntities.add(Entities.createQuestionTypeEntity(
+                1,
+                "name1"
+        ));
+        questionTypeEntities.add(Entities.createQuestionTypeEntity(
+                2,
+                "name2"
+        ));
+        return questionTypeEntities;
+    }
+
+    private List<QuestionTypeModelInterface> getQuestionTypeModels(){
+        final List<QuestionTypeModelInterface> questionTypeModels = new ArrayList<>(2);
+        for(QuestionTypeEntity questionTypeEntity: this.getQuestionTypeEntities()){
+            questionTypeModels.add(this.mapQuestionTypeModel(questionTypeEntity));
+        }
+        return questionTypeModels;
     }
 
 }
