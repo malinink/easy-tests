9ecfe86038eef0986763840eb8454996d24bc082
malinink <apmath@malinink.ru>
2017-11-11 00:02:52 +0300

    Add abstract entity test, we must use it in each entity test, provide meanbean confiuration

---
+++ src/test/java/easytests/core/entities/AbstractEntityTest.java
@@ -0,0 +1,26 @@
+package easytests.core.entities;
+
+import org.junit.runner.RunWith;
+import org.meanbean.test.Configuration;
+import org.meanbean.test.ConfigurationBuilder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @author malinink
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class AbstractEntityTest {
+
+
+    protected Configuration getConfiguration() {
+        return new ConfigurationBuilder().iterations(10).build();
+    }
+
+    public abstract void testCommon() throws Exception;
+
+    public abstract void testMap() throws Exception;
+
+}
