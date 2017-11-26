21d7e36365f34e7c9f81b5ea46be77cf7b6d04f8
ielay <ielay@yandex.ru>
2017-11-26 18:01:45 +0300

    Added TopicModelTest class for testing TopicModel class.

---
+++ src/test/java/easytests/core/models/TopicModelTest.java
@@ -0,0 +1,28 @@
+package easytests.core.models;
+
+import easytests.core.entities.TopicEntity;
+import easytests.support.TopicsSupport;
+import org.junit.Test;
+
+/**
+ * @author lelay
+ */
+public class TopicModelTest extends AbstractModelTest {
+
+    private TopicsSupport topicsSupport = new TopicsSupport();
+
+    @Test
+    public void testCommon() throws Exception {
+        this.testCommon(TopicModel.class);
+    }
+
+    @Test
+    public void testMap() throws Exception {
+        final TopicEntity topicEntity = this.topicsSupport.getEntityFixtureMock(0);
+        final TopicModel topicModel = new TopicModel();
+
+        topicModel.map(topicEntity);
+
+        this.topicsSupport.assertEquals(topicEntity, topicModel);
+    }
+}
