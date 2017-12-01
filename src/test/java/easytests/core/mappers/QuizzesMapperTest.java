0bf0d69db6068c707f50e335d4131bd52c361632
miron97 <miron972@mail.ru>
2017-12-02 02:31:22 +0500

    Fix some mistakes in QuizzesMapperTest

--- src/test/java/easytests/core/mappers/QuizzesMapperTest.java
+++ src/test/java/easytests/core/mappers/QuizzesMapperTest.java
@@ -1,14 +1,15 @@
 package easytests.core.mappers;
 
 import easytests.core.entities.QuizEntity;
-import java.time.LocalDateTime;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import easytests.core.entities.UserEntity;
 import easytests.support.QuizzesSupport;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +28,13 @@ public class QuizzesMapperTest extends AbstractMapperTest {
     @Test
     public void testFindAll() throws Exception {
 
-        final List<QuizEntity> quizEntities = this.quizzesMapper.findAll();
+        final List<QuizEntity> quizFoundedEntities = this.quizzesMapper.findAll();
 
-        Assert.assertNotNull(quizEntities);
-        Assert.assertEquals((long) 3, (long) quizEntities.size());
+        Assert.assertNotNull(quizFoundedEntities);
+        Assert.assertEquals((long) 3, (long) quizFoundedEntities.size());
 
         Integer index = 0;
-        for(QuizEntity quizEntity:quizEntities){
+        for(QuizEntity quizEntity: quizFoundedEntities){
             final QuizEntity quizFixtureEntity= this.quizzesSupport.getEntityFixtureMock(index);
 
             this.quizzesSupport.assertEquals(quizFixtureEntity, quizEntity);
@@ -46,73 +47,60 @@ public class QuizzesMapperTest extends AbstractMapperTest {
     public void testFind() throws Exception {
 
         final QuizEntity quizFixtureEntity = this.quizzesSupport.getEntityFixtureMock(1);
-        final QuizEntity quizEntity = this.quizzesMapper.find(quizFixtureEntity.getId());
+        final QuizEntity quizFoundedEntity = this.quizzesMapper.find(quizFixtureEntity.getId());
 
-        this.quizzesSupport.assertEquals(quizFixtureEntity, quizEntity);
-    }
-
-
-
-    @Test
-    public void testIssueNotNull() throws Exception {
-
-        final List<QuizEntity> quizEntities = this.quizzesMapper.findByIssueId(4);
-
-        Assert.assertNotNull(quizEntities);
-        Assert.assertEquals(0, quizEntities.size());
-
+        this.quizzesSupport.assertEquals(quizFixtureEntity, quizFoundedEntity);
     }
 
     @Test
     public void testFindByIssueId() throws Exception {
+        final List<QuizEntity> quizzesFixtureEntities = new ArrayList<>();
+        quizzesFixtureEntities.add(this.quizzesSupport.getEntityFixtureMock(1));
 
-        final QuizEntity quizFixtureEntity = this.quizzesSupport.getEntityFixtureMock(1);
-        final List<QuizEntity> quizEntities = this.quizzesMapper.findByIssueId(quizFixtureEntity.getIssueId());
+        final List<QuizEntity> quizzesFoundedEntities = this.quizzesMapper.findByIssueId(2);
 
-        for(QuizEntity quizEntity:quizEntities){
-            this.quizzesSupport.assertEquals(quizFixtureEntity, quizEntity);
-        }
+        Assert.assertEquals(1, quizzesFoundedEntities.size());
+        this.quizzesSupport.assertEquals(quizzesFixtureEntities.get(0), quizzesFoundedEntities.get(0));
     }
 
     @Test
     public void testInsert() throws Exception {
         final ArgumentCaptor<Integer> id = ArgumentCaptor.forClass(Integer.class);
-        final QuizEntity quizAdditionalEntity = this.quizzesSupport.getEntityAdditionalMock(0);
+        final QuizEntity quizUnidentifiedEntity = this.quizzesSupport.getEntityAdditionalMock(0);
 
-        quizzesMapper.insert(quizAdditionalEntity);
+        quizzesMapper.insert(quizUnidentifiedEntity);
 
-        verify(quizAdditionalEntity, times(1)).setId(id.capture());
+        verify(quizUnidentifiedEntity, times(1)).setId(id.capture());
         Assert.assertNotNull(id.getValue());
 
         final QuizEntity quizInsertedEntity = quizzesMapper.find(id.getValue());
 
-        this.quizzesSupport.assertEqualsWithoutId(quizAdditionalEntity, quizInsertedEntity);
+        this.quizzesSupport.assertEqualsWithoutId(quizUnidentifiedEntity, quizInsertedEntity);
     }
 
     @Test
     public void testUpdate() throws Exception {
 
-        final QuizEntity quizAdditionalEntity = this.quizzesSupport.getEntityAdditionalMock(1);
-        final Integer id = quizAdditionalEntity.getId();
-        final QuizEntity quizEntity = this.quizzesMapper.find(id);
+        final QuizEntity quizChangedEntity = this.quizzesSupport.getEntityAdditionalMock(1);
+        final QuizEntity quizBeforeUpdateEntity = this.quizzesMapper.find(quizChangedEntity.getId());
 
-        Assert.assertNotNull(quizEntity);
-        this.quizzesSupport.assertNotEqualsWithoutIds(quizAdditionalEntity, quizEntity);
+        Assert.assertNotNull(quizBeforeUpdateEntity);
+        this.quizzesSupport.assertNotEqualsWithoutIds(quizChangedEntity, quizBeforeUpdateEntity);
 
-        this.quizzesMapper.update(quizAdditionalEntity);
-        final QuizEntity quizUpdatedEntity = this.quizzesMapper.find(id);
+        this.quizzesMapper.update(quizChangedEntity);
+        final QuizEntity quizUpdatedEntity = this.quizzesMapper.find(quizChangedEntity.getId());
 
-        this.quizzesSupport.assertEquals(quizAdditionalEntity, quizUpdatedEntity);
+        this.quizzesSupport.assertEquals(quizChangedEntity, quizUpdatedEntity);
     }
 
     @Test
     public void testDelete() throws Exception {
         final Integer id = this.quizzesSupport.getEntityFixtureMock(0).getId();
-        QuizEntity quizEntity = this.quizzesMapper.find(id);
+        QuizEntity quizFoundedEntity = this.quizzesMapper.find(id);
 
-        Assert.assertNotNull(quizEntity);
+        Assert.assertNotNull(quizFoundedEntity);
 
-        this.quizzesMapper.delete(quizEntity);
-        quizEntity = this.quizzesMapper.find(id);
-        Assert.assertNull(quizEntity);
+        this.quizzesMapper.delete(quizFoundedEntity);
+        quizFoundedEntity = this.quizzesMapper.find(id);
+        Assert.assertNull(quizFoundedEntity);
     }}
