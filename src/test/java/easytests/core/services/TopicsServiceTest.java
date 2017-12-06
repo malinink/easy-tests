002d346817b40d3a479052ea9791904d9f501d21
ielay <ielay@yandex.ru>
2017-12-06 03:53:06 +0300

    Add test for models list saving and deleting + [cs].

--- src/test/java/easytests/core/services/TopicsServiceTest.java
+++ src/test/java/easytests/core/services/TopicsServiceTest.java
@@ -4,8 +4,6 @@ import easytests.core.entities.TopicEntity;
 import easytests.core.mappers.TopicsMapper;
 import easytests.core.models.SubjectModelInterface;
 import easytests.core.models.TopicModelInterface;
-import easytests.core.options.QuestionsOptionsInterface;
-import easytests.core.options.SubjectsOptionsInterface;
 import easytests.core.options.TopicsOptionsInterface;
 import easytests.core.services.exceptions.DeleteUnidentifiedModelException;
 import easytests.support.SubjectsSupport;
@@ -61,10 +59,14 @@ public class TopicsServiceTest {
         return topicsModels;
     }
 
-    private void assertServiceSet(TopicsOptionsInterface topicOptions) throws Exception {
-        verify(topicOptions, times(1)).setTopicsService(any(TopicsServiceInterface.class));
-        verify(topicOptions, times(1)).setSubjectsService(any(SubjectsServiceInterface.class));
-        verify(topicOptions, times(1)).setQuestionsService(any(QuestionsServiceInterface.class));
+    private void assertServicesSet(TopicsOptionsInterface topicsOptions) throws Exception{
+        this.assertServicesSet(topicsOptions, 1);
+    }
+
+    private void assertServicesSet(TopicsOptionsInterface topicsOptions, Integer times) throws Exception {
+        verify(topicsOptions, times(times)).setTopicsService(any(TopicsServiceInterface.class));
+        verify(topicsOptions, times(times)).setSubjectsService(any(SubjectsServiceInterface.class));
+        verify(topicsOptions, times(times)).setQuestionsService(any(QuestionsServiceInterface.class));
     }
 
     @Test
@@ -74,7 +76,7 @@ public class TopicsServiceTest {
 
         final List<TopicModelInterface> topicsFoundedModels = this.topicsService.findAll();
 
-        this.topicsSupport.assertModelsListEquals(topicsFoundedModels, this.getTopicsFixturesModels());
+        this.topicsSupport.assertModelsListEquals(this.getTopicsFixturesModels(), topicsFoundedModels);
     }
 
     @Test
@@ -90,20 +92,18 @@ public class TopicsServiceTest {
     @Test
     public void testFindAllWithOptions() throws Exception {
         final ArgumentCaptor<List> listCaptor = ArgumentCaptor.forClass(List.class);
-        // Why mockito's 'when' crashing if I want to pass 'this.get...Entitites()' as a parameter
-        // instead of passing predetermined 'topicEntities' ?
-        final List<TopicEntity> topicEntities = this.getTopicsFixturesEntities();
-        final List<TopicModelInterface> topicModels = this.getTopicsFixturesModels();
+        final List<TopicEntity> topicsEntities = this.getTopicsFixturesEntities();
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
         final TopicsOptionsInterface topicsOptions = mock(TopicsOptionsInterface.class);
 
-        when(this.topicsMapper.findAll()).thenReturn(topicEntities);
-        when(topicsOptions.withRelations(listCaptor.capture())).thenReturn(topicModels);
+        when(this.topicsMapper.findAll()).thenReturn(topicsEntities);
+        when(topicsOptions.withRelations(listCaptor.capture())).thenReturn(topicsModels);
 
-        final List<TopicModelInterface> topicFoundedModels = this.topicsService.findAll(topicsOptions);
+        final List<TopicModelInterface> topicsFoundedModels = this.topicsService.findAll(topicsOptions);
 
-        this.topicsSupport.assertModelsListEquals(topicModels, listCaptor.getValue());
-        Assert.assertSame(topicModels, topicFoundedModels);
-        this.assertServiceSet(topicsOptions);
+        this.topicsSupport.assertModelsListEquals(topicsModels, listCaptor.getValue());
+        Assert.assertSame(topicsModels, topicsFoundedModels);
+        this.assertServicesSet(topicsOptions);
     }
 
     @Test
@@ -140,29 +140,29 @@ public class TopicsServiceTest {
 
         this.topicsSupport.assertEquals(topicModel, topicModelCaptor.getValue());
         Assert.assertSame(topicModel, topicFoundedModel);
-        this.assertServiceSet(topicOptions);
+        this.assertServicesSet(topicOptions);
     }
 
     @Test
     public void testFindBySubjectPresentList() throws Exception {
         final SubjectModelInterface subjectModel = this.subjectsSupport.getModelFixtureMock(0);
-        final List<TopicEntity> topicEntities = this.getTopicsFixturesEntities();
+        final List<TopicEntity> topicsEntities = this.getTopicsFixturesEntities();
 
-        when(this.topicsMapper.findBySubjectId(subjectModel.getId())).thenReturn(topicEntities);
+        when(this.topicsMapper.findBySubjectId(subjectModel.getId())).thenReturn(topicsEntities);
 
-        final List<TopicModelInterface> topicFoundedModels = this.topicsService.findBySubject(subjectModel);
+        final List<TopicModelInterface> topicsFoundedModels = this.topicsService.findBySubject(subjectModel);
 
-        this.topicsSupport.assertModelsListEquals(this.getTopicsFixturesModels(), topicFoundedModels);
+        this.topicsSupport.assertModelsListEquals(this.getTopicsFixturesModels(), topicsFoundedModels);
     }
 
     @Test
     public void testFindBySubjectAbsentList() throws Exception {
-        final SubjectModelInterface absentSubject = this.subjectsSupport.getModelFixtureMock(0);
+        final SubjectModelInterface subjectModel = this.subjectsSupport.getModelFixtureMock(0);
 
-        when(this.topicsMapper.findBySubjectId(absentSubject.getId())).thenReturn(new ArrayList<>(0));
+        when(this.topicsMapper.findBySubjectId(subjectModel.getId())).thenReturn(new ArrayList<>(0));
 
         final List<TopicModelInterface> topicFoundedModels =
-                this.topicsService.findBySubject(absentSubject);
+                this.topicsService.findBySubject(subjectModel);
 
         Assert.assertNotNull(topicFoundedModels);
         Assert.assertEquals(0, topicFoundedModels.size());
@@ -171,21 +171,21 @@ public class TopicsServiceTest {
     @Test
     public void testFindBySubjectWithOptions() throws Exception {
         final ArgumentCaptor<List> listCaptor = ArgumentCaptor.forClass(List.class);
-        final List<TopicEntity> topicEntities = this.getTopicsFixturesEntities();
-        final List<TopicModelInterface> topicModels = this.getTopicsFixturesModels();
+        final List<TopicEntity> topicsEntities = this.getTopicsFixturesEntities();
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
         final TopicsOptionsInterface topicOptions = mock(TopicsOptionsInterface.class);
 
         final SubjectModelInterface subjectModel = this.subjectsSupport.getModelFixtureMock(0);
 
-        when(this.topicsMapper.findBySubjectId(subjectModel.getId())).thenReturn(topicEntities);
-        when(topicOptions.withRelations(listCaptor.capture())).thenReturn(topicModels);
+        when(this.topicsMapper.findBySubjectId(subjectModel.getId())).thenReturn(topicsEntities);
+        when(topicOptions.withRelations(listCaptor.capture())).thenReturn(topicsModels);
 
-        final List<TopicModelInterface> topicFoundedModels =
+        final List<TopicModelInterface> topicsFoundedModels =
                 this.topicsService.findBySubject(subjectModel, topicOptions);
 
-        this.topicsSupport.assertModelsListEquals(topicModels, listCaptor.getValue());
-        Assert.assertSame(topicModels, topicFoundedModels);
-        this.assertServiceSet(topicOptions);
+        this.topicsSupport.assertModelsListEquals(topicsModels, listCaptor.getValue());
+        Assert.assertSame(topicsModels, topicsFoundedModels);
+        this.assertServicesSet(topicOptions);
     }
 
     @Test
@@ -231,7 +231,33 @@ public class TopicsServiceTest {
         this.topicsService.save(topicModel, topicOptions);
 
         verify(topicOptions, times(1)).saveWithRelations(topicModel);
-        this.assertServiceSet(topicOptions);
+        this.assertServicesSet(topicOptions);
+        verifyNoMoreInteractions(this.topicsMapper);
+    }
+
+    @Test
+    public void testSaveModelsList() throws Exception {
+        final ArgumentCaptor<TopicEntity> topicEntityCaptor = ArgumentCaptor.forClass(TopicEntity.class);
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
+
+        this.topicsService.save(topicsModels);
+
+        verify(this.topicsMapper, times(topicsModels.size())).update(topicEntityCaptor.capture());
+        this.topicsSupport.assertEntitiesListEquals(this.getTopicsFixturesEntities(), topicEntityCaptor.getAllValues());
+    }
+
+    @Test
+    public void testSaveModelsListWithOptions() throws Exception {
+        final ArgumentCaptor<TopicModelInterface> topicModelCaptor = ArgumentCaptor.forClass(TopicModelInterface.class);
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
+        final TopicsOptionsInterface topicsOptions = Mockito.mock(TopicsOptionsInterface.class);
+
+        this.topicsService.save(topicsModels, topicsOptions);
+
+        this.assertServicesSet(topicsOptions, topicsModels.size());
+        verify(topicsOptions, times(topicsModels.size())).saveWithRelations(topicModelCaptor.capture());
+        this.topicsSupport.assertModelsListEquals(topicsModels, topicModelCaptor.getAllValues());
+        verifyNoMoreInteractions(this.topicsMapper);
     }
 
     @Test
@@ -260,6 +286,34 @@ public class TopicsServiceTest {
         this.topicsService.delete(topicModel, topicOptions);
 
         verify(topicOptions, times(1)).deleteWithRelations(topicModel);
-        this.assertServiceSet(topicOptions);
+        this.assertServicesSet(topicOptions);
+        verifyNoMoreInteractions(this.topicsMapper);
     }
+
+    @Test
+    public void testDeleteModelsList() throws Exception {
+        final ArgumentCaptor<TopicEntity> topicEntityCaptor = ArgumentCaptor.forClass(TopicEntity.class);
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
+
+        this.topicsService.delete(topicsModels);
+
+        verify(this.topicsMapper, times(topicsModels.size())).delete(topicEntityCaptor.capture());
+        this.topicsSupport.assertEntitiesListEquals(this.getTopicsFixturesEntities(), topicEntityCaptor.getAllValues());
+    }
+
+    @Test
+    public void testDeleteModelsListWithOptions() throws Exception {
+        final ArgumentCaptor<TopicModelInterface> topicModelCaptor = ArgumentCaptor.forClass(TopicModelInterface.class);
+        final List<TopicModelInterface> topicsModels = this.getTopicsFixturesModels();
+        final TopicsOptionsInterface topicsOptions = Mockito.mock(TopicsOptionsInterface.class);
+
+        this.topicsService.delete(topicsModels, topicsOptions);
+
+        this.assertServicesSet(topicsOptions, topicsModels.size());
+        verify(topicsOptions, times(topicsModels.size())).deleteWithRelations(topicModelCaptor.capture());
+        this.topicsSupport.assertModelsListEquals(topicsModels, topicModelCaptor.getAllValues());
+        verifyNoMoreInteractions(this.topicsMapper);
+    }
+
+
 }
