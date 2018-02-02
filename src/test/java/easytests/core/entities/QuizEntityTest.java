3d2b21b531ba1c6260acc0f2be8a6fdb6e35c66c
malinink <apmath@malinink.ru>
2018-02-02 22:46:28 +0300

    Remove unused import statements, do not ignore LocalDateTime fields any more

--- src/test/java/easytests/core/entities/QuizEntityTest.java
+++ src/test/java/easytests/core/entities/QuizEntityTest.java
@@ -1,15 +1,9 @@
 package easytests.core.entities;
 
 import easytests.core.models.QuizModelInterface;
-import easytests.core.models.empty.IssueModelEmpty;
 import easytests.support.QuizzesSupport;
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.meanbean.test.*;
-import org.mockito.Mockito;
 
-import java.time.LocalDateTime;
 
 /**
  * @author miron97
@@ -17,20 +11,6 @@ import java.time.LocalDateTime;
 public class QuizEntityTest extends AbstractEntityTest {
 
     private QuizzesSupport quizSupport = new QuizzesSupport();
-
-    @Override
-    protected Configuration getConfiguration(){
-        return new ConfigurationBuilder().iterations(10)
-                .ignoreProperty("startedAt")
-                .ignoreProperty("finishedAt")
-                .build();
-    }
-
-    @Override
-    protected void testCommon(Class entityClass) {
-        final Configuration configuration = this.getConfiguration();
-        new BeanTester().testBean(entityClass, configuration);
-    }
 
     @Test
     public void testCommon() throws Exception {
@@ -41,7 +21,9 @@ public class QuizEntityTest extends AbstractEntityTest {
     public void testMap() throws Exception {
         final QuizModelInterface quizModel = this.quizSupport.getModelFixtureMock(0);
         final QuizEntity quizEntity = new QuizEntity();
+
         quizEntity.map(quizModel);
+
         this.quizSupport.assertEquals(quizModel, quizEntity);
     }
 
