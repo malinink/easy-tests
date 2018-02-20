794207def507e1fcc97848d66a3283c454dc13e6
malinink <apmath@malinink.ru>
2018-02-20 22:30:28 +0300

    cs on QuestionTypeEntityTest

--- src/test/java/easytests/core/entities/QuestionTypeEntityTest.java
+++ src/test/java/easytests/core/entities/QuestionTypeEntityTest.java
@@ -24,7 +24,6 @@ public class QuestionTypeEntityTest extends AbstractEntityTest {
 
         questionTypeEntity.map(questionTypeModel);
 
-        this.questionTypesSupport.assertEquals(questionTypeModel,questionTypeEntity );
+        this.questionTypesSupport.assertEquals(questionTypeModel, questionTypeEntity);
     }
-
 }
