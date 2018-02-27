df20c05801b937d2bb45087a45afb52b6760bc0c
malinink <apmath@malinink.ru>
2018-02-27 22:44:08 +0300

    Add V1 API UsersMapper

---
+++ src/main/java/easytests/api/v1/mappers/UsersMapper.java
@@ -0,0 +1,13 @@
+package easytests.api.v1.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author malinink
+ */
+@Service("UsersMapperV1")
+public class UsersMapper extends ModelMapper {
+
+}
