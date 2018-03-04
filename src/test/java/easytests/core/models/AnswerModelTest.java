2645644d5e94324c5c6ffd767afe901c6b3a3475
malinink <apmath@malinink.ru>
2018-03-04 13:52:00 +0300

    cs on AnswersModelTest

--- src/test/java/easytests/core/models/AnswerModelTest.java
+++ src/test/java/easytests/core/models/AnswerModelTest.java
@@ -5,6 +5,7 @@ import easytests.support.AnswersSupport;
 import org.junit.Test;
 import org.meanbean.test.ConfigurationBuilder;
 
+
 /**
  * @author AnyaMaz
  */
@@ -27,11 +28,9 @@ public class AnswerModelTest extends AbstractModelTest {
     public void testMap() throws Exception {
         final AnswerEntity answerEntity = this.answersSupport.getEntityFixtureMock(0);
         final AnswerModel answerModel = new AnswerModel();
+
         answerModel.map(answerEntity);
 
         this.answersSupport.assertEquals(answerEntity, answerModel);
     }
 }
-
-
-
