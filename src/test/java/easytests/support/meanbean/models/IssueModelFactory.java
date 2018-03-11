61be3e39540b375df44d46e63fb07029a3a20015
miron97 <miron972@mail.ru>
2018-03-11 18:39:14 +0500

    Add name of author to Issue and Testee factories

--- src/test/java/easytests/support/meanbean/models/IssueModelFactory.java
+++ src/test/java/easytests/support/meanbean/models/IssueModelFactory.java
@@ -4,6 +4,10 @@ import easytests.core.models.IssueModel;
 import easytests.core.models.IssueModelInterface;
 import org.meanbean.lang.Factory;
 
+
+/**
+ * @author miron97
+ */
 public class IssueModelFactory implements Factory<IssueModelInterface> {
 
     @Override
--- src/test/java/easytests/support/meanbean/models/TesteeModelFactory.java
+++ src/test/java/easytests/support/meanbean/models/TesteeModelFactory.java
@@ -4,7 +4,12 @@ import easytests.core.models.TesteeModel;
 import easytests.core.models.TesteeModelInterface;
 import org.meanbean.lang.Factory;
 
+
+/**
+ * @author miron97
+ */
 public class TesteeModelFactory implements Factory<TesteeModelInterface> {
+
     @Override
     public TesteeModelInterface create() {
         return new TesteeModel();
