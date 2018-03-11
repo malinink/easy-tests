de067059e19bf43173915696ba8027622162b4e8
miron97 <miron972@mail.ru>
2018-03-11 18:31:18 +0500

    Update TesteeModelFactory

--- src/test/java/easytests/support/meanbean/models/TesteeModelFactory.java
+++ src/test/java/easytests/support/meanbean/models/TesteeModelFactory.java
@@ -1,4 +1,12 @@
 package easytests.support.meanbean.models;
 
-public class TesteeModelFactory {
+import easytests.core.models.TesteeModel;
+import easytests.core.models.TesteeModelInterface;
+import org.meanbean.lang.Factory;
+
+public class TesteeModelFactory implements Factory<TesteeModelInterface> {
+    @Override
+    public TesteeModelInterface create() {
+        return new TesteeModel();
+    }
 }
