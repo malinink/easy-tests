fcd00966a2c28421aa9ca61b01ed74f699870cd4
miron97 <miron972@mail.ru>
2018-03-11 18:29:35 +0500

    Update IssueModelFactory

--- src/test/java/easytests/support/meanbean/models/IssueModelFactory.java
+++ src/test/java/easytests/support/meanbean/models/IssueModelFactory.java
@@ -1,4 +1,13 @@
 package easytests.support.meanbean.models;
 
-public class IssueModelFactory {
+import easytests.core.models.IssueModel;
+import easytests.core.models.IssueModelInterface;
+import org.meanbean.lang.Factory;
+
+public class IssueModelFactory implements Factory<IssueModelInterface> {
+
+    @Override
+    public IssueModelInterface create() {
+        return new IssueModel();
+    }
 }
