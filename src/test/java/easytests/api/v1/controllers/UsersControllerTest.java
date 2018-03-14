dda3c8f9b08bc584bbeb0b2bed2b245862da3aea
malinink <apmath@malinink.ru>
2018-03-14 22:59:15 +0300

    Add simple controller test, without swagger spec response validation

---
+++ src/test/java/easytests/api/v1/controllers/UsersControllerTest.java
@@ -0,0 +1,55 @@
+package easytests.api.v1.controllers;
+
+import easytests.api.v1.mappers.UsersMapper;
+import easytests.core.models.UserModelInterface;
+import easytests.core.options.UsersOptions;
+import easytests.core.options.UsersOptionsInterface;
+import easytests.core.options.builder.UsersOptionsBuilder;
+import easytests.core.services.UsersService;
+import easytests.support.UsersSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.BDDMockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ * @author malinink
+ */
+@Import(UsersMapper.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UsersController.class, secure = false)
+public class UsersControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UsersService usersService;
+
+    @MockBean
+    private UsersOptionsBuilder usersOptionsBuilder;
+
+    private UsersSupport usersSupport = new UsersSupport();
+
+    @Test
+    public void testViewReturnsData() throws Exception {
+        final UserModelInterface userModel = this.usersSupport.getModelFixtureMock(0);
+        when(this.usersOptionsBuilder.forAuth()).thenReturn(new UsersOptions());
+        when(this.usersService.find(any(Integer.class), any(UsersOptionsInterface.class))).thenReturn(userModel);
+
+        mvc.perform(get("/v1/users/1/")
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andReturn();
+    }
+}
