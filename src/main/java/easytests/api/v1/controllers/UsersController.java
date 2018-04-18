be33699cb4d10b100974d119bee08a468fac7fd6
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-04-18 23:37:40 +0300

    changes

--- src/main/java/easytests/api/v1/controllers/UsersController.java
+++ src/main/java/easytests/api/v1/controllers/UsersController.java
@@ -1,4 +1,34 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.mappers.UsersMapper;
+import easytests.api.v1.models.User;
+import easytests.common.exceptions.NotFoundException;
+import easytests.core.models.UserModelInterface;
+import easytests.core.options.SubjectsOptions;
+import easytests.core.options.UsersOptions;
+import easytests.core.options.UsersOptionsInterface;
+import easytests.core.options.builder.UsersOptionsBuilder;
+import easytests.core.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * @author SvetlanaTselikova
+ */
+@RestController("UsersControllerV1")
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+@RequestMapping("/v1/users/")
 public class UsersController {
+
+    @Autowired
+    protected UsersService usersService;
+
+    @Autowired
+    private UsersOptionsBuilder usersOptionsBuilder;
+
+    @Autowired
+    @Qualifier("UsersMapperV1")
+    private UsersMapper usersMapper;
 }
