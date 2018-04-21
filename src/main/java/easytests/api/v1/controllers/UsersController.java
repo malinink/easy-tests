e3011cabca15f34d3e8e257ccf0c2461316c7c1a
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-04-21 18:16:02 +0300

    add todo list

--- src/main/java/easytests/api/v1/controllers/UsersController.java
+++ src/main/java/easytests/api/v1/controllers/UsersController.java
@@ -1,25 +1,30 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.BadRequestException;
+import easytests.api.v1.exceptions.IdentifiedModelException;
+import easytests.api.v1.exceptions.NotFoundException;
+import easytests.api.v1.exceptions.UnidentifiedModelException;
 import easytests.api.v1.mappers.UsersMapper;
+import easytests.api.v1.models.Identity;
 import easytests.api.v1.models.User;
-import easytests.common.exceptions.NotFoundException;
+import easytests.core.models.UserModel;
 import easytests.core.models.UserModelInterface;
 import easytests.core.options.SubjectsOptions;
 import easytests.core.options.UsersOptions;
 import easytests.core.options.UsersOptionsInterface;
 import easytests.core.options.builder.UsersOptionsBuilder;
 import easytests.core.services.UsersService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
-/**
- * @author SvetlanaTselikova
- */
 @RestController("UsersControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
-@RequestMapping("/v1/users/")
+@RequestMapping("/v1/users")
 public class UsersController {
 
     @Autowired
@@ -31,6 +36,22 @@ public class UsersController {
     @Autowired
     @Qualifier("UsersMapperV1")
     private UsersMapper usersMapper;
-    
-
+    /**
+     * list
+     */
+    /**
+     * create
+     */
+    /**
+     * update
+     */
+    /**
+     * show(userId)
+     */
+    /**
+     * delete(userId)
+     */
+    /**
+     * ShowMe
+     */
 }
