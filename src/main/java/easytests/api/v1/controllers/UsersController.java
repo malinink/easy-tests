04eafe59887bdd447c37b2a54aed415744c4bd2c
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-04-23 02:12:34 +0300

    c

--- src/main/java/easytests/api/v1/controllers/UsersController.java
+++ src/main/java/easytests/api/v1/controllers/UsersController.java
@@ -1,5 +1,10 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.mappers.UsersMapper;
+import easytests.core.options.builder.UsersOptionsBuilder;
+import easytests.core.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,6 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/users")
 public class UsersController {
+
+    @Autowired
+    protected UsersService usersService;
+
+    @Autowired
+    private UsersOptionsBuilder usersOptionsBuilder;
+
+    @Autowired
+    @Qualifier("UsersMapperV1")
+    private UsersMapper usersMapper;
 
     /**
      * list
