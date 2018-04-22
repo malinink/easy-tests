829e22f39cb9efab074189716e18b22cd1c8cf30
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-04-23 01:26:32 +0300

    c

--- src/main/java/easytests/api/v1/controllers/UsersController.java
+++ src/main/java/easytests/api/v1/controllers/UsersController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author SvetlanaTselikova
  */
+@RestController("ObjectsControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/users")
 public class UsersController {
