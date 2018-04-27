da01e42ddeb93fbb9c40e40727a903d2a2377b37
ielay <ielay@yandex.ru>
2018-04-27 20:07:50 +0300

    Create empty mapper, model and controller

---
+++ src/main/java/easytests/api/v1/controllers/TopicsController.java
@@ -0,0 +1,45 @@
+package easytests.api.v1.controllers;
+
+import easytests.api.v1.mappers.TopicsMapper;
+import easytests.core.options.builder.TopicsOptionsBuilderInterface;
+import easytests.core.services.TopicsServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lelay
+ */
+@RestController("TopicsControllerV1")
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
+@RequestMapping("/v1/topics")
+public class TopicsController {
+
+    @Autowired
+    protected TopicsServiceInterface topicsService;
+    //todo: I think it's better to autowire an service interface instead of it's realisation (because we don't care how exactly our service works)
+
+    @Autowired
+    private TopicsOptionsBuilderInterface topicsOptions;
+
+    @Autowired
+    @Qualifier("TopicsMapperV1")
+    private TopicsMapper usersMapper;
+
+    /**
+     * list
+     */
+    /**
+     * create
+     */
+    /**
+     * update
+     */
+    /**
+     * show(userId)
+     */
+    /**
+     * delete(userId)
+     */
+}
---
+++ src/main/java/easytests/api/v1/models/Topic.java
@@ -0,0 +1,10 @@
+package easytests.api.v1.models;
+
+import lombok.Data;
+
+/**
+ * @author lelay
+ */
+@Data
+public class Topic {
+}
