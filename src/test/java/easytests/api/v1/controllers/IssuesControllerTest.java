e3e1ea08630969941efbffdb24975a01364237d2
Yarik2308 <yarik1706@mail.ru>
2018-05-05 12:56:18 +0300

    Delete listMethod and add ControllerTest

---
+++ src/test/java/easytests/api/v1/controllers/IssuesControllerTest.java
@@ -0,0 +1,61 @@
+package easytests.api.v1.controllers;
+
+/**
+ * @author Yarik2308
+ */
+public class IssuesControllerTest {
+    /**
+     * testListSuccess
+     */
+    /**
+     * testListBadRequest
+     */
+    /**
+     * testListPermissionDenied
+     */
+    /**
+     * testListSubjectIdNotFound
+     */
+    /**
+     * testCreateSuccess
+     */
+    /**
+     * testCreateWithId
+     */
+    /**
+     * testCreateBadRequest
+     */
+    /**
+     * testCreatePermissionDenied
+     */
+    /**
+     * testUpdateSuccess
+     */
+    /**
+     * testUpdateBadRequest
+     */
+    /**
+     * testUpdatePermissionDenied
+     */
+    /**
+     * testShowByIdSuccess
+     */
+    /**
+     * testShowByIdBadRequest
+     */
+    /**
+     * testShowByIdIssueNotFound
+     */
+    /**
+     * testDeleteByIdSuccess
+     */
+    /**
+     * testDeleteByIdBadRequest
+     */
+    /**
+     * testDeleteByIdPermissionDenied
+     */
+    /**
+     * testDeleteByIdIssueNotFound
+     */
+}
