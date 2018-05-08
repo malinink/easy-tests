36492fb9a04280a67d44fa0f89195c7653798063
SvetlanaTselikova <svetacelikova1@gmail.com>
2018-05-08 18:21:13 +0300

    changes

--- src/main/java/easytests/api/v1/controllers/UsersController.java
+++ src/main/java/easytests/api/v1/controllers/UsersController.java
@@ -3,8 +3,8 @@ package easytests.api.v1.controllers;
 import easytests.api.v1.mappers.UsersMapper;
 import easytests.api.v1.models.User;
 import easytests.core.models.UserModelInterface;
-import easytests.core.options.builder.UsersOptionsBuilder;
-import easytests.core.services.UsersService;
+import easytests.core.options.builder.UsersOptionsBuilderInterface;
+import easytests.core.services.UsersServiceInterface;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +21,10 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
 
     @Autowired
-    protected UsersService usersService;
+    protected UsersServiceInterface usersService;
 
     @Autowired
-    private UsersOptionsBuilder usersOptionsBuilder;
+    private UsersOptionsBuilderInterface usersOptionsBuilder;
 
     @Autowired
     @Qualifier("UsersMapperV1")
