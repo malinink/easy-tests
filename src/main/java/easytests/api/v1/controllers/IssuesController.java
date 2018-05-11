eb89a7fdcd2f80ac0f999f58d9783cc16bd5cb00
Yarik2308 <yarik1706@mail.ru>
2018-05-11 20:03:53 +0300

    Add exeptions

--- src/main/java/easytests/api/v1/controllers/IssuesController.java
+++ src/main/java/easytests/api/v1/controllers/IssuesController.java
@@ -1,11 +1,14 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.BadRequestException;
+import easytests.api.v1.exceptions.UnidentifiedModelException;
 import easytests.api.v1.mappers.IssuesMapper;
 import easytests.api.v1.models.Issue;
 import easytests.core.models.IssueModelInterface;
 import easytests.core.models.SubjectModel;
 import easytests.core.options.builder.IssuesOptionsBuilder;
 import easytests.core.services.IssuesService;
+import easytests.core.services.SubjectsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -24,17 +27,22 @@ public class IssuesController {
     protected IssuesService issuesService;
 
     @Autowired
+    protected SubjectsService subjectsService;
+
+    @Autowired
     protected IssuesOptionsBuilder issuesOptions;
 
     @Autowired
     @Qualifier("IssuesMapperV1")
     private IssuesMapper issuesMapper;
 
-    /**
-     * listIssues
-     */
-    @GetMapping("/get")
-    public List<Issue> list(@RequestParam("subjectId") int subjectId){
+    @GetMapping("")
+    public List<Issue> list(@RequestParam int subjectId) throws BadRequestException {
+        if (subjectsService.find(subjectId) == null)
+        {
+            throw new UnidentifiedModelException();
+        }
+
         final SubjectModel subjectModel = new SubjectModel();
         subjectModel.setId(subjectId);
 
