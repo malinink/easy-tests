198c1fa0dcb9fabc1b014364aa1010f7f0577f6c
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-15 01:44:14 +0300

    Fix methods in Controller Test

--- src/test/java/easytests/api/v1/controllers/SubjectsControllerTest.java
+++ src/test/java/easytests/api/v1/controllers/SubjectsControllerTest.java
@@ -4,50 +4,22 @@ package easytests.api.v1.controllers;
  * @author VeronikaRevjakina
  */
 public class SubjectsControllerTest {
+
     /**
-     * testListSuccess
+     * list
      */
     /**
-     * testCreateSuccess
+     * create
      */
     /**
-     * testCreateWithIdFailed
+     * update
      */
     /**
-     * testCreateWithSubjectsFailed
+     * show(subjectId)
      */
     /**
-     * testUpdateSuccess
+     * delete(subjectId)
      */
-    /**
-     * testUpdateWithoutIdFailed
-     */
-    /**
-     * testUpdateWithSubjectsFailed
-     */
-    /**
-     * testShowSuccess
-     */
-    /**
-     * testShowFailed
-     */
-    /**
-     * testShowWithSubjectsSuccess
-     */
-    /**
-     * testDeleteSuccess
-     */
-    /**
-     * testDeleteFailed
-     */
-    /**
-     * testShowMeSuccess
-     */
-    /**
-     * testShowMeFailed
-     */
-    /**
-     * testShowMeWithSubjectsSuccess
-     */
+    
 }
 
