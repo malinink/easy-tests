9c2119d6665474f434d2b762add19447311bb5aa
Yarik2308 <yarik1706@mail.ru>
2018-05-15 13:23:17 +0300

    Add Exeptions

--- src/main/java/easytests/api/v1/controllers/IssuesController.java
+++ src/main/java/easytests/api/v1/controllers/IssuesController.java
@@ -1,11 +1,16 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.BadRequestException;
+import easytests.api.v1.exceptions.ForbiddenException;
 import easytests.api.v1.mappers.IssuesMapper;
 import easytests.api.v1.models.Issue;
+import easytests.common.exceptions.NotFoundException;
 import easytests.core.models.IssueModelInterface;
+import easytests.core.models.SubjectModelInterface;
 import easytests.core.models.empty.SubjectModelEmpty;
 import easytests.core.options.builder.IssuesOptionsBuilder;
 import easytests.core.services.IssuesServiceInterface;
+import easytests.core.services.SubjectsServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +24,7 @@ import java.util.stream.Collectors;
 @RestController("IssuesControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/issues")
-public class IssuesController {
+public class IssuesController extends AbstractController{
 
     @Autowired
     protected IssuesServiceInterface issuesService;
@@ -28,14 +33,31 @@ public class IssuesController {
     protected IssuesOptionsBuilder issuesOptions;
 
     @Autowired
+    protected SubjectsServiceInterface subjectsService;
+
+    @Autowired
     @Qualifier("IssuesMapperV1")
     private IssuesMapper issuesMapper;
 
     @GetMapping
-    public List<Issue> list(@RequestParam(name = "subjectId", required = true) Integer subjectId) {
+    public List<Issue> list(@RequestParam(name = "subjectId", required = true) Integer subjectId)
+            throws NotFoundException, ForbiddenException {
+        final SubjectModelInterface subjectModel = this.subjectsService.find(subjectId);
+
+        if(subjectModel == null) {
+            throw new NotFoundException();
+        }
+
+        if(!this.acl.hasAccess(subjectModel)) {
+            throw new ForbiddenException();
+        }
 
         final List<IssueModelInterface> issuesModels = this.issuesService
                 .findBySubject(new SubjectModelEmpty(subjectId));
+
+        if(issuesModels == null) {
+            throw new NotFoundException();
+        }
 
         return issuesModels
                 .stream()
--- src/test/java/easytests/api/v1/controllers/IssuesControllerTest.java
+++ src/test/java/easytests/api/v1/controllers/IssuesControllerTest.java
@@ -22,7 +22,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
