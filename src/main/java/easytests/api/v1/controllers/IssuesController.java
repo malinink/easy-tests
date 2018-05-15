9200f0d5519cf69be40b1ff0a0c13fcb97983d9b
Yarik2308 <yarik1706@mail.ru>
2018-05-15 14:46:36 +0300

    Begining

--- src/main/java/easytests/api/v1/controllers/IssuesController.java
+++ src/main/java/easytests/api/v1/controllers/IssuesController.java
@@ -1,8 +1,17 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.BadRequestException;
+import easytests.api.v1.exceptions.ForbiddenException;
+import easytests.api.v1.exceptions.UnidentifiedModelException;
 import easytests.api.v1.mappers.IssuesMapper;
+import easytests.api.v1.models.Issue;
+import easytests.common.exceptions.NotFoundException;
+import easytests.core.models.IssueModelInterface;
+import easytests.core.options.IssuesOptionsInterface;
 import easytests.core.options.builder.IssuesOptionsBuilder;
-import easytests.core.services.IssuesService;
+import easytests.core.options.builder.IssuesOptionsBuilderInterface;
+import easytests.core.services.IssuesServiceInterface;
+import easytests.core.services.SubjectsServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +19,19 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author Yarik2308
  */
-@RestController("IssueControllerV1")
+@RestController("IssuesControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/issues")
-public class IssuesController {
+public class IssuesController extends AbstractController{
 
     @Autowired
-    protected IssuesService issuesService;
+    protected IssuesServiceInterface issuesService;
 
     @Autowired
-    protected IssuesOptionsBuilder issuesOptions;
+    protected IssuesOptionsBuilderInterface issuesOptionsBuilder;
+
+    @Autowired
+    protected SubjectsServiceInterface subjectsService;
 
     @Autowired
     @Qualifier("IssuesMapperV1")
@@ -31,9 +43,22 @@ public class IssuesController {
     /**
      * create
      */
-    /**
-     * update
-     */
+
+    @PutMapping
+    public void update(@RequestBody Issue issue) throws BadRequestException, NotFoundException {
+
+        if(issue.getId()==null) {
+            throw new UnidentifiedModelException();
+        }
+        
+
+        final IssueModelInterface issueModel = this.getIssueModel(issue.getId());
+
+        this.issuesMapper.map(issue, issueModel);
+
+        this.issuesService.save(issueModel);
+    }
+
     /**
      * show(issueId)
      */
@@ -41,4 +66,15 @@ public class IssuesController {
      * delete(issueId)
      */
 
+    private IssueModelInterface getIssueModel(Integer id, IssuesOptionsInterface issueOptions) throws NotFoundException {
+        final IssueModelInterface issueModel = this.issuesService.find(id, issueOptions);
+        if (issueModel == null) {
+            throw new NotFoundException();
+        }
+        return issueModel;
+    }
+
+    private IssueModelInterface getIssueModel(Integer id) throws NotFoundException {
+        return this.getIssueModel(id, this.issuesOptionsBuilder.forAuth());
+    }
 }
