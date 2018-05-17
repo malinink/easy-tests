9b83eac4e65a3c6a13efbe90d6a47b2b80a37e24
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-17 22:56:31 +0300

    Add list method into SubjectsController

--- src/main/java/easytests/api/v1/controllers/SubjectsController.java
+++ src/main/java/easytests/api/v1/controllers/SubjectsController.java
@@ -1,10 +1,17 @@
 package easytests.api.v1.controllers;
 
 import easytests.api.v1.mappers.SubjectsMapper;
+import easytests.api.v1.models.Subject;
+import easytests.core.models.SubjectModelInterface;
+import easytests.core.models.empty.UserModelEmpty;
 import easytests.core.options.builder.SubjectsOptionsBuilderInterface;
 import easytests.core.services.SubjectsServiceInterface;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -26,9 +33,18 @@ public class SubjectsController {
     @Qualifier("SubjectsMapperV1")
     private SubjectsMapper subjectsMapper;
 
-    /**
-     * list
-     */
+
+    @GetMapping("")
+    public List<Subject> list(@RequestParam(name = "userId", required = true) Integer userId) {
+        final List<SubjectModelInterface> subjectsModels = this.subjectsService.findByUser(new UserModelEmpty(userId));
+
+        //todo: ACL
+
+        return subjectsModels
+                .stream()
+                .map(model -> this.subjectsMapper.map(model, Subject.class))
+                .collect(Collectors.toList());
+    }
     /**
      * create
      */
