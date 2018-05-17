4a6be23abeda58c0585673d2cddb2f2b59443a81
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-17 23:55:48 +0300

    Add ACL into SubjectsController

--- src/main/java/easytests/api/v1/controllers/SubjectsController.java
+++ src/main/java/easytests/api/v1/controllers/SubjectsController.java
@@ -1,11 +1,15 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.ForbiddenException;
 import easytests.api.v1.mappers.SubjectsMapper;
 import easytests.api.v1.models.Subject;
+import easytests.common.exceptions.NotFoundException;
 import easytests.core.models.SubjectModelInterface;
+import easytests.core.models.UserModelInterface;
 import easytests.core.models.empty.UserModelEmpty;
 import easytests.core.options.builder.SubjectsOptionsBuilderInterface;
 import easytests.core.services.SubjectsServiceInterface;
+import easytests.core.services.UsersServiceInterface;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController("SubjectsControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/subjects")
-public class SubjectsController {
+public class SubjectsController extends AbstractController{
 
     @Autowired
     protected SubjectsServiceInterface subjectsService;
@@ -30,21 +34,34 @@ public class SubjectsController {
     private SubjectsOptionsBuilderInterface subjectsOptionsBuilder;
 
     @Autowired
+    protected UsersServiceInterface usersService;
+
+    @Autowired
     @Qualifier("SubjectsMapperV1")
     private SubjectsMapper subjectsMapper;
 
 
     @GetMapping("")
-    public List<Subject> list(@RequestParam(name = "userId", required = true) Integer userId) {
-        final List<SubjectModelInterface> subjectsModels = this.subjectsService.findByUser(new UserModelEmpty(userId));
+    public List<Subject> list(@RequestParam(name = "userId", required = true) Integer userId)
+         throws NotFoundException, ForbiddenException {
+            final UserModelInterface userModel = this.usersService.find(userId);
 
-        //todo: ACL
+            if (userModel == null) {
+                throw new NotFoundException();
+            }
+
+            if (!this.acl.hasAccess(userModel)) {
+                throw new ForbiddenException();
+            }
+
+        final List<SubjectModelInterface> subjectsModels = this.subjectsService.findByUser(new UserModelEmpty(userId));
 
         return subjectsModels
                 .stream()
                 .map(model -> this.subjectsMapper.map(model, Subject.class))
                 .collect(Collectors.toList());
     }
+
     /**
      * create
      */
