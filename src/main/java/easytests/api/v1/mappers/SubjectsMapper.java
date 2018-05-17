6618d68672ee85fe8847af215492272cbd6f233f
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-17 22:57:31 +0300

    Change SubjectsMapper

--- src/main/java/easytests/api/v1/mappers/SubjectsMapper.java
+++ src/main/java/easytests/api/v1/mappers/SubjectsMapper.java
@@ -1,5 +1,8 @@
 package easytests.api.v1.mappers;
 
+import easytests.api.v1.models.Subject;
+import easytests.core.models.SubjectModel;
+import easytests.core.models.empty.ModelsListEmpty;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,15 @@ import org.springframework.stereotype.Service;
  */
 @Service("SubjectsMapperV1")
 public class SubjectsMapper extends ModelMapper {
+    public SubjectsMapper() {
+        super();
+        this.createTypeMap(SubjectModel.class, Subject.class)
+                .addMappings(
+                        mapper -> mapper.when(
+                                context -> !(context.getSource() instanceof ModelsListEmpty)
+                        ).<Integer>map(issueModel -> issueModel.getUser().getId(),
+                                (issue, id) -> issue.getUser().setId(id))
+                );
+    }
 
 }
