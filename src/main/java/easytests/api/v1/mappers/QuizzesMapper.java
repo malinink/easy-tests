c2651d5e85723fd0580ef4d44375bd949e5c1070
miron97 <miron972@mail.ru>
2018-05-18 16:54:10 +0500

    create QuizzesMapper

---
+++ src/main/java/easytests/api/v1/mappers/QuizzesMapper.java
@@ -0,0 +1,12 @@
+package easytests.api.v1.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author miron97
+ */
+@Service("QuizzesMapperV1")
+public class QuizzesMapper extends ModelMapper {
+}
