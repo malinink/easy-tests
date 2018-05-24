a854be646819261e1f93d44c9a619701a0ec654e
SakhPrace <prace_life@mail.ru>
2018-05-25 02:02:35 +0300

    resolve my mistakes

--- src/main/java/easytests/api/v1/mappers/QuestionsMapper.java
+++ src/main/java/easytests/api/v1/mappers/QuestionsMapper.java
@@ -1,13 +1,7 @@
 package easytests.api.v1.mappers;
 
-import easytests.api.v1.models.AdminAnswer;
-import easytests.api.v1.models.Question;
-import easytests.core.models.QuestionModel;
-import easytests.core.models.empty.ModelsListEmpty;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 
 /**
@@ -15,24 +9,4 @@ import java.util.List;
  */
 @Service("QuestionsMapperV1")
 public class QuestionsMapper extends ModelMapper {
-    public QuestionsMapper() {
-        super();
-        this.createTypeMap(QuestionModel.class, Question.class)
-                .addMappings(
-                        mapper -> {
-                            mapper.when(
-                                    context -> !(context.getSource() instanceof ModelsListEmpty)
-                            ).<Integer>map(questionModel -> questionModel.getQuestionType().getId(),
-                                    (question, id) -> question.setType(id));
-                            mapper.when(
-                                    context -> !(context.getSource() instanceof ModelsListEmpty)
-                            ).<Integer>map(questionModel -> questionModel.getTopic().getId(),
-                                    (question, id) -> question.getTopic().setId(id));
-                            mapper.when(
-                                    context -> !(context.getSource() instanceof ModelsListEmpty)
-                            ).<List<AdminAnswer>>map(questionModel -> questionModel.getAnswers(),
-                                    (question, list) -> question.setAnswers(list));
-                        }
-                        );
-            }
 }
