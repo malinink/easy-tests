f50971207052866ed81631e856714be1dbe0209f
Vlasovigor <vlasovigoor@gmail.com>
2018-05-25 17:35:09 +0300

    delete method for questions

--- src/main/java/easytests/api/v1/controllers/QuestionsController.java
+++ src/main/java/easytests/api/v1/controllers/QuestionsController.java
@@ -1,7 +1,10 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.exceptions.*;
 import easytests.api.v1.mappers.QuestionsMapper;
-import easytests.core.options.builder.QuestionsOptionsBuilder;
+import easytests.core.models.QuestionModelInterface;
+import easytests.core.options.QuestionsOptionsInterface;
+import easytests.core.options.builder.QuestionsOptionsBuilderInterface;
 import easytests.core.services.QuestionsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,13 +16,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController("QuestionControllerV1")
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 @RequestMapping("/v1/questions")
-public class QuestionsController {
+public class QuestionsController extends AbstractController {
 
     @Autowired
     protected QuestionsService questionsService;
 
     @Autowired
-    protected QuestionsOptionsBuilder questionsOptions;
+    protected QuestionsOptionsBuilderInterface questionsOptionsBuilder;
 
     @Autowired
     @Qualifier("QuestionsMapperV1")
@@ -37,8 +40,31 @@ public class QuestionsController {
     /**
      * show(questionId)
      */
-    /**
-     * delete(questionId)
-     */
+    @DeleteMapping("/{questionId}")
+    public void delete(@PathVariable Integer questionId) throws NotFoundException, ForbiddenException {
+        final QuestionModelInterface questionModel = this.getQuestionModel(questionId);
+
+        if (questionModel == null) {
+            throw new NotFoundException();
+        }
+
+        if (!this.acl.hasAccess(questionModel)) {
+            throw new ForbiddenException();
+        }
+
+        this.questionsService.delete(questionModel);
+    }
+
+    private QuestionModelInterface getQuestionModel(Integer id, QuestionsOptionsInterface questionOptions) throws NotFoundException {
+        final QuestionModelInterface questionModel = this.questionsService.find(id, questionOptions);
+        if (questionModel == null) {
+            throw new NotFoundException();
+        }
+        return questionModel;
+    }
+
+    private QuestionModelInterface getQuestionModel(Integer id) throws NotFoundException {
+        return this.getQuestionModel(id, this.questionsOptionsBuilder.forDelete());
+    }
 
 }
--- src/test/java/easytests/api/v1/controllers/QuestionsControllerTest.java
+++ src/test/java/easytests/api/v1/controllers/QuestionsControllerTest.java
@@ -1,9 +1,52 @@
 package easytests.api.v1.controllers;
 
+import easytests.api.v1.mappers.QuestionsMapper;
+import easytests.auth.services.AccessControlLayerServiceInterface;
+import easytests.config.SwaggerRequestValidationConfig;
+import easytests.core.models.QuestionModel;
+import easytests.core.models.QuestionModelInterface;
+import easytests.core.options.builder.QuestionsOptionsBuilder;
+import easytests.core.options.QuestionsOptions;
+import easytests.core.options.QuestionsOptionsInterface;
+import easytests.core.services.QuestionsService;
+import easytests.support.JsonSupport;
+import easytests.support.QuestionsSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.BDDMockito.*;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 /**
  * @author RisaMagpie
  */
+@Import({QuestionsMapper.class, SwaggerRequestValidationConfig.class})
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = QuestionsController.class, secure = false)
 public class QuestionsControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private QuestionsService questionsService;
+
+    @MockBean
+    private AccessControlLayerServiceInterface acl;
+
+    @MockBean
+    private QuestionsOptionsBuilder questionsOptionsBuilder;
+
+    private QuestionsSupport questionsSupport = new QuestionsSupport();
 
     /**
      * list
@@ -17,8 +60,44 @@ public class QuestionsControllerTest {
     /**
      * show(issueId)
      */
-    /**
-     * delete(issueId)
-     */
+    @Test
+    public void testDeleteSuccess() throws Exception {
+        final QuestionModelInterface questionModel = this.questionsSupport.getModelFixtureMock(0);
+        when(this.questionsService.find(any(), any())).thenReturn(questionModel);
+        when(this.acl.hasAccess(any(QuestionModelInterface.class))).thenReturn(true);
+        final ArgumentCaptor<QuestionModelInterface> argumentCaptor = ArgumentCaptor.forClass(QuestionModelInterface.class);
+        this.mvc.perform(delete("/v1/questions/1")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(""))
+                .andReturn();
+        verify(this.questionsService, times(1)).delete(argumentCaptor.capture());
+        this.questionsSupport.assertEquals(argumentCaptor.getValue(), questionModel);
+    }
 
+    @Test
+    public void testDeleteNotFound() throws Exception {
+        when(this.questionsService.find(any(Integer.class))).thenReturn(null);
+
+        mvc.perform(delete("/v1/questions/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(""))
+                .andReturn();
+    }
+
+    @Test
+    public void testDeleteForbidden() throws Exception {
+        final QuestionModelInterface questionModel = new QuestionModel();
+        questionModel.map(this.questionsSupport.getEntityFixtureMock(0));
+        when(this.questionsService.find(any(), any())).thenReturn(questionModel);
+        when(this.acl.hasAccess(any(QuestionModelInterface.class))).thenReturn(false);
+
+        mvc.perform(delete("/v1/questions/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string(""))
+                .andReturn();
+    }
 }
