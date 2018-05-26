702bb34ad77b1d232b5c637c10eacd94fda64589
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-26 08:08:57 +0300

    Change mapper

--- src/main/java/easytests/api/v1/mappers/SubjectsMapper.java
+++ src/main/java/easytests/api/v1/mappers/SubjectsMapper.java
@@ -3,10 +3,11 @@ package easytests.api.v1.mappers;
 import easytests.api.v1.models.Identity;
 import easytests.api.v1.models.Subject;
 import easytests.core.models.SubjectModel;
-import easytests.core.models.UserModelInterface;
-import easytests.core.services.UsersServiceInterface;
-import lombok.Getter;
+import easytests.core.models.UserModel;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.stereotype.Service;
 
 
@@ -15,19 +16,31 @@ import org.springframework.stereotype.Service;
  */
 @Service("SubjectsMapperV1")
 public class SubjectsMapper extends ModelMapper {
-
-    @Getter
-    private UsersServiceInterface usersService;
-
     public SubjectsMapper() {
         super();
         this.createTypeMap(SubjectModel.class, Subject.class)
                 .addMappings(mapper -> mapper.<Integer>map(subjectModel -> subjectModel.getUser().getId(),
                     (subject, id) -> subject.getUser().setId(id)));
 
-        this.createTypeMap(Identity.class, UserModelInterface.class)
-                .addMappings(mapper -> mapper
-                        .skip(UserModelInterface::setId));
+        final Converter<Identity, UserModel> convertIdentityToUserModel =
+                new Converter<Identity, UserModel>() {
+                    public UserModel convert(MappingContext<Identity, UserModel> context) {
+                        final UserModel userModel = new UserModel();
+                        userModel.setId(context.getSource().getId());
+
+                        return userModel;
+                    }
+                };
+        final PropertyMap<Subject, SubjectModel> mymap = new PropertyMap<Subject, SubjectModel>() {
+            protected void configure() {
+                map(source.getId()).setId(null);
+                map(source.getName()).setName(null);
+                map(source.getDescription()).setDescription(null);
+                using(convertIdentityToUserModel).map(source.getUser()).setUser(null);
+            }
+        };
+
+        this.addMappings(mymap);
     }
 
 
