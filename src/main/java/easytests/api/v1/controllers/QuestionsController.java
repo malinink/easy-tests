70c339e855c95c068afb89b590b4b42eabd515cb
SakhPrace <prace_life@mail.ru>
2018-05-26 10:32:45 +0300

    Add Calidators and few fix QuestionControl

--- src/main/java/easytests/api/v1/controllers/QuestionsController.java
+++ src/main/java/easytests/api/v1/controllers/QuestionsController.java
@@ -5,7 +5,6 @@ import easytests.api.v1.exceptions.IdentifiedModelException;
 import easytests.api.v1.mappers.QuestionsMapper;
 import easytests.api.v1.models.Identity;
 import easytests.api.v1.models.Question;
-import easytests.core.models.AnswerModelInterface;
 import easytests.core.models.QuestionModel;
 import easytests.core.models.QuestionModelInterface;
 import easytests.core.options.builder.QuestionsOptionsBuilder;
@@ -14,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import easytests.api.v1.validators.*;
 
 /**
  * @author RisaMagpie
@@ -32,6 +32,7 @@ public class QuestionsController extends AbstractController{
     @Autowired
     @Qualifier("QuestionsMapperV1")
     private QuestionsMapper questionsMapper;
+
 
     /**
      * list
@@ -53,10 +54,11 @@ public class QuestionsController extends AbstractController{
         }
 
        //VALIDATOR
+        final QuiestionsValidator quiestionsValidator = new QuiestionsValidator();
 
         final QuestionModelInterface questionModel = this.questionsMapper.map(question, QuestionModel.class);
 
-        this.questionAnswersValidator(questionModel);
+        quiestionsValidator.validateQuestion(questionModel);
 
         this.questionsService.save(questionModel);
 
@@ -72,61 +74,4 @@ public class QuestionsController extends AbstractController{
     /**
      * delete(questionId)
      */
-
-    private void questionAnswersValidator(QuestionModelInterface questionModel) throws Exception {
-        int rightAnswersCount = 0;
-
-        for (AnswerModelInterface answerModel: questionModel.getAnswers()) {
-            this.answerValidator(answerModel);
-            if (answerModel.getRight()) {
-                ++rightAnswersCount;
-            }
-        }
-        switch (questionModel.getQuestionType().getId()) {
-            case 1: {
-                if (rightAnswersCount == 0) {
-                    throw new BadRequestException("Right Answer must be exist");
-                }
-                if (rightAnswersCount > 1) {
-                    throw new BadRequestException("This Question can have only one right Answer");
-                }
-                break;
-            }
-            case 2: {
-                if (rightAnswersCount == 0) {
-                    throw new BadRequestException("Right Answers must be exist");
-                }
-                if (rightAnswersCount == 1) {
-                    throw new BadRequestException("This Question can't have only one right Answer");
-                }
-                if (rightAnswersCount == questionModel.getAnswers().size()) {
-                    throw new BadRequestException("This Question can't have all right Answers");
-                }
-                break;
-            }
-            /**
-             * TODO
-             * Add check for Numeric And Text Answers
-             */
-            default: break;
-        }
-    }
-
-    private void answerValidator(AnswerModelInterface answerModel) throws Exception {
-        if (!(answerModel.getId() == null)) {
-            throw new BadRequestException("Answer Id must be absent");
-        }
-        if (!answerModel.getTxt().getClass().getName().equals("String".getClass().getName())) {
-            throw new BadRequestException("Text in Answer must be String");
-        }
-        if (answerModel.getTxt().equals("") || answerModel.getTxt() == null) {
-            throw new BadRequestException("Text in answer must be not absent");
-        }
-        if (!answerModel.getRight().getClass().getName().equals(Boolean.class.getName())) {
-            throw new BadRequestException("Type of isRight must be boolean");
-        }
-        if (!answerModel.getSerialNumber().getClass().getName().equals(Integer.class.getName())) {
-            throw new BadRequestException("Serial Number must be Integer");
-        }
-    }
 }
