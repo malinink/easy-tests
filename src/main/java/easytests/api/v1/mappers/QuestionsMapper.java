27a9413c85bfd00ce19e7e6ac30b76085b0aabf6
SakhPrace <prace_life@mail.ru>
2018-05-26 11:25:35 +0300

    fix CS

--- src/main/java/easytests/api/v1/mappers/QuestionsMapper.java
+++ src/main/java/easytests/api/v1/mappers/QuestionsMapper.java
@@ -23,24 +23,24 @@ public class QuestionsMapper extends ModelMapper {
         super();
         this.createTypeMap(QuestionModel.class, Question.class)
                 .addMappings(mapper -> {
-                            mapper.<Integer>map(questionModel -> questionModel.getQuestionType().getId(),
-                                    (question, id) -> question.setType(id));
+                    mapper.<Integer>map(questionModel -> questionModel.getQuestionType().getId(),
+                            (question, id) -> question.setType(id));
 
-                            mapper.<Integer>map(questionModel -> questionModel.getTopic().getId(),
-                                    (question, id) -> question.getTopic().setId(id));
+                    mapper.<Integer>map(questionModel -> questionModel.getTopic().getId(),
+                            (question, id) -> question.getTopic().setId(id));
 
-                            mapper.<List<AdminAnswer>>map(questionModel -> questionModel.getAnswers(),
-                                    (question, list) -> question.setAnswers(list));
-                        }
-                );
+                    mapper.<List<AdminAnswer>>map(questionModel -> questionModel.getAnswers(),
+                            (question, list) -> question.setAnswers(list));
+                }
+                git);
         this.createTypeMap(AnswerModel.class, AdminAnswer.class)
                 .addMappings(mapper -> {
-                            mapper.<String>map(answerModel -> answerModel.getTxt(),
-                                    (answer, text) -> answer.setText(text));
+                    mapper.<String>map(answerModel -> answerModel.getTxt(),
+                            (answer, text) -> answer.setText(text));
 
-                            mapper.<Boolean>map(answerModel -> answerModel.getRight(),
-                                    (answer, right) -> answer.setIsRight(right));
-                        }
+                    mapper.<Boolean>map(answerModel -> answerModel.getRight(),
+                            (answer, right) -> answer.setIsRight(right));
+                }
                 );
 
         Converter<Identity, TopicModel> convertIdentityToTopicModel
