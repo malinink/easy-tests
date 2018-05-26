9dbfdf4e8f3ea6017bc0cc71e13653fab55ca99d
VeronikaRevjakina <veronikaw777@gmail.com>
2018-05-26 09:29:10 +0300

    Merge dev

--- src/main/java/easytests/api/v1/controllers/SubjectsController.java
+++ src/main/java/easytests/api/v1/controllers/SubjectsController.java
@@ -9,6 +9,7 @@ import easytests.api.v1.models.Subject;
 import easytests.core.models.SubjectModel;
 import easytests.core.models.SubjectModelInterface;
 import easytests.core.models.UserModelInterface;
+import easytests.core.options.SubjectsOptionsInterface;
 import easytests.core.options.builder.SubjectsOptionsBuilderInterface;
 import easytests.core.services.SubjectsServiceInterface;
 import easytests.core.services.UsersServiceInterface;
@@ -98,6 +99,29 @@ public class SubjectsController extends AbstractController {
     /**
      * show(subjectId)
      */
+
+    @GetMapping("/{subjectId}")
+    public Subject show(@PathVariable Integer subjectId) throws NotFoundException, ForbiddenException {
+        final SubjectModelInterface subjectModel = this.getSubjectModel(subjectId);
+
+        if (!this.acl.hasAccess(subjectModel)) {
+            throw new ForbiddenException();
+        }
+        return this.subjectsMapper.map(subjectModel, Subject.class);
+    }
+
+    private SubjectModelInterface getSubjectModel(Integer id, SubjectsOptionsInterface subjectOptions)
+            throws NotFoundException {
+        final SubjectModelInterface subjectModel = this.subjectsService.find(id, subjectOptions);
+        if (subjectModel == null) {
+            throw new NotFoundException();
+        }
+        return subjectModel;
+    }
+
+    private SubjectModelInterface getSubjectModel(Integer id) throws NotFoundException {
+        return this.getSubjectModel(id, this.subjectsOptionsBuilder.forAuth());
+    }
     /**
      * delete(subjectId)
      */
