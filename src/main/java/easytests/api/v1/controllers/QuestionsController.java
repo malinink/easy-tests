24bec052031d91523d19122fd9d7657a71f4b911
greenbarrow <green_russia@yahoo.com>
2018-05-27 18:23:07 +0300

    tmp

--- src/main/java/easytests/api/v1/controllers/QuestionsController.java
+++ src/main/java/easytests/api/v1/controllers/QuestionsController.java
@@ -2,10 +2,12 @@ package easytests.api.v1.controllers;
 
 import easytests.api.v1.exceptions.ForbiddenException;
 import easytests.api.v1.exceptions.NotFoundException;
+import easytests.api.v1.exceptions.UnidentifiedModelException;
 import easytests.api.v1.mappers.QuestionsMapper;
 import easytests.api.v1.models.Question;
 import easytests.core.models.QuestionModelInterface;
 import easytests.core.models.TopicModelInterface;
+import easytests.core.models.UserModelInterface;
 import easytests.core.options.AnswersOptions;
 import easytests.core.options.QuestionsOptions;
 import easytests.core.options.QuestionsOptionsInterface;
@@ -15,6 +17,8 @@ import easytests.core.services.QuestionsServiceInterface;
 import easytests.core.services.TopicsServiceInterface;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import easytests.core.services.UsersServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -35,7 +39,10 @@ public class QuestionsController extends AbstractController {
 
     @Autowired
     protected QuestionsOptionsBuilderInterface questionsOptionsBuilder;
-  
+
+    @Autowired
+    protected UsersServiceInterface usersService;
+
     @Autowired
     protected TopicsOptionsBuilderInterface topicsOptionsBuilder;
 
@@ -68,9 +75,21 @@ public class QuestionsController extends AbstractController {
     /**
      * create
      */
-    /**
-     * update
-     */
+
+    @PutMapping("")
+    public void update(@RequestBody Question question) throws UnidentifiedModelException, NotFoundException, ForbiddenException {
+
+        if (question.getId() == null) {
+            throw new UnidentifiedModelException();
+        }
+        final QuestionModelInterface questionModel = this.getQuestionModel(question.getId());
+
+        this.checkUser(question);
+
+        this.questionsMapper.map(question, questionModel);
+
+        this.questionsService.save(questionModel);
+    }
     
     @GetMapping("/{questionId}")
     public Question show(@PathVariable Integer questionId) throws NotFoundException, ForbiddenException {
@@ -82,6 +101,18 @@ public class QuestionsController extends AbstractController {
             throw new ForbiddenException();
         }
         return this.questionsMapper.map(questionModel, Question.class);
+    }
+
+    private void checkUser(Question question) throws ForbiddenException {
+        final UserModelInterface userModel = this.usersService.find(
+                question.getId(),
+                this.questionsOptionsBuilder.forAuth().withAnswers()
+        );
+
+        if (!this.acl.hasAccess(userModel)) {
+            throw new ForbiddenException();
+        }
+
     }
 
     private QuestionModelInterface getQuestionModel(Integer id, QuestionsOptionsInterface
